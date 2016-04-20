e0c365682a3e65ed8062a998f39b9a5049614e73
Himanshu Satija <himanshu@Himanshus-MacBook-Air.local>
2016-04-20 18:52:33 +0530

    remove redundant styles

--- Components/Widgets/CheckBox.js
+++ Components/Widgets/CheckBox.js
@@ -1,31 +1,17 @@
 /* @flow */
 'use strict';
 
-import React, { Text, View, TouchableOpacity } from 'react-native';
+import React from 'react-native';
 import NativeBaseComponent from '../Base/NativeBaseComponent';
 import _ from 'lodash';
 import computeProps from '../../Utils/computeProps';
-import Icon from 'react-native-vector-icons/Ionicons';
 import Checkbox from 'react-native-checkbox';
 
 export default class CheckBox extends NativeBaseComponent {
 
     getInitialStyle() {
         return {
-            button: {
-                padding: 10,
-                flex: 1,
-                borderColor: 'transparent',
-                height: 45,
-                justifyContent: 'space-around',
-                flexDirection: 'row',
-                marginBottom: 10,
-                backgroundColor: this.getTheme().btnPrimaryBg,
-            },
-            buttonText: {
-                fontSize: this.getTheme().btnTextSize,
-                color: this.getTheme().btnPrimaryColor
-            }
+            checkbox: {}
         }
     }
     prepareRootProps() {
