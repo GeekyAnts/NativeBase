04c1b4e45c63376578d476585cd0d235a935d184
Himanshu Satija <himanshu@Himanshus-MacBook-Air.local>
2016-04-25 13:17:17 +0530

    merge props

--- Components/Widgets/ListItem.js
+++ Components/Widgets/ListItem.js
@@ -1,7 +1,7 @@
 /* @flow */
 'use strict';
 
-import React from 'react-native';
+import React, {Image} from 'react-native';
 import NativeBaseComponent from '../Base/NativeBaseComponent';
 import _ from 'lodash';
 import computeProps from '../../Utils/computeProps';
@@ -9,6 +9,7 @@ import Icon from './Icon';
 import Text from './Text';
 import View from './View';
 import Button from './Button';
+import Thumbnail from './Thumbnail';
 
 export default class ListItemNB extends NativeBaseComponent {
 
@@ -36,8 +37,29 @@ export default class ListItemNB extends NativeBaseComponent {
             itemNote: {
                 fontSize: 15,
                 color: '#999'
+            },
+            itemSubNote: {
+                fontSize: 15,
+                color: '#999'
+            },
+            thumbnail: {
+
+            },
+            fullImage: {
+                width: 300,
+                height: 300
             }
         }
+    }
+
+    isThumbnail() {
+        var thumbnailComponentPresent = false;
+        React.Children.forEach(this.props.children, function (child) {
+            if(child.type == Thumbnail)
+                thumbnailComponentPresent = true;
+        })
+
+        return thumbnailComponentPresent;
     }
 
     getChildStyle(child) {
@@ -47,9 +69,11 @@ export default class ListItemNB extends NativeBaseComponent {
         }
 
         else if(child.type == Text) {
-            if(child.props.note)
+            if(child.props.note && this.isThumbnail())
+                return _.merge(mergedStyle, this.getInitialStyle().itemSubNote, child.props.style);
+            else if(child.props.note)
                 return _.merge(mergedStyle, this.getInitialStyle().itemNote, child.props.style);
-            else 
+            else
                 return _.merge(mergedStyle, this.getInitialStyle().itemText, child.props.style);
         }
 
@@ -57,9 +81,22 @@ export default class ListItemNB extends NativeBaseComponent {
             return _.merge(mergedStyle, this.getInitialStyle().itemButton, child.props.style);
         }
 
+        else if(child.type == Thumbnail) {
+            return _.merge(mergedStyle, this.getInitialStyle().thumbnail, child.props.style);
+        }
+
+        else if(child.type == Image && !Array.isArray(this.props.children))
+            return _.merge(mergedStyle, this.getInitialStyle().fullImage, child.props.style);
         else 
             return child.props.style;
     }
+
+    getChildProps(child) {
+        if(child.type == Image && !Array.isArray(this.props.children))
+            return _.merge({resizeMode: 'stretch'}, child.props);
+        else 
+            return child.props;
+    }  
 
     prepareRootProps() {
         if(this.props.itemDivider)
@@ -79,7 +116,7 @@ export default class ListItemNB extends NativeBaseComponent {
 
     renderChildren() {
         var newChildren = React.Children.map(this.props.children, (child) => {
-          return React.cloneElement(child, {style: this.getChildStyle(child)})
+          return React.cloneElement(child, computeProps(this.getChildProps(child), {style: this.getChildStyle(child)}));
         });
 
         console.log(newChildren);
