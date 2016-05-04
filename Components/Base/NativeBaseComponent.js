7d7077359c7125f3ea0d4141c6db8d47b2d9531f
Himanshu Satija <himanshu@Himanshus-MacBook-Air.local>
2016-05-04 13:20:48 +0530

    add childContext for theme

--- Components/Base/NativeBaseComponent.js
+++ Components/Base/NativeBaseComponent.js
@@ -1,16 +1,23 @@
 /* @flow */
 'use strict';
 
-import React, {Component, View} from 'react-native';
+import React, {Component} from 'react-native';
 import lightTheme from '../Themes/light';
-
 
 export default class NativeBaseComponent extends Component {			
 	static contextTypes = {
-	    theme: React.PropTypes.object
-	  }
+		theme: React.PropTypes.object
+	}
+
+	static childContextTypes = {
+		theme: React.PropTypes.object
+	}
+
+	getChildContext() {
+		return {theme: this.props.theme ? this.props.theme : this.getTheme()};
+	}
 
 	getTheme() {
-   		return this.context.theme || lightTheme
-	}  
+		return this.props.theme ? this.props.theme : this.context.theme || lightTheme
+	}
 }
