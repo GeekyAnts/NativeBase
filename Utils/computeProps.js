99f12dd8adc9916c4d6dba84ff94ff0081357a36
Himanshu <himanshu@Himanshus-MacBook-Air-2.local>
2016-07-07 20:03:29 +0530

    fix

--- Utils/computeProps.js
+++ Utils/computeProps.js
@@ -1,46 +1,42 @@
 'use_strict';
-
 import _ from 'lodash';
 import ReactNativePropRegistry from 'react/lib/ReactNativePropRegistry';
-
 // For compatibility with RN 0.25
 // import ReactNativePropRegistry from "react-native/Libraries/ReactNative/ReactNativePropRegistry";
-
 module.exports = function(incomingProps, defaultProps) {
-
-
     // External props has a higher precedence
     var computedProps = {};
-
     incomingProps = _.clone(incomingProps);
     delete incomingProps.children;
-
+    var incomingPropsStyle = incomingProps.style;
+    delete incomingProps.style;
     // console.log(defaultProps, incomingProps);
-
     if(incomingProps)
         _.merge(computedProps, defaultProps, incomingProps);
     else
         computedProps = defaultProps;
-
     // Pass the merged Style Object instead
-    if(incomingProps.style) {
-
-        var incomingPropsStyle;
-
-        if(typeof incomingProps.style == 'number') {
-            incomingPropsStyle = ReactNativePropRegistry.getByID(incomingProps.style);
-            computedProps.style = {};
-        } else {
-            incomingPropsStyle = incomingProps.style;
+    if(incomingPropsStyle) {
+        var computedPropsStyle = {};
+        computedProps.style = {};
+        if (Array.isArray(incomingPropsStyle)) {
+            _.forEach(incomingPropsStyle, (style)=>{
+                if(typeof style == 'number') {
+                    _.merge(computedPropsStyle, ReactNativePropRegistry.getByID(style));
+                } else {
+                    _.merge(computedPropsStyle, style);
+                }
+            })
         }
-
-        _.merge(computedProps.style, defaultProps.style, incomingPropsStyle);
-
+        else {
+            if(typeof incomingPropsStyle == 'number') {
+                computedPropsStyle = ReactNativePropRegistry.getByID(incomingPropsStyle);
+            } else {
+                computedPropsStyle = incomingPropsStyle;
+            }
+        }
+        _.merge(computedProps.style, defaultProps.style, computedPropsStyle);
     }
-
     // console.log("computedProps ", computedProps);
-
     return computedProps;
-
-
 }
