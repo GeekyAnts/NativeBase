a91a1a805ccdae9b88703109b4373ef01d5677cc
Sankhadeep Roy <sankhadeeproy@Sankhadeeps-MacBook-Air.local>
2016-07-08 17:39:56 +0530

    ecomm fix

--- Components/Widgets/InputGroup.js
+++ Components/Widgets/InputGroup.js
@@ -18,7 +18,7 @@ export default class InputGroup extends NativeBaseComponent {
 				backgroundColor: 'transparent',
 				flex: 1,
 				flexDirection: 'row',
-				borderColor: this.getContextForegroundColor(),
+				borderColor: this.getTheme().inputBorderColor,
 				paddingRight: 5
 			},
 			outerBorder: {
