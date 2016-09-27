3253fe81fac3004c752c196c13be1208d5b33341
Nazar Yablonskiy <nazaryablonskiy@gmail.com>
2016-09-27 15:14:32 +0200

    fix: goToPage of Tabs

--- Components/Widgets/Tabs.js
+++ Components/Widgets/Tabs.js
@@ -7,9 +7,18 @@ import computeProps from '../../Utils/computeProps';
 import ScrollableTabView from './../vendor/react-native-scrollable-tab-view';
 
 export default class TabNB extends NativeBaseComponent {
-
     propTypes: {
         style : React.PropTypes.object
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.goToPage = this.goToPage.bind(this);
+    }
+
+    goToPage(page) {
+        this._scrollableTabView.goToPage(page);
     }
 
     getInitialStyle() {
@@ -32,7 +41,7 @@ export default class TabNB extends NativeBaseComponent {
 
     render() {
         return(
-            <ScrollableTabView {...this.prepareRootProps()} >
+            <ScrollableTabView ref={(c) => {this._scrollableTabView = c;}} {...this.prepareRootProps()} >
             {this.props.children.filter(child => child)}
             </ScrollableTabView>
         );
