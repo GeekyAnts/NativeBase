0cd25536b5d78efe07c21b1cd88fe801a6c369b3
Sankhadeep <sankhadeep@sahusoft.com>
2016-10-25 12:15:41 +0530

    top tabBAr added

--- Components/Widgets/Button.js
+++ Components/Widgets/Button.js
@@ -29,7 +29,8 @@ export default class Button extends NativeBaseComponent {
         rounded : React.PropTypes.bool,
         large : React.PropTypes.bool,
         small : React.PropTypes.bool,
-        inputButton : React.PropTypes.bool
+        inputButton : React.PropTypes.bool,
+        tabButton : React.PropTypes.bool
     }
 
     getInitialStyle() {
@@ -125,9 +126,11 @@ export default class Button extends NativeBaseComponent {
                 (this.props.bordered) ? this.getTheme().btnPrimaryBg :
                 (this.props.color) ? this.props.color :
                 (this.props.header) ? this.getTheme().toolbarTextColor :
+                (this.props.activeTabButton) ? this.getTheme().topTabBarActiveTextColor :
+                (this.props.tabButton) ? this.getTheme().topTabBarTextColor :
                 (this.props.transparent) ? this.getContextForegroundColor() :
                 this.getTheme().inverseTextColor,
-
+            marginBottom: ((this.props.vertical) && (Platform.OS == 'android')) ? 2 : undefined,
             fontSize: (this.props.large) ? this.getTheme().iconSizeLarge :
                       (this.props.small) ? this.getTheme().iconSizeSmall :
                       (this.props.inputButton) ? this.getTheme().toolbarIconSize :
@@ -150,7 +153,6 @@ export default class Button extends NativeBaseComponent {
         return iconComponentPresent;
     }
     renderChildren() {
-
         if(typeof this.props.children == 'string') {
             return <Text style={this.getTextStyle()}>{(Platform.OS==='ios' || !this.props.capitalize) ? this.props.children : this.props.children.toUpperCase()}</Text>
         }
