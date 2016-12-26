c48626916e9a0ea125e192a30a730ab6bc77b545
William Candillon <w@28.io>
2016-12-26 13:03:24 +0100

    Update FooterTab.js

--- Components/Widgets/FooterTab.js
+++ Components/Widgets/FooterTab.js
@@ -50,8 +50,8 @@ export default class Footer extends NativeBaseComponent {
                 alignSelf: 'center',
                 flex: 1,
                 justifyContent: 'center',
-                margin: this.getTheme().footerBtnMargin ? this.getTheme().footerBtnMargin : 1,
-                height: this.getTheme().footerHeight - 4 * (this.getTheme().footerBtnMargin ? this.getTheme().footerBtnMargin : 1),
+                margin: this.getTheme().footerBtnMargin !== undefined ? this.getTheme().footerBtnMargin : 1,
+                height: this.getTheme().footerHeight - 4 * (this.getTheme().footerBtnMargin !== undefined ? this.getTheme().footerBtnMargin : 1),
                 borderRadius: (Platform.OS == 'ios') ? 7 : 0
             }
         }
