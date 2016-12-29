fed69d3b8a7deb5eb3986b73574e95e2c90e9567
William Candillon <w@28.io>
2016-12-29 10:41:06 +0100

    Update light.js

--- Components/Themes/light.js
+++ Components/Themes/light.js
@@ -111,7 +111,6 @@ export default {
     tabBarTextColor: (Platform.OS === 'ios' ) ? '#6b6b6b' : '#b3c7f9',
     tabBarActiveTextColor: (Platform.OS === 'ios' ) ? '#007aff' : '#fff',
     tabActiveBgColor: (Platform.OS=='ios') ? '#cde1f9' : undefined,
-    tabBorderRadius: (Platform.OS == 'ios') ? 7 : undefined,
     tabActiveBorderRadius: (Platform.OS=='ios') ? 7 : undefined,
 
     //Tab
