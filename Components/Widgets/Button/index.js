9173fc71a80dbc601b039d37220bb684b523ae15
Sankhadeep <sankhadeep@sahusoft.com>
2017-01-14 01:34:50 +0530

    ios button fix

--- Components/Widgets/Button/index.js
+++ Components/Widgets/Button/index.js
@@ -136,8 +136,8 @@ export default class Button extends NativeBaseComponent {
                       (this.props.small) ? this.getTheme().iconSizeSmall :
                       (this.props.inputButton) ? this.getTheme().toolbarIconSize :
                       (this.props.fabButton) ? 22 :
-                      (this.props.header) ? this.getTheme().iconFontSize : this.getTheme().iconFontSize-5,
-            lineHeight: (this.props.large) ? 52: (this.props.small || this.props.inputButton) ? 22 : this.getTheme().iconLineHeight-9
+                      (this.props.header) ? this.getTheme().iconFontSize+2 : this.getTheme().iconFontSize-5,
+            lineHeight: (this.props.large) ? 52 : (this.props.small || this.props.inputButton) ? 22 : (this.props.header) ? 36 : this.getTheme().iconLineHeight-9
         }
 
         var defaultProps = {
