775c0f30c8a2f66106b16b8f34aac0b2bea86361
Sankhadeep <sankhadeep@sahusoft.com>
2017-01-18 18:32:23 +0530

    reverted back to es5 imports

--- dist/index.js
+++ dist/index.js
@@ -1,61 +1,101 @@
-Object.defineProperty(exports,"__esModule",{value:true});exports.mapPropsToStyleNames=exports.Text=exports.Grid=exports.Row=exports.Col=exports.Gravatar=exports.Textarea=exports.Label=exports.Subtitle=exports.Item=exports.DeckSwiper=exports.ListItem=exports.List=exports.Picker=exports.FooterTab=exports.Tabs=exports.Tab=exports.Footer=exports.Content=exports.Container=exports.Switch=exports.Spinner=exports.H3=exports.H2=exports.H1=exports.CardItem=exports.Card=exports.Thumbnail=exports.Radio=exports.CheckBox=exports.Badge=exports.Body=exports.Right=exports.Left=exports.Fab=exports.Title=exports.Input=exports.InputGroup=exports.Header=exports.Icon=exports.IconNB=exports.Button=exports.Drawer=exports.StyleProvider=exports.variables=exports.getTheme=exports.Swipeout=undefined;var _theme=require('@shoutem/theme');Object.defineProperty(exports,'StyleProvider',{enumerable:true,get:function get(){return _theme.
+Object.defineProperty(exports,"__esModule",{value:true});exports.mapPropsToStyleNames=exports.Text=exports.Grid=exports.Row=exports.Col=exports.Gravatar=exports.Textarea=exports.Label=exports.Subtitle=exports.Item=exports.DeckSwiper=exports.ListItem=exports.List=exports.Picker=exports.FooterTab=exports.Tabs=exports.Tab=exports.Footer=exports.Content=exports.Container=exports.Switch=exports.Spinner=exports.H3=exports.H2=exports.H1=exports.CardItem=exports.Card=exports.Thumbnail=exports.Radio=exports.CheckBox=exports.Badge=exports.Body=exports.Right=exports.Left=exports.Fab=exports.Title=exports.Input=exports.InputGroup=exports.Header=exports.Icon=exports.IconNB=exports.Button=exports.Drawer=exports.StyleProvider=exports.variables=exports.getTheme=exports.Swipeout=undefined;var _theme=require('@shoutem/theme');var _theme2=_interopRequireDefault(_theme);
+var _lodash=require('lodash');
+var _reactNativeEasyGrid=require('react-native-easy-grid');
+var _Gravatar=require('./src/backward/Widgets/Gravatar');
+var _init=require('./init');var _init2=_interopRequireDefault(_init);
+var _theme3=require('./src/theme');var _theme4=_interopRequireDefault(_theme3);
+var _variables=require('./src/theme/variables');var _variables2=_interopRequireDefault(_variables);
+var _Drawer=require('./src/basic/Drawer');var _Drawer2=_interopRequireDefault(_Drawer);
+var _Swipe=require('./src/basic/Swipe');
+var _Text=require('./src/basic/Text');var _Text2=_interopRequireDefault(_Text);
+var _Button=require('./src/basic/Button');var _Button2=_interopRequireDefault(_Button);
+var _IconNB=require('./src/basic/IconNB');var _IconNB2=_interopRequireDefault(_IconNB);
+var _Icon=require('./src/basic/Icon');var _Icon2=_interopRequireDefault(_Icon);
+var _Header=require('./src/basic/Header');var _Header2=_interopRequireDefault(_Header);
+var _InputGroup=require('./src/basic/InputGroup');var _InputGroup2=_interopRequireDefault(_InputGroup);
+var _Input=require('./src/basic/Input');var _Input2=_interopRequireDefault(_Input);
+var _Title=require('./src/basic/Title');var _Title2=_interopRequireDefault(_Title);
+var _Fab=require('./src/basic/Fab');var _Fab2=_interopRequireDefault(_Fab);
+var _Left=require('./src/basic/Left');var _Left2=_interopRequireDefault(_Left);
+var _Right=require('./src/basic/Right');var _Right2=_interopRequireDefault(_Right);
+var _Body=require('./src/basic/Body');var _Body2=_interopRequireDefault(_Body);
+var _Badge=require('./src/basic/Badge');var _Badge2=_interopRequireDefault(_Badge);
+var _Checkbox=require('./src/basic/Checkbox');var _Checkbox2=_interopRequireDefault(_Checkbox);
+var _Radio=require('./src/basic/Radio');var _Radio2=_interopRequireDefault(_Radio);
+var _Thumbnail=require('./src/basic/Thumbnail');var _Thumbnail2=_interopRequireDefault(_Thumbnail);
+var _Card=require('./src/basic/Card');var _Card2=_interopRequireDefault(_Card);
+var _CardItem=require('./src/basic/CardItem');var _CardItem2=_interopRequireDefault(_CardItem);
+var _H=require('./src/basic/H1');var _H2=_interopRequireDefault(_H);
+var _H3=require('./src/basic/H2');var _H4=_interopRequireDefault(_H3);
+var _H5=require('./src/basic/H3');var _H6=_interopRequireDefault(_H5);
+var _Spinner=require('./src/basic/Spinner');var _Spinner2=_interopRequireDefault(_Spinner);
+var _Switch=require('./src/basic/Switch');var _Switch2=_interopRequireDefault(_Switch);
+var _Container=require('./src/basic/Container');var _Container2=_interopRequireDefault(_Container);
+var _Content=require('./src/basic/Content');var _Content2=_interopRequireDefault(_Content);
+var _Footer=require('./src/basic/Footer');var _Footer2=_interopRequireDefault(_Footer);
+var _Picker=require('./src/basic/Picker');var _Picker2=_interopRequireDefault(_Picker);
+var _List=require('./src/basic/List');var _List2=_interopRequireDefault(_List);
+var _ListItem=require('./src/basic/ListItem');var _ListItem2=_interopRequireDefault(_ListItem);
+var _DeckSwiper=require('./src/basic/DeckSwiper');var _DeckSwiper2=_interopRequireDefault(_DeckSwiper);
+var _Item=require('./src/basic/Item');var _Item2=_interopRequireDefault(_Item);
+var _Label=require('./src/basic/Label');var _Label2=_interopRequireDefault(_Label);
+var _Textarea=require('./src/basic/Textarea');var _Textarea2=_interopRequireDefault(_Textarea);
+var _Tab=require('./src/basic/Tab');var _Tab2=_interopRequireDefault(_Tab);
+var _Tabs=require('./src/basic/Tabs');var _Tabs2=_interopRequireDefault(_Tabs);
+var _Subtitle=require('./src/backward/Widgets/Subtitle');var _Subtitle2=_interopRequireDefault(_Subtitle);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}
+
+(0,_init2.default)();exports.
 
 
-
-
-
-
-
-
-
-
-
-
-
-StyleProvider;}});var _Button=require('./src/basic/Button');Object.defineProperty(exports,'Button',{enumerable:true,get:function get(){return _Button.
-
-Button;}});var _IconNB=require('./src/basic/IconNB');Object.defineProperty(exports,'IconNB',{enumerable:true,get:function get(){return _IconNB.
-IconNB;}});var _Icon=require('./src/basic/Icon');Object.defineProperty(exports,'Icon',{enumerable:true,get:function get(){return _Icon.
-Icon;}});var _Header=require('./src/basic/Header');Object.defineProperty(exports,'Header',{enumerable:true,get:function get(){return _Header.
-Header;}});var _InputGroup=require('./src/basic/InputGroup');Object.defineProperty(exports,'InputGroup',{enumerable:true,get:function get(){return _InputGroup.
-InputGroup;}});var _Input=require('./src/basic/Input');Object.defineProperty(exports,'Input',{enumerable:true,get:function get(){return _Input.
-Input;}});var _Title=require('./src/basic/Title');Object.defineProperty(exports,'Title',{enumerable:true,get:function get(){return _Title.
-Title;}});var _Fab=require('./src/basic/Fab');Object.defineProperty(exports,'Fab',{enumerable:true,get:function get(){return _Fab.
-Fab;}});var _Left=require('./src/basic/Left');Object.defineProperty(exports,'Left',{enumerable:true,get:function get(){return _Left.
-Left;}});var _Right=require('./src/basic/Right');Object.defineProperty(exports,'Right',{enumerable:true,get:function get(){return _Right.
-Right;}});var _Body=require('./src/basic/Body');Object.defineProperty(exports,'Body',{enumerable:true,get:function get(){return _Body.
-Body;}});var _Badge=require('./src/basic/Badge');Object.defineProperty(exports,'Badge',{enumerable:true,get:function get(){return _Badge.
-Badge;}});var _Checkbox=require('./src/basic/Checkbox');Object.defineProperty(exports,'CheckBox',{enumerable:true,get:function get(){return _Checkbox.
-CheckBox;}});var _Radio=require('./src/basic/Radio');Object.defineProperty(exports,'Radio',{enumerable:true,get:function get(){return _Radio.
-Radio;}});var _Thumbnail=require('./src/basic/Thumbnail');Object.defineProperty(exports,'Thumbnail',{enumerable:true,get:function get(){return _Thumbnail.
-Thumbnail;}});var _Card=require('./src/basic/Card');Object.defineProperty(exports,'Card',{enumerable:true,get:function get(){return _Card.
-Card;}});var _CardItem=require('./src/basic/CardItem');Object.defineProperty(exports,'CardItem',{enumerable:true,get:function get(){return _CardItem.
-CardItem;}});var _H=require('./src/basic/H1');Object.defineProperty(exports,'H1',{enumerable:true,get:function get(){return _H.
-H1;}});var _H2=require('./src/basic/H2');Object.defineProperty(exports,'H2',{enumerable:true,get:function get(){return _H2.
-H2;}});var _H3=require('./src/basic/H3');Object.defineProperty(exports,'H3',{enumerable:true,get:function get(){return _H3.
-H3;}});var _Spinner=require('./src/basic/Spinner');Object.defineProperty(exports,'Spinner',{enumerable:true,get:function get(){return _Spinner.
-Spinner;}});var _Switch=require('./src/basic/Switch');Object.defineProperty(exports,'Switch',{enumerable:true,get:function get(){return _Switch.
-Switch;}});var _Container=require('./src/basic/Container');Object.defineProperty(exports,'Container',{enumerable:true,get:function get(){return _Container.
-Container;}});var _Content=require('./src/basic/Content');Object.defineProperty(exports,'Content',{enumerable:true,get:function get(){return _Content.
-Content;}});var _Footer=require('./src/basic/Footer');Object.defineProperty(exports,'Footer',{enumerable:true,get:function get(){return _Footer.
-Footer;}});var _Tab=require('./src/basic/Tab');Object.defineProperty(exports,'Tab',{enumerable:true,get:function get(){return _Tab.
-Tab;}});var _Tabs=require('./src/basic/Tabs');Object.defineProperty(exports,'Tabs',{enumerable:true,get:function get(){return _Tabs.
-Tabs;}});var _FooterTab=require('./src/basic/FooterTab');Object.defineProperty(exports,'FooterTab',{enumerable:true,get:function get(){return _FooterTab.
-FooterTab;}});var _Picker=require('./src/basic/Picker');Object.defineProperty(exports,'Picker',{enumerable:true,get:function get(){return _Picker.
-PickerNB;}});var _List=require('./src/basic/List');Object.defineProperty(exports,'List',{enumerable:true,get:function get(){return _List.
-List;}});var _ListItem=require('./src/basic/ListItem');Object.defineProperty(exports,'ListItem',{enumerable:true,get:function get(){return _ListItem.
-ListItem;}});var _DeckSwiper=require('./src/basic/DeckSwiper');Object.defineProperty(exports,'DeckSwiper',{enumerable:true,get:function get(){return _DeckSwiper.
-DeckSwiper;}});var _Item=require('./src/basic/Item');Object.defineProperty(exports,'Item',{enumerable:true,get:function get(){return _Item.
-Item;}});var _Subtitle=require('./src/backward/Widgets/Subtitle');Object.defineProperty(exports,'Subtitle',{enumerable:true,get:function get(){return _Subtitle.
-Subtitle;}});var _Label=require('./src/basic/Label');Object.defineProperty(exports,'Label',{enumerable:true,get:function get(){return _Label.
-Label;}});var _Textarea=require('./src/basic/Textarea');Object.defineProperty(exports,'Textarea',{enumerable:true,get:function get(){return _Textarea.
-Textarea;}});var _reactNativeEasyGrid=require('react-native-easy-grid');var _Gravatar=require('./src/backward/Widgets/Gravatar');var _init=require('./init');var _init2=_interopRequireDefault(_init);var _theme2=require('./src/theme');var _theme3=_interopRequireDefault(_theme2);var _variables=require('./src/theme/variables');var _variables2=_interopRequireDefault(_variables);var _Drawer=require('./src/basic/Drawer');var _Drawer2=_interopRequireDefault(_Drawer);var _Swipe=require('./src/basic/Swipe');var _Text=require('./src/basic/Text');var _Text2=_interopRequireDefault(_Text);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}(0,_init2.default)();exports.Swipeout=_Swipe.Swipeout;exports.getTheme=_theme3.default;exports.variables=_variables2.default;exports.Drawer=_Drawer2.default;exports.
+Swipeout=_Swipe.Swipeout;exports.
+getTheme=_theme4.default;exports.
+variables=_variables2.default;exports.
+StyleProvider=_theme2.default;exports.
+Drawer=_Drawer2.default;exports.
+Button=_Button2.default;exports.
+IconNB=_IconNB2.default;exports.
+Icon=_Icon2.default;exports.
+Header=_Header2.default;exports.
+InputGroup=_InputGroup2.default;exports.
+Input=_Input2.default;exports.
+Title=_Title2.default;exports.
+Fab=_Fab2.default;exports.
+Left=_Left2.default;exports.
+Right=_Right2.default;exports.
+Body=_Body2.default;exports.
+Badge=_Badge2.default;exports.
+CheckBox=_Checkbox2.default;exports.
+Radio=_Radio2.default;exports.
+Thumbnail=_Thumbnail2.default;exports.
+Card=_Card2.default;exports.
+CardItem=_CardItem2.default;exports.
+H1=_H2.default;exports.
+H2=_H4.default;exports.
+H3=_H6.default;exports.
+Spinner=_Spinner2.default;exports.
+Switch=_Switch2.default;exports.
+Container=_Container2.default;exports.
+Content=_Content2.default;exports.
+Footer=_Footer2.default;exports.
+Tab=_Tab2.default;exports.
+Tabs=_Tabs2.default;exports.
+FooterTab=FooterTab;exports.
+Picker=_Picker2.default;exports.
+List=_List2.default;exports.
+ListItem=_ListItem2.default;exports.
+DeckSwiper=_DeckSwiper2.default;exports.
+Item=_Item2.default;exports.
+Subtitle=_Subtitle2.default;exports.
+Label=_Label2.default;exports.
+Textarea=_Textarea2.default;exports.
 Gravatar=_Gravatar.Gravatar;exports.
-Col=_reactNativeEasyGrid.Col;exports.Row=_reactNativeEasyGrid.Row;exports.Grid=_reactNativeEasyGrid.Grid;exports.
+Col=_reactNativeEasyGrid.Col;exports.
+Row=_reactNativeEasyGrid.Row;exports.
+Grid=_reactNativeEasyGrid.Grid;exports.
 Text=_Text2.default;
 
 
-var mapPropsToStyleNames=function mapPropsToStyleNames(styleNames,props){return _.keys(props);};exports.
+var mapPropsToStyleNames=function mapPropsToStyleNames(styleNames,props){return(0,_lodash.keys)(props);};exports.
 
 mapPropsToStyleNames=mapPropsToStyleNames;
 //# sourceMappingURL=index.js.map
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import StyleProvider from '@shoutem/theme';
+import { keys } from 'lodash';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import { Gravatar } from './src/backward/Widgets/Gravatar';
 import setDefaultThemeStyle from './init';
@@ -6,56 +8,94 @@ import variables from './src/theme/variables';
 import Drawer from './src/basic/Drawer';
 import { Swipeout } from './src/basic/Swipe';
 import Text from './src/basic/Text';
+import Button from './src/basic/Button';
+import IconNB from './src/basic/IconNB';
+import Icon from './src/basic/Icon';
+import Header from './src/basic/Header';
+import InputGroup from './src/basic/InputGroup';
+import Input from './src/basic/Input';
+import Title from './src/basic/Title';
+import Fab from './src/basic/Fab';
+import Left from './src/basic/Left';
+import Right from './src/basic/Right';
+import Body from './src/basic/Body';
+import Badge from './src/basic/Badge';
+import CheckBox from './src/basic/Checkbox';
+import Radio from './src/basic/Radio';
+import Thumbnail from './src/basic/Thumbnail';
+import Card from './src/basic/Card';
+import CardItem from './src/basic/CardItem';
+import H1 from './src/basic/H1';
+import H2 from './src/basic/H2';
+import H3 from './src/basic/H3';
+import Spinner from './src/basic/Spinner';
+import Switch from './src/basic/Switch';
+import Container from './src/basic/Container';
+import Content from './src/basic/Content';
+import Footer from './src/basic/Footer';
+import PickerNB from './src/basic/Picker';
+import List from './src/basic/List';
+import ListItem from './src/basic/ListItem';
+import DeckSwiper from './src/basic/DeckSwiper';
+import Item from './src/basic/Item';
+import Label from './src/basic/Label';
+import Textarea from './src/basic/Textarea';
+import Tab from './src/basic/Tab';
+import Tabs from './src/basic/Tabs';
+import Subtitle from './src/backward/Widgets/Subtitle';
 
 setDefaultThemeStyle();
 // Theme
-export { Swipeout };
-export { getTheme };
-export { variables };
-export { StyleProvider } from '@shoutem/theme';
-export { Drawer };
-export { Button } from './src/basic/Button';
-export { IconNB } from './src/basic/IconNB';
-export { Icon } from './src/basic/Icon';
-export { Header } from './src/basic/Header';
-export { InputGroup } from './src/basic/InputGroup';
-export { Input } from './src/basic/Input';
-export { Title } from './src/basic/Title';
-export { Fab } from './src/basic/Fab';
-export { Left } from './src/basic/Left';
-export { Right } from './src/basic/Right';
-export { Body } from './src/basic/Body';
-export { Badge } from './src/basic/Badge';
-export { CheckBox } from './src/basic/Checkbox';
-export { Radio } from './src/basic/Radio';
-export { Thumbnail } from './src/basic/Thumbnail';
-export { Card } from './src/basic/Card';
-export { CardItem } from './src/basic/CardItem';
-export { H1 } from './src/basic/H1';
-export { H2 } from './src/basic/H2';
-export { H3 } from './src/basic/H3';
-export { Spinner } from './src/basic/Spinner';
-export { Switch } from './src/basic/Switch';
-export { Container } from './src/basic/Container';
-export { Content } from './src/basic/Content';
-export { Footer } from './src/basic/Footer';
-export { Tab } from './src/basic/Tab';
-export { Tabs } from './src/basic/Tabs';
-export { FooterTab } from './src/basic/FooterTab';
-export { PickerNB as Picker } from './src/basic/Picker';
-export { List } from './src/basic/List';
-export { ListItem } from './src/basic/ListItem';
-export { DeckSwiper } from './src/basic/DeckSwiper';
-export { Item } from './src/basic/Item';
-export { Subtitle } from './src/backward/Widgets/Subtitle';
-export { Label } from './src/basic/Label';
-export { Textarea } from './src/basic/Textarea';
-export { Gravatar };
-export { Col, Row, Grid };
-export { Text };
+export {
+  Swipeout,
+  getTheme,
+  variables,
+  StyleProvider,
+  Drawer,
+  Button,
+  IconNB,
+  Icon,
+  Header,
+  InputGroup,
+  Input,
+  Title,
+  Fab,
+  Left,
+  Right,
+  Body,
+  Badge,
+  CheckBox,
+  Radio,
+  Thumbnail,
+  Card,
+  CardItem,
+  H1,
+  H2,
+  H3,
+  Spinner,
+  Switch,
+  Container,
+  Content,
+  Footer,
+  Tab,
+  Tabs,
+  FooterTab,
+  PickerNB as Picker,
+  List,
+  ListItem,
+  DeckSwiper,
+  Item,
+  Subtitle,
+  Label,
+  Textarea,
+  Gravatar,
+  Col,
+  Row,
+  Grid,
+  Text,
+};
 
-
-const mapPropsToStyleNames = (styleNames, props) => _.keys(props);
+const mapPropsToStyleNames = (styleNames, props) => keys(props);
 
 export { mapPropsToStyleNames };
 
