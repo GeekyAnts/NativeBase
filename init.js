a955a8b164450e943e9823c31994bc7d84f22345
Sankhadeep <sankhadeep@sahusoft.com>
2017-01-18 16:17:46 +0530

    entry script fix

--- init.js
+++ init.js
@@ -1,11 +1,11 @@
 import { Theme } from '@shoutem/theme';
-import getThemeStyle from './theme';
+import getThemeStyle from './src/theme';
 
 function setDefaultThemeStyle() {
   const theme = getThemeStyle();
   Theme.setDefaultThemeStyle(theme);
 }
 
-export {
+export default {
   setDefaultThemeStyle,
 };
