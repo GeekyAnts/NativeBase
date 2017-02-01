39d95bf81ee8591485c36d2cbd39189ef49f3baf
Sankhadeep <sankhadeep@sahusoft.com>
2017-02-01 16:07:43 +0530

    index changed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import StyleProvider from '@shoutem/theme';
+import { StyleProvider } from '@shoutem/theme';
 import { keys } from 'lodash';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import { Gravatar } from './backward/Widgets/Gravatar';
@@ -107,4 +107,3 @@ export {
 const mapPropsToStyleNames = (styleNames, props) => keys(props);
 
 export { mapPropsToStyleNames };
-
