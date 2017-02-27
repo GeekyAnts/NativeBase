8d58e661a3023fe300646fe42aac58db3473bc5e
Sankhadeep <sankhadeep@sahusoft.com>
2017-02-27 19:09:33 +0530

    Text and statusbar fix

--- src/theme/components/Text.js
+++ src/theme/components/Text.js
@@ -7,7 +7,7 @@ export default (variables = variable) => {
   const textTheme = {
       fontSize: variables.DefaultFontSize - 1,
       fontFamily: variables.fontFamily,
-      color: '#000',
+      color: variables.textColor,
       '.note': {
         color: '#a7a7a7',
         fontSize: variables.noteFontSize
