cb82455ec00e716bfef4a543fec2d34ac5fc3641
Sankhadeep <sankhadeep@sahusoft.com>
2017-03-09 16:20:40 +0530

    transpiled

--- dist/src/basic/List.js
+++ dist/src/basic/List.js
@@ -4,9 +4,21 @@ var _reactNative=require('react-native');
 var _theme=require('@shoutem/theme');
 var _mapPropsToStyleNames=require('../Utils/mapPropsToStyleNames');var _mapPropsToStyleNames2=_interopRequireDefault(_mapPropsToStyleNames);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self,call){if(!self){throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call&&(typeof call==="object"||typeof call==="function")?call:self;}function _inherits(subClass,superClass){if(typeof superClass!=="function"&&superClass!==null){throw new TypeError("Super expression must either be null or a function, not "+typeof superClass);}subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,enumerable:false,writable:true,configurable:true}});if(superClass)Object.setPrototypeOf?Object.setPrototypeOf(subClass,superClass):subClass.__proto__=superClass;}var
 
-List=function(_Component){_inherits(List,_Component);function List(){_classCallCheck(this,List);return _possibleConstructorReturn(this,(List.__proto__||Object.getPrototypeOf(List)).apply(this,arguments));}_createClass(List,[{key:'renderChildren',value:function renderChildren()
+List=function(_Component){_inherits(List,_Component);
 
+function List(props){_classCallCheck(this,List);var _this=_possibleConstructorReturn(this,(List.__proto__||Object.getPrototypeOf(List)).call(this,
+props));
+var ds=new _reactNative.ListView.DataSource({rowHasChanged:function rowHasChanged(r1,r2){return r1!==r2;}});
+_this.state={
+dataSource:ds.cloneWithRows(_this.props.dataArray)};return _this;
 
+}_createClass(List,[{key:'componentWillReceiveProps',value:function componentWillReceiveProps(
+nextProps){
+var ds=new _reactNative.ListView.DataSource({rowHasChanged:function rowHasChanged(r1,r2){return r1!==r2;}});
+this.setState({
+dataSource:ds.cloneWithRows(nextProps.dataArray)});
+
+}},{key:'renderChildren',value:function renderChildren()
 {
 var childrenArray=_react2.default.Children.map(this.props.children,function(child){return child;});
 
@@ -15,13 +27,11 @@ return childrenArray;
 
 {var _this2=this;
 if(this.props.dataArray&&this.props.renderRow){
-var ds=new _reactNative.ListView.DataSource({rowHasChanged:function rowHasChanged(r1,r2){return r1!==r2;}});
-var dataSource=ds.cloneWithRows(this.props.dataArray);
 return(
 _react2.default.createElement(_reactNative.ListView,_extends({},
 this.props,{
 enableEmptySections:true,
-dataSource:dataSource,
+dataSource:this.state.dataSource,
 renderRow:this.props.renderRow})));
 
 
