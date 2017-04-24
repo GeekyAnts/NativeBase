b5df26406a49a32e5dfb9d616adba8613d0bd922
Sampsa Saarela <sampsa@booni.fi>
2017-04-24 21:14:11 +0300

    fixed property

--- src/basic/ToastContainer.js
+++ src/basic/ToastContainer.js
@@ -46,7 +46,7 @@ class ToastContainer extends Component {
   render() {
     return (
       <Modal
-        supportedOrientations={this.props.supportedOrientations || null}
+        supportedOrientations={this.state.supportedOrientations || null}
         animationType={(this.state.position=='bottom') ? "slide" : "fade"}
         transparent={true}
         visible={this.state.modalVisible}
