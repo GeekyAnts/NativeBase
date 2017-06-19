4b08cdcb8686b309f9e49949a46e3ef7cfed0c83
Shivraj <shivraj@geekyants.com>
2017-06-19 15:46:22 +0530

    fix for #745(android)

--- src/theme/components/Input.js
+++ src/theme/components/Input.js
@@ -1,15 +1,18 @@
-import variable from "./../variables/platform";
+import variable from './../variables/platform';
 
 export default (variables = variable) => {
-  const inputTheme = {
-    height: variables.inputHeightBase,
-    color: variables.inputColor,
-    paddingLeft: 5,
-    paddingRight: 5,
-    flex: 1,
-    fontSize: variables.inputFontSize,
-    lineHeight: variables.inputLineHeight
-  };
+	const inputTheme = {
+		'.multiline': {
+			height: null,
+		},
+		height: variables.inputHeightBase,
+		color: variables.inputColor,
+		paddingLeft: 5,
+		paddingRight: 5,
+		flex: 1,
+		fontSize: variables.inputFontSize,
+		lineHeight: variables.inputLineHeight,
+	};
 
-  return inputTheme;
+	return inputTheme;
 };
