5b233bf4e464e3e9e0e41c087a12f3268bf29ce3
Shivraj <shivraj@geekyants.com>
2017-06-28 17:18:52 +0530

    group prop added for Separator #903

--- dist/src/theme/components/Separator.js
+++ dist/src/theme/components/Separator.js
@@ -1,33 +1,45 @@
-Object.defineProperty(exports,"__esModule",{value:true});var _platform=require("./../variables/platform");var _platform2=_interopRequireDefault(_platform);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}exports.default=
+Object.defineProperty(exports,"__esModule",{value:true});var _platform=require('./../variables/platform');var _platform2=_interopRequireDefault(_platform);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}exports.default=
 
 function(){var variables=arguments.length>0&&arguments[0]!==undefined?arguments[0]:_platform2.default;
 var theme={
-".bordered":{
-".noTopBorder":{
+'.group':{
+height:50,
+paddingVertical:variables.listItemPadding-8,
+paddingTop:variables.listItemPadding+12,
+'.bordered':{
+height:50,
+paddingVertical:variables.listItemPadding-8,
+paddingTop:variables.listItemPadding+12}},
+
+
+'.bordered':{
+'.noTopBorder':{
 borderTopWidth:0},
 
-".noBottomBorder":{
+'.noBottomBorder':{
 borderBottomWidth:0},
 
 height:35,
-paddingVertical:variables.listItemPadding+1,
+paddingTop:variables.listItemPadding+2,
+paddingBottom:variables.listItemPadding,
 borderBottomWidth:variables.borderWidth,
 borderTopWidth:variables.borderWidth,
 borderColor:variables.listBorderColor},
 
-"NativeBase.Text":{
-fontSize:variables.tabBarTextSize},
+'NativeBase.Text':{
+fontSize:variables.tabBarTextSize-2,
+color:'#777'},
 
-".noTopBorder":{
+'.noTopBorder':{
 borderTopWidth:0},
 
-".noBottomBorder":{
+'.noBottomBorder':{
 borderBottomWidth:0},
 
 height:38,
-backgroundColor:"#F0EFF5",
+backgroundColor:'#F0EFF5',
 flex:1,
-justifyContent:"center",
+justifyContent:'center',
 paddingLeft:variables.listItemPadding+5};
 
 
