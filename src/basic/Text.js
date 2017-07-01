72b305e9a614661db95545d925ebc44e91482148
HJ.Park <hyunjun19@gmail.com>
2017-07-01 11:34:38 +0900

    Text add debug code

--- src/basic/Text.js
+++ src/basic/Text.js
@@ -1,13 +1,14 @@
-import React, { Component } from "react";
-import { Text as RNText } from "react-native";
+import React, { Component } from 'react';
+import { Text as RNText } from 'react-native';
 import _ from 'lodash';
 
-import { connectStyle } from "native-base-shoutem-theme";
-import { connectAnimation } from "@shoutem/animation";
-import mapPropsToStyleNames from "../Utils/mapPropsToStyleNames";
+import { connectStyle } from 'native-base-shoutem-theme';
+import { connectAnimation } from '@shoutem/animation';
+import mapPropsToStyleNames from '../Utils/mapPropsToStyleNames';
 
 class Text extends Component {
   render() {
+    console.log('children', this.props.children);
     return (
       <RNText ref={c => (this._root = c)} {...this.props}>
         {this.props.uppercase
@@ -21,15 +22,15 @@ class Text extends Component {
 Text.propTypes = {
   ...RNText.propTypes,
   uppercase: React.PropTypes.bool,
-  style: React.PropTypes.object
+  style: React.PropTypes.object,
 };
 
 Text.defaultProps = {
-  uppercase: false
+  uppercase: false,
 };
 
 const AnimatedText = connectAnimation(Text);
-const StyledText = connectStyle("NativeBase.Text", {}, mapPropsToStyleNames)(
+const StyledText = connectStyle('NativeBase.Text', {}, mapPropsToStyleNames)(
   Text
 );
 
