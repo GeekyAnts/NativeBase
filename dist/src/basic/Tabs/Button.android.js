400037f70f61672b92cd1d408ef46e8f80ae0cc5
akhil-geekyants <akhil@geekyants.com>
2017-10-13 13:13:22 +0530

    Committing after transpile

--- dist/src/basic/Tabs/Button.android.js
+++ dist/src/basic/Tabs/Button.android.js
@@ -1,4 +1,4 @@
-Object.defineProperty(exports,"__esModule",{value:true});var _extends=Object.assign||function(target){for(var i=1;i<arguments.length;i++){var source=arguments[i];for(var key in source){if(Object.prototype.hasOwnProperty.call(source,key)){target[key]=source[key];}}}return target;};var _jsxFileName="src/basic/Tabs/Button.android.js";var React=require("react");
+var _extends=Object.assign||function(target){for(var i=1;i<arguments.length;i++){var source=arguments[i];for(var key in source){if(Object.prototype.hasOwnProperty.call(source,key)){target[key]=source[key];}}}return target;};var _jsxFileName="src/basic/Tabs/Button.android.js";var React=require("react");
 var ReactNative=require("react-native");var
 TouchableNativeFeedback=ReactNative.TouchableNativeFeedback,View=ReactNative.View;
 
@@ -12,7 +12,7 @@ props,{__source:{fileName:_jsxFileName,lineNumber:7}}),
 props.children));
 
 
-};exports.default=
+};
 
-Button;
+module.exports=Button;
 //# sourceMappingURL=Button.android.js.map
