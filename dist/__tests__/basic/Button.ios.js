9951d0cbda5b649ea54223904ca409a7a9d4826d
Supriya <>
2017-11-23 14:59:15 +0530

    Transpiled

---
+++ dist/__tests__/basic/Button.ios.js
@@ -0,0 +1,18 @@
+var _jsxFileName="__tests__/basic/Button.ios.js";var _react=require("react");var _react2=_interopRequireDefault(_react);
+var _reactNative=require("react-native");
+var _reactTestRenderer=require("react-test-renderer");var _reactTestRenderer2=_interopRequireDefault(_reactTestRenderer);
+var _Button=require("../../src/basic/Button");function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}
+
+
+
+jest.mock("Platform",function(){
+var Platform=require.requireActual("Platform");
+Platform.OS="ios";
+return Platform;
+});
+
+it("renders regular button",function(){
+var tree=_reactTestRenderer2.default.create(_react2.default.createElement(_Button.Button,{__source:{fileName:_jsxFileName,lineNumber:15}},"Test")).toJSON();
+expect(tree).toMatchSnapshot();
+});
+//# sourceMappingURL=Button.ios.js.map
---
+++ dist/__tests__/basic/ListItem.android.js
@@ -0,0 +1,18 @@
+var _jsxFileName="__tests__/basic/ListItem.android.js";var _react=require("react");var _react2=_interopRequireDefault(_react);
+require("react-native");
+var _reactTestRenderer=require("react-test-renderer");var _reactTestRenderer2=_interopRequireDefault(_reactTestRenderer);
+var _ListItem=require("../../src/basic/ListItem");function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}
+
+
+
+jest.mock("Platform",function(){
+var Platform=require.requireActual("Platform");
+Platform.OS="android";
+return Platform;
+});
+
+it("renders regular listItem",function(){
+var tree=_reactTestRenderer2.default.create(_react2.default.createElement(_ListItem.ListItem,{__source:{fileName:_jsxFileName,lineNumber:15}},"This is ListItem")).toJSON();
+expect(tree).toMatchSnapshot();
+});
+//# sourceMappingURL=ListItem.android.js.map
