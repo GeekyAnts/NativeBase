03502034bcdd3cf86344d876fcc4216ce3c8c9aa
akhil-geekyants <akhil@geekyants.com>
2017-12-11 18:42:04 +0530

    Possible Fix for issue #1239 Replaced ListView with FlatList in Picker

--- src/basic/Picker.ios.js
+++ src/basic/Picker.ios.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import createReactClass from "create-react-class";
-import { Picker, Modal, View, ListView, ViewPropTypes } from "react-native";
+import { Picker, Modal, View, ViewPropTypes, FlatList } from "react-native";
 import _ from "lodash";
 import { Text } from "./Text";
 import { List } from "./List";
@@ -185,32 +185,30 @@ class PickerNB extends Component {
           <Container>
             {this.renderHeader()}
             <Content>
-              <List
-                dataArray={this.state.dataSource}
-                renderRow={child => (
-                  <ListItem
-                    selected={child.props.value === this.props.selectedValue}
-                    button
-                    style={this.props.itemStyle}
-                    onPress={() => {
-                      this._setModalVisible(false);
-                      this.props.onValueChange(child.props.value);
-                      this.setState({ current: child.props.label });
-                    }}
-                  >
-                    <Text style={this.props.itemTextStyle}>
-                      {child.props.label}
-                    </Text>
-                    <Right>
-                      {child.props.value === this.props.selectedValue ? (
-                        <Radio selected />
-                      ) : (
-                        <Radio selected={false} />
-                      )}
-                    </Right>
-                  </ListItem>
+            <FlatList
+              data={this.state.dataSource}
+              renderItem={({item}) => <ListItem
+              selected={item.props.value === this.props.selectedValue}
+              button
+              style={this.props.itemStyle}
+              onPress={() => {
+                this._setModalVisible(false);
+                this.props.onValueChange(item.props.value);
+                this.setState({ current: item.props.label });
+              }}
+            >
+              <Text style={this.props.itemTextStyle}>
+                {item.props.label}
+              </Text>
+              <Right>
+                {item.props.value === this.props.selectedValue ? (
+                  <Radio selected />
+                ) : (
+                  <Radio selected={false} />
                 )}
-              />
+              </Right>
+            </ListItem>}
+            />
             </Content>
           </Container>
         </Modal>
