56357e2df4b5ecc36576fb52c0e682417d50d25d
Steven Yoon <stevenyoon95@gmail.com>
2017-12-21 10:47:22 -0500

    changing source for init

--- src/init.js
+++ src/init.js
@@ -1,7 +1,7 @@
-import { Theme } from "native-base-shoutem-theme";
-import getThemeStyle from "./theme/components";
+Object.defineProperty(exports,"__esModule",{value:true});exports.default=
 
-export default function setDefaultThemeStyle() {
-  const theme = getThemeStyle();
-  Theme.setDefaultThemeStyle(theme);
+
+setDefaultThemeStyle;var _nativeBaseShoutemTheme=require("native-base-shoutem-theme");var _components=require("./theme/components");var _components2=_interopRequireDefault(_components);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}function setDefaultThemeStyle(){
+var theme=(0,_components2.default)();
+_nativeBaseShoutemTheme.Theme.setDefaultThemeStyle(theme);
 }
