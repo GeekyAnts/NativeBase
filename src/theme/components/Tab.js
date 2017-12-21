72c3fb0be79d2c67982191f47e7e2a74cd7d28ab
Steven Yoon <stevenyoon95@gmail.com>
2017-12-21 15:06:08 -0500

    make tab background transparent

--- src/theme/components/Tab.js
+++ src/theme/components/Tab.js
@@ -3,7 +3,7 @@ import variable from "./../variables/platform";
 export default (variables = variable) => {
   const tabTheme = {
     flex: 1,
-    backgroundColor: "#FFF"
+    backgroundColor: 'transparent'
   };
 
   return tabTheme;
