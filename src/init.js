fae12a0821d1303f5e6cbab9bce3740f7dda4f86
Steven Yoon <stevenyoon95@gmail.com>
2018-01-02 16:06:32 -0500

    fixing path

--- src/init.js
+++ src/init.js
@@ -1,5 +1,5 @@
 import { Theme } from "native-base-shoutem-theme";
-import getThemeStyle from "/src/theme/components/index";
+import getThemeStyle from "./theme/components";
 
 export default function setDefaultThemeStyle() {
   const theme = getThemeStyle();
