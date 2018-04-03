3a11e3bed3c564b674b86db4cb144521097b40f2
Murat Akbal <murat@carbonhealth.com>
2018-04-03 13:21:12 -0700

    Use variables from theme if provided in Input component

--- src/basic/Input.js
+++ src/basic/Input.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { TextInput } from "react-native";
 import { connectStyle } from "native-base-shoutem-theme";
-import variables from "../theme/variables/platform";
+import variable from "../theme/variables/platform";
 import mapPropsToStyleNames from "../Utils/mapPropsToStyleNames";
 import NativeBaseComponent from "./Base/NativeBaseComponent";
 
 class Input extends NativeBaseComponent {
 	render() {
+		const variables = this.context.theme
+			? this.context.theme["@@shoutem.theme/themeStyle"].variables
+			: variable;
 		return (
 			<TextInput
 				ref={c => {
