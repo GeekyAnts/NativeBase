0f0748f6465413082c3e49ec9a4b5a6ecfb2430d
Supriya Kalghatgi <supriya@sahusoft.com>
2018-04-13 11:45:52 +0530

    Fixed #1766

--- src/theme/components/Item.js
+++ src/theme/components/Item.js
@@ -49,7 +49,8 @@ export default (variables = variable) => {
         alignSelf: Platform.OS === "ios" ? "stretch" : "flex-start",
         flex: 1,
         width: Platform.OS === "ios" ? null : variables.deviceWidth - 25,
-        fontSize: variables.inputFontSize
+        fontSize: variables.inputFontSize,
+        lineHeight: variables.inputLineHeight - 6,
       },
       flexDirection: null,
       height: variables.inputHeightBase + 15
