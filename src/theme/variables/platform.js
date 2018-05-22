7c13fc3ae2a7f904fd4ad9f054b77928585d26bc
akhil-geekyants <akhil@geekyants.com>
2018-05-22 15:49:48 +0530

    Added iPhoneX safe area padding values to theme/variable files

--- src/theme/variables/platform.js
+++ src/theme/variables/platform.js
@@ -245,5 +245,21 @@ export default {
   deviceWidth,
   deviceHeight,
   isIphoneX,
-  inputGroupRoundedBorderRadius: 30
+  inputGroupRoundedBorderRadius: 30,
+
+  //iPhoneX SafeArea
+  Inset: {
+    portrait: {
+      topInset: 24,
+      leftInset: 0,
+      rightInset: 0,
+      bottomInset: 34
+    },
+    landscape: {
+      topInset: 0,
+      leftInset: 44,
+      rightInset: 44,
+      bottomInset: 21
+    }
+  }
 };
