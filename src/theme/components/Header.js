e7569a7bac31bab0b6f6c64e726354ada3476f10
Supriya Kalghatgi <supriya@sahusoft.com>
2018-06-22 12:51:32 +0530

    fixed for iPhoneX

--- src/theme/components/Header.js
+++ src/theme/components/Header.js
@@ -48,7 +48,7 @@ export default (variables = variable) => {
       shadowRadius: null,
       shadowOpacity: null,
       paddingTop: platform === "android" ? StatusBar.currentHeight : undefined,
-      height: platform === "android" ? variables.toolbarHeight + StatusBar.currentHeight : undefined
+      height: platform === "android" ? variables.toolbarHeight + StatusBar.currentHeight : variables.toolbarHeight
     },
     ".noShadow": {
       elevation: 0,
