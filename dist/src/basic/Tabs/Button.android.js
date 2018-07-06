f6b810f5f0d9b1a4cfabaccbcf733cafc7edff42
Supriya Kalghatgi <supriya@Supriyas-Air.Dlink>
2018-07-06 19:23:39 +0530

    Upgraded deps and dev-deps, compatible with RN 0.56.0

--- dist/src/basic/Tabs/Button.android.js
+++ dist/src/basic/Tabs/Button.android.js
@@ -1,17 +1,2 @@
-var _extends=Object.assign||function(target){for(var i=1;i<arguments.length;i++){var source=arguments[i];for(var key in source){if(Object.prototype.hasOwnProperty.call(source,key)){target[key]=source[key];}}}return target;};var _jsxFileName="src/basic/Tabs/Button.android.js";var React=require("react");
-var ReactNative=require("react-native");var
-TouchableNativeFeedback=ReactNative.TouchableNativeFeedback,View=ReactNative.View;
-
-var Button=function Button(props){
-return(
-React.createElement(TouchableNativeFeedback,_extends({
-delayPressIn:0},
-props,{__source:{fileName:_jsxFileName,lineNumber:7}}),
-
-props.children));
-
-
-};
-
-module.exports=Button;
+var _extends=Object.assign||function(target){for(var i=1;i<arguments.length;i++){var source=arguments[i];for(var key in source){if(Object.prototype.hasOwnProperty.call(source,key)){target[key]=source[key];}}}return target;};var _jsxFileName="src/basic/Tabs/Button.android.js";var React=require("react");var ReactNative=require("react-native");var TouchableNativeFeedback=ReactNative.TouchableNativeFeedback,View=ReactNative.View;var Button=function Button(props){return React.createElement(TouchableNativeFeedback,_extends({delayPressIn:0},props,{__source:{fileName:_jsxFileName,lineNumber:7}}),props.children);};module.exports=Button;
 //# sourceMappingURL=Button.android.js.map
