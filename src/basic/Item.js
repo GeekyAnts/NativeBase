3d8c04f1f767e67b3abbb1d5c327bbff9b0bba62
Luciano Lima <beavess@gmail.com>
2018-07-18 05:37:59 -0300

    Fix floatingLabel cut in some Android devices

--- src/basic/Item.js
+++ src/basic/Item.js
@@ -167,7 +167,7 @@ class Item extends Component {
                 right: 0,
                 top: this.state.topAnim,
                 opacity: this.state.opacAnim,
-                paddingTop: Platform.OS === "ios" ? undefined : undefined,
+                paddingTop: Platform.OS === "ios" ? undefined : 2,
                 paddingBottom: Platform.OS === "ios" ? undefined : 12
               }}
             >
@@ -212,7 +212,7 @@ class Item extends Component {
             right: 0,
             top: this.state.topAnim,
             opacity: this.state.opacAnim,
-            paddingTop: Platform.OS === "ios" ? undefined : undefined,
+            paddingTop: Platform.OS === "ios" ? undefined : 2,
             paddingBottom: Platform.OS === "ios" ? undefined : 12
           }}
         >
