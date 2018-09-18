2e4d41483dd15e3f3e871b2c1a995c8d3a466fcb
Supriya Kalghatgi <supriya@Supriyas-Air.blr-office.geekyants.com>
2018-09-18 12:32:30 +0530

    Added Thumbnail support for FloatingLabel, transpiled, #2271

--- src/basic/Item.js
+++ src/basic/Item.js
@@ -10,6 +10,7 @@ import {
 import { Input } from "./Input";
 import { Label } from "./Label";
 import { Icon } from "./Icon";
+import { Thumbnail } from "./Thumbnail";
 
 import { connectStyle } from "native-base-shoutem-theme";
 import variables from "../theme/variables/platform";
@@ -141,6 +142,15 @@ class Item extends Component {
         return item;
       }
     });
+
+    let image = [];
+    let imageProps = {};
+    image = _.remove(childrenArray, item => {
+      if (item.type === Thumbnail) {
+        imageProps = item.props;
+        return item;
+      }
+    });
     if (this.props.floatingLabel && icon.length) {
       let isIcon = false;
       for (let i = 0; i < this.props.children.length; i++) {
@@ -197,6 +207,87 @@ class Item extends Component {
               onChangeText={text => {
                 this.setState({ text });
                 inputProps.onChangeText && inputProps.onChangeText(text);
+              }}
+            />
+          );
+        }
+      }
+    } else if (this.props.floatingLabel && image.length) {
+      let isImage = false;
+      for (let i = 0; i < this.props.children.length; i++) {
+        if (this.props.children[i].type.displayName === "Styled(Thumbnail)") {
+          isImage = true;
+          newChildren.push(
+            <Thumbnail
+              small
+              key={[i]}
+              {...this.props.children[i].props}
+              style={{
+                right: 10,
+                left: (i == this.props.children.length - 1)
+                  ? undefined
+                  : 0
+              }}
+            />
+          );
+        }
+        if (this.props.children[i].props.children) {
+          newChildren.push(
+            <Animated.View
+              key="float"
+              style={{
+                position: "absolute",
+                left:
+                  this.props.last && isImage
+                    ? 57
+                    : this.props.last
+                      ? 15
+                      : isImage
+                        ? 42
+                        : 0,
+                right: 0,
+                top: this.state.topAnim,
+                opacity: this.state.opacAnim,
+                paddingTop: Platform.OS === "ios" ? undefined : undefined,
+                paddingBottom: Platform.OS === "ios" ? undefined : 12
+              }}
+            >
+              <Label {...labelProps}>
+                {this.renderLabel(label, labelProps)}
+              </Label>
+            </Animated.View>
+          );
+          newChildren.push(
+            <Input
+              ref={c => (this._inputRef = c)}
+              key="l2"
+              {...inputProps}
+              onFocus={() => {
+                this.setState({ isFocused: true });
+                inputProps.onFocus && inputProps.onFocus();
+              }}
+              onBlur={() => {
+                inputProps.value
+                  ? this.setState({
+                    isFocused: true
+                  })
+                  : !this.state.text.length &&
+                  this.setState({ isFocused: false });
+                inputProps.onBlur && inputProps.onBlur();
+              }}
+              onChangeText={text => {
+                this.setState({ text });
+                inputProps.onChangeText && inputProps.onChangeText(text);
+              }}
+              style={{
+                left: this.props.last && isImage
+                  ? 10
+                  : this.props.last
+                    ? 4
+                    : isImage
+                      ? 10
+                      : 0,
+                marginRight: 12
               }}
             />
           );
