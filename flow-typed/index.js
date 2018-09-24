1fb273b5fc822058b27576a0477f5e1c587044a3
Supriya Kalghatgi <supriya@Supriyas-Air.blr-office.geekyants.com>
2018-09-24 15:10:46 +0530

    Flow support to ejected theme, transpiled, Fixed #2188

---
+++ flow-typed/index.js
@@ -0,0 +1,9 @@
+declare module 'react-native' {
+  declare module.exports: any;
+}
+declare module 'lodash' {
+  declare module.exports: any;
+}
+declare module 'color' {
+  declare module.exports: any;
+}
