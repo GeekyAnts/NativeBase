3bff9e92866717f1338d577cde68d8fac1f12f5e
Emanuele Pavanello <47756116+epavanello@users.noreply.github.com>
2019-07-18 12:49:23 +0200

    Update Icon.js (#2762)
    Allow Icon to inherit color

--- src/theme/components/Icon.js
+++ src/theme/components/Icon.js
@@ -5,7 +5,7 @@ import variable from './../variables/platform';
 export default (variables /* : * */ = variable) => {
   const iconTheme = {
     fontSize: variables.iconFontSize,
-    color: '#000'
+    color: variable.textColor
   };
 
   return iconTheme;
