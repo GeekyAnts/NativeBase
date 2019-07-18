b929255badf064b3a00eac2a8272581ca70dd483
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2019-07-18 16:06:08 +0530

    chore(): adds newwest chages from theme package

---
+++ dist/react-native.config.js
@@ -0,0 +1,2 @@
+module.exports={dependency:{assets:['Fonts']}};
+//# sourceMappingURL=react-native.config.js.map
--- dist/src/theme/components/Label.js
+++ dist/src/theme/components/Label.js
@@ -1,2 +1,2 @@
-Object.defineProperty(exports,"__esModule",{value:true});var _platform=require("./../variables/platform");var _platform2=_interopRequireDefault(_platform);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}exports.default=function(){var variables=arguments.length>0&&arguments[0]!==undefined?arguments[0]:_platform2.default;var labelTheme={".focused":{width:0},fontSize:17};return labelTheme;};
+Object.defineProperty(exports,"__esModule",{value:true});exports.default=function(){var labelTheme={'.focused':{width:0},fontSize:17};return labelTheme;};
 //# sourceMappingURL=Label.js.map
