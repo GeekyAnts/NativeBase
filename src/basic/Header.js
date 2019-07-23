9fcab56d599eca3f9b67bd86d4ad3cc43b3f8a9e
Plaban J Rajkhowa <plabanj@geekyants.com>
2019-07-23 18:30:55 +0530

    Refactor Header.js & fix Issue #2798 (#2803)

--- src/basic/Header.js
+++ src/basic/Header.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { View, StatusBar, ViewPropTypes, StyleSheet } from 'react-native';
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-unneeded-ternary */
+import { get } from 'lodash';
 import { connectStyle } from 'native-base-shoutem-theme';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { View, StatusBar, StyleSheet, ViewPropTypes } from 'react-native';
 
 import mapPropsToStyleNames from '../utils/mapPropsToStyleNames';
 import variable from '../theme/variables/platform';
@@ -10,96 +13,111 @@ class Header extends Component {
   static contextTypes = {
     theme: PropTypes.object
   };
+
   constructor(props) {
     super(props);
     this.state = {
       orientation: 'portrait'
     };
   }
+
   layoutChange(val) {
     const maxComp = Math.max(variable.deviceWidth, variable.deviceHeight);
+
     if (val.width >= maxComp) this.setState({ orientation: 'landscape' });
-    else {
-      this.setState({ orientation: 'portrait' });
-    }
+    else this.setState({ orientation: 'portrait' });
   }
+
   calculateHeight(mode, inSet) {
+    const { style } = this.props;
     let inset = null;
+
     if (inSet !== undefined) {
       inset = inSet;
     } else {
       inset = variable.Inset;
     }
+
     const InsetValues = mode === 'portrait' ? inset.portrait : inset.landscape;
     let oldHeight = null;
-    if (this.props.style.height !== undefined) {
-      oldHeight = this.props.style.height;
-    } else if (this.props.style[1]) {
-      oldHeight = this.props.style[1].height
-        ? this.props.style[1].height
-        : this.props.style[0].height;
+
+    if (style.height !== undefined) {
+      oldHeight = style.height;
+    } else if (style[1]) {
+      oldHeight = style[1].height ? style[1].height : style[0].height;
     } else {
-      oldHeight = this.props.style[0].height;
+      oldHeight = style[0].height;
     }
+
     const height = oldHeight + InsetValues.topInset;
+
     return height;
   }
+
   calculatePadder(mode, inSet) {
     let inset = null;
+
     if (inSet !== undefined) {
       inset = inSet;
     } else {
       inset = variable.Inset;
     }
+
     const InsetValues = mode === 'portrait' ? inset.portrait : inset.landscape;
     let topPadder = null;
     const style = StyleSheet.flatten(this.props.style);
+
     if (style.padding !== undefined || style.paddingTop !== undefined) {
       topPadder =
-        (style.paddingTop ? style.paddingTop : style.padding) +
+        (style.paddingTop ? style.paddingTop : get(style, 'padding', 0)) +
         InsetValues.topInset;
     } else {
       topPadder = InsetValues.topInset;
     }
+
     return topPadder;
   }
+
   render() {
+    const {
+      androidStatusBarColor,
+      iosBarStyle,
+      style,
+      transparent,
+      translucent
+    } = this.props;
+    const { orientation } = this.state;
     const variables = this.context.theme
       ? this.context.theme['@@shoutem.theme/themeStyle'].variables
       : variable;
     const platformStyle = variables.platformStyle;
+
     return (
       <View onLayout={e => this.layoutChange(e.nativeEvent.layout)}>
         <StatusBar
           backgroundColor={
-            this.props.androidStatusBarColor
-              ? this.props.androidStatusBarColor
+            androidStatusBarColor
+              ? androidStatusBarColor
               : variables.statusBarColor
           }
           barStyle={
-            this.props.iosBarStyle
-              ? this.props.iosBarStyle
+            iosBarStyle
+              ? iosBarStyle
               : platformStyle === 'material'
               ? 'light-content'
               : variables.iosStatusbar
           }
-          translucent={this.props.transparent ? true : this.props.translucent}
+          translucent={transparent ? true : translucent}
         />
         {variables.isIphoneX ? (
           <View
             ref={c => (this._root = c)}
             {...this.props}
             style={[
-              this.props.style,
+              style,
               {
-                height: this.calculateHeight(
-                  this.state.orientation,
-                  variables.Inset
-                ),
-                paddingTop: this.calculatePadder(
-                  this.state.orientation,
-                  variables.Inset
-                )
+                height: this.calculateHeight(orientation, variables.Inset),
+                paddingTop: this.calculatePadder(orientation, variables.Inset)
               }
             ]}
           />
