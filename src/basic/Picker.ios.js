181f175c4ef38e1bbae236d3ee66894d8db6e1f2
Plaban J Rajkhowa <plabanjr@gmail.com>
2019-08-16 15:48:04 +0530

    Fix #2856 (#2873)

--- src/basic/Picker.ios.js
+++ src/basic/Picker.ios.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import createReactClass from 'create-react-class';
 import { FlatList, Modal, Picker, View, ViewPropTypes } from 'react-native';
 import { connectStyle } from 'native-base-shoutem-theme';
-import _ from 'lodash';
+import { find, flatten, get, isArray } from 'lodash';
 
 import computeProps from '../utils/computeProps';
 import mapPropsToStyleNames from '../utils/mapPropsToStyleNames';
@@ -22,18 +22,25 @@ import { Body } from './Body';
 class PickerNB extends Component {
   static getDerivedStateFromProps(nextProps, prevState) {
     let nextDS;
+
     if (nextProps.children && !Array.isArray(nextProps.children)) {
       nextDS = [].concat(nextProps.children);
       // eslint-disable-next-line prefer-spread
     } else nextDS = [].concat.apply([], nextProps.children);
 
-    const item = _.find(
-      nextProps.children,
-      child => child.props.value === nextProps.selectedValue
-    );
+    let item;
+
+    if (isArray(nextProps.children)) {
+      item = find(
+        flatten(nextProps.children),
+        child => child.props.value === nextProps.selectedValue
+      );
+    } else {
+      item = nextProps.children;
+    }
 
     const currentLabel = prevState.currentLabel;
-    const nextLabel = _.get(item, 'props.label');
+    const nextLabel = get(item, 'props.label');
     const currentDS = prevState.dataSource;
 
     if (currentLabel !== nextLabel) {
@@ -69,15 +76,15 @@ class PickerNB extends Component {
 
   getLabel(props) {
     const children = this.getChildren(props.children);
-    const item = _.find(
+    const item = find(
       children,
       child => child.props.value === props.selectedValue
     );
-    return _.get(item, 'props.label');
+    return get(item, 'props.label');
   }
 
   getSelectedItem() {
-    return _.find(
+    return find(
       this.props.children,
       child => child.props.value === this.props.selectedValue
     );
