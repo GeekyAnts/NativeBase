93b0fc4df002865fe759b15c0029ccec72c096b9
Bodhi Hu <huhuaishun@icloud.com>
2019-12-03 16:11:30 +0800

    src/theme/variables/platform.js : titleFontColor : web support

--- src/theme/variables/platform.js
+++ src/theme/variables/platform.js
@@ -303,7 +303,8 @@ export default {
   titleFontSize: platform === PLATFORM.IOS ? 17 : 19,
   subTitleFontSize: platform === PLATFORM.IOS ? 11 : 14,
   subtitleColor: platform === PLATFORM.IOS ? '#8e8e93' : '#FFF',
-  titleFontColor: platform === PLATFORM.IOS ? '#000' : '#FFF',
+  titleFontColor:
+    platform === PLATFORM.IOS || platform === PLATFORM.WEB ? '#000' : '#FFF',
 
   // Other
   borderRadiusBase: platform === PLATFORM.IOS ? 5 : 2,
