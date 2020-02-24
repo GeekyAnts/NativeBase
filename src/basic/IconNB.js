1c9966917b707d97542adde391dd5b87d764e465
Bodhi Hu <huhuaishun@icloud.com>
2020-02-24 14:59:50 +0800

    remove evil icons

--- src/basic/IconNB.js
+++ src/basic/IconNB.js
@@ -4,7 +4,6 @@ import { connectStyle } from 'native-base-shoutem-theme';
 import { get } from 'lodash';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Entypo from 'react-native-vector-icons/Entypo';
-import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import Feather from 'react-native-vector-icons/Feather';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
@@ -43,50 +42,51 @@ class IconNB extends Component {
         .iconFamily;
     }
     switch (iconType) {
-      case 'AntDesign':
-        this.Icon = AntDesign;
-        break;
-      case 'Entypo':
-        this.Icon = Entypo;
-        break;
-      case 'EvilIcons':
-        this.Icon = EvilIcons;
-        break;
-      case 'Feather':
-        this.Icon = Feather;
-        break;
-      case 'FontAwesome':
-        this.Icon = FontAwesome;
-        break;
-      case 'FontAwesome5':
-        this.Icon = FontAwesome5;
-        break;
-      case 'Fontisto':
-        this.Icon = Fontisto;
-        break;
-      case 'Foundation':
-        this.Icon = Foundation;
-        break;
-      case 'Ionicons':
-        this.Icon = Ionicons;
-        break;
-      case 'MaterialCommunityIcons':
-        this.Icon = MaterialCommunityIcons;
-        break;
-      case 'MaterialIcons':
-        this.Icon = MaterialIcons;
-        break;
-      case 'Octicons':
-        this.Icon = Octicons;
-        break;
-      case 'SimpleLineIcons':
-        this.Icon = SimpleLineIcons;
-        break;
-      case 'Zocial':
-        this.Icon = Zocial;
-        break;
-      default:
-        this.Icon = Ionicons;
+    case 'AntDesign':
+      this.Icon = AntDesign;
+      break;
+    case 'Entypo':
+      this.Icon = Entypo;
+      break;
+    case 'Feather':
+      this.Icon = Feather;
+      break;
+    case 'FontAwesome':
+      this.Icon = FontAwesome;
+      break;
+    case 'FontAwesome5':
+      this.Icon = FontAwesome5;
+      break;
+    case 'Foundation':
+      this.Icon = Foundation;
+      break;
+    case 'Ionicons':
+      this.Icon = Ionicons;
+      break;
+    case 'MaterialCommunityIcons':
+      this.Icon = MaterialCommunityIcons;
+      break;
+    case 'MaterialIcons':
+      this.Icon = MaterialIcons;
+      break;
+    case 'Octicons':
+      this.Icon = Octicons;
+      break;
+    case 'SimpleLineIcons':
+      this.Icon = SimpleLineIcons;
+      break;
+    case 'Zocial':
+      this.Icon = Zocial;
+      break;
+    default:
+      this.Icon = Ionicons;
+    }
+  }
+
+  // eslint-disable-next-line camelcase
+  UNSAFE_componentWillUpdate(nextProps) {
+    if (nextProps.type && this.props.type !== nextProps.type) {
+      this.setIcon(nextProps.type);
     }
   }
 
@@ -99,7 +99,6 @@ IconNB.propTypes = {
   type: PropTypes.oneOf([
     'AntDesign',
     'Entypo',
-    'EvilIcons',
     'Feather',
     'FontAwesome',
     'FontAwesome5',
