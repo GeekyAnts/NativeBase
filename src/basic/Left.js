883da430d25b0d365e508068eb41df33eaa078f1
anuskaleem <anuskaleem@gmail.com>
2020-02-25 12:12:02 +0500

    Update Left.js

--- src/basic/Left.js
+++ src/basic/Left.js
@@ -7,7 +7,7 @@ import mapPropsToStyleNames from '../utils/mapPropsToStyleNames';
 
 class Left extends Component {
   render() {
-    return <View ref={c => (this._root = c)} {...this.props} />;
+    return <View testID="leftViewID" ref={c => (this._root = c)} {...this.props} />;
   }
 }
 
