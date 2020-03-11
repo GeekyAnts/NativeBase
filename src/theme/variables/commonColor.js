b1bbfd4f6ce77c1a44004e2be78fc1146e67b5cf
Hany Kumar <hanykumar@Hanys-Air.blr-office.geekyants.com>
2020-03-11 15:30:34 +0530

    bug fix 3071

--- src/theme/variables/commonColor.js
+++ src/theme/variables/commonColor.js
@@ -136,7 +136,7 @@ export default {
   brandDanger: '#d9534f',
   brandWarning: '#f0ad4e',
   brandDark: '#000',
-  brandLight: '#f4f4f4',
+  brandLight: '#a9a9a9',
 
   // Container
   containerBgColor: '#fff',
