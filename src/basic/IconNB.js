90dff6724f10190813ad6b5e3b21994521753402
Hany Kumar <kumar.hany1994@gmail.com>
2020-03-12 18:10:34 +0530

    bug fix 3063 (#3091)
    Co-authored-by: Hany Kumar <hanykumar@Hanys-Air.blr-office.geekyants.com>

--- src/basic/IconNB.js
+++ src/basic/IconNB.js
@@ -8,6 +8,7 @@ import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import Feather from 'react-native-vector-icons/Feather';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 import Foundation from 'react-native-vector-icons/Foundation';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -59,6 +60,9 @@ class IconNB extends Component {
         break;
       case 'FontAwesome5':
         this.Icon = FontAwesome5;
+        break;
+      case 'Fontisto':
+        this.Icon = Fontisto;
         break;
       case 'Foundation':
         this.Icon = Foundation;
