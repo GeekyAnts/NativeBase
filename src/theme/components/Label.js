c62414eb4573c12863b31fea53abcb3d299ff446
Bodhi Hu <huhuaishun@icloud.com>
2020-04-21 11:36:01 +0800

    Label: set fontFamily

--- src/theme/components/Label.js
+++ src/theme/components/Label.js
@@ -1,11 +1,14 @@
 // @flow
 
-export default () => {
+import variable from './../variables/platform';
+
+export default (variables /* : * */ = variable) => {
   const labelTheme = {
     '.focused': {
       width: 0
     },
-    fontSize: 17
+    fontSize: 17,
+    fontFamily: variables.fontFamily,
   };
 
   return labelTheme;
