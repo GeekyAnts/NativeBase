28c84a51f1248b44b36c9fdb8e82aaf0d0b83f63
abdelhaksam <47641482+abdelhaksam@users.noreply.github.com>
2020-05-23 09:05:05 +0100

    Fixed vertical alignment of badge for android (#3151)

--- src/theme/components/Badge.js
+++ src/theme/components/Badge.js
@@ -31,7 +31,7 @@ export default (variables /* : * */ = variable) => {
     padding: variables.badgePadding,
     paddingHorizontal: 6,
     alignSelf: 'flex-start',
-    justifyContent: variables.platform === PLATFORM.IOS ? 'center' : undefined,
+    justifyContent: 'center',
     borderRadius: 13.5,
     height: 27
   };
