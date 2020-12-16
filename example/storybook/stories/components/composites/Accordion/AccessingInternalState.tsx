5ce2b05c6c55dd273a9cbaf0830599baa7a254d0
Ankur Kedia <theankurkedia@gmail.com>
2020-12-16 15:21:36 +0530

    test commit

--- example/storybook/stories/components/composites/Accordion/AccessingInternalState.tsx
+++ example/storybook/stories/components/composites/Accordion/AccessingInternalState.tsx
@@ -18,7 +18,7 @@ export default function () {
             <AccordionIcon />
           </AccordionButton>
           <AccordionPanel>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            Lorem ipsum dolor sit amet, consectetur adipiscig elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
