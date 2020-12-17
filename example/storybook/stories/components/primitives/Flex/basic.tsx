f57be55a0e9db5a40e2b179bb90b03ce96b89813
Ankur Kedia <theankurkedia@gmail.com>
2020-12-17 13:00:13 +0530

    Fixes for flex example

--- example/storybook/stories/components/primitives/Flex/basic.tsx
+++ example/storybook/stories/components/primitives/Flex/basic.tsx
@@ -3,16 +3,16 @@ import { Flex, Text, Center, Box } from 'native-base';
 
 export default function () {
   return (
-    <Flex>
-      <Center w="100px" bg="green.500">
+    <Flex height="500px" width="100px">
+      <Center w="100px" h="100px" bg="green.500">
         <Text>Box 1</Text>
       </Center>
       <Center bg="blue.300" h="100px" w="100px">
         <Text>Box 2</Text>
       </Center>
-      <Box flex={1} bg="tomato">
+      <Center flex={1} bg="tomato">
         Box 3
-      </Box>
+      </Center>
     </Flex>
   );
 }
