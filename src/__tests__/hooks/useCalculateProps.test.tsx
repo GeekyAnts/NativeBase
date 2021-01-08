2e9e17770edb58ca6ef4900e89c3160f032785f0
Ankur Kedia <theankurkedia@gmail.com>
2021-01-08 16:26:47 +0530

    Added unit test for usePropsConfig and useCalculateProps

--- src/__tests__/hooks/useCalculateProps.test.tsx
+++ src/__tests__/hooks/useCalculateProps.test.tsx
@@ -10,7 +10,7 @@ describe('useCalculateProps', () => {
     setColorMode: () => {},
   };
 
-  test('empty', () => {
+  test('No component theme + no props', () => {
     const wrapper = ({ children }: any) => (
       <NativeBaseProvider>{children}</NativeBaseProvider>
     );
@@ -19,5 +19,53 @@ describe('useCalculateProps', () => {
       { wrapper }
     );
     expect(result.current).toEqual({ style: {} });
+  });
+
+  test('Only component theme(Badge) + no props', () => {
+    const wrapper = ({ children }: any) => (
+      <NativeBaseProvider>{children}</NativeBaseProvider>
+    );
+    const componentTheme = (theme as any)['components']['Badge'];
+    const { result } = renderHook(
+      () => useCalculateProps(theme, colorModeProps, componentTheme, {}),
+      { wrapper }
+    );
+    expect(result.current).toEqual({
+      px: 1,
+      py: 1,
+      textTransform: 'uppercase',
+      fontSize: 'xs',
+      borderRadius: 'sm',
+      fontWeight: 'bold',
+      bg: 'gray.100',
+      color: 'gray.700',
+      borderWidth: 1,
+      borderColor: 'transparent',
+      style: {},
+    });
+  });
+
+  test('Component theme(Badge) + some props', () => {
+    const wrapper = ({ children }: any) => (
+      <NativeBaseProvider>{children}</NativeBaseProvider>
+    );
+    const componentTheme = (theme as any)['components']['Badge'];
+    const { result } = renderHook(
+      () => useCalculateProps(theme, colorModeProps, componentTheme, { py: 3 }),
+      { wrapper }
+    );
+    expect(result.current).toEqual({
+      px: 1,
+      py: 3,
+      textTransform: 'uppercase',
+      fontSize: 'xs',
+      borderRadius: 'sm',
+      fontWeight: 'bold',
+      bg: 'gray.100',
+      color: 'gray.700',
+      borderWidth: 1,
+      borderColor: 'transparent',
+      style: {},
+    });
   });
 });
--- src/__tests__/hooks/usePropsConfig.test.tsx
+++ src/__tests__/hooks/usePropsConfig.test.tsx
@@ -1,34 +1,58 @@
-// import { usePropsConfig } from './../../hooks/usePropsConfig';
+import { usePropsConfig } from './../../hooks/usePropsConfig';
+import { NativeBaseProvider } from 'native-base';
+import React from 'react';
+import { renderHook } from '@testing-library/react-hooks';
+
 describe('usePropsConfig', () => {
-  test('empty', () => {
-    // expect(usePropsConfig({}, {})).toEqual({ style: {} });
+  test('Box component + no props', () => {
+    const wrapper = ({ children }: any) => (
+      <NativeBaseProvider>{children}</NativeBaseProvider>
+    );
+    const { result } = renderHook(() => usePropsConfig('Box', {}), { wrapper });
+    expect(result.current).toEqual({ style: {} });
   });
-  // test('basic', () => {
-  //   expect(
-  //     filterShadowProps(
-  //       {
-  //         top: 10,
-  //         shadowColor: '#FFF',
-  //         shadowOffset: {
-  //           width: 0,
-  //           height: 1,
-  //         },
-  //         shadowOpacity: 0.18,
-  //         shadowRadius: 1.0,
-  //       },
-  //       {}
-  //     )
-  //   ).toEqual({
-  //     top: 10,
-  //     style: {
-  //       shadowColor: '#FFF',
-  //       shadowOffset: {
-  //         width: 0,
-  //         height: 1,
-  //       },
-  //       shadowOpacity: 0.18,
-  //       shadowRadius: 1.0,
-  //     },
-  //   });
-  // });
+
+  test('Badge Component + no props', () => {
+    const wrapper = ({ children }: any) => (
+      <NativeBaseProvider>{children}</NativeBaseProvider>
+    );
+    const { result } = renderHook(() => usePropsConfig('Badge', {}), {
+      wrapper,
+    });
+    expect(result.current).toEqual({
+      px: 1,
+      py: 1,
+      textTransform: 'uppercase',
+      fontSize: 'xs',
+      borderRadius: 'sm',
+      fontWeight: 'bold',
+      bg: 'gray.100',
+      color: 'gray.700',
+      borderWidth: 1,
+      borderColor: 'transparent',
+      style: {},
+    });
+  });
+
+  test('Badge + some props', () => {
+    const wrapper = ({ children }: any) => (
+      <NativeBaseProvider>{children}</NativeBaseProvider>
+    );
+    const { result } = renderHook(() => usePropsConfig('Badge', { py: 3 }), {
+      wrapper,
+    });
+    expect(result.current).toEqual({
+      px: 1,
+      py: 3,
+      textTransform: 'uppercase',
+      fontSize: 'xs',
+      borderRadius: 'sm',
+      fontWeight: 'bold',
+      bg: 'gray.100',
+      color: 'gray.700',
+      borderWidth: 1,
+      borderColor: 'transparent',
+      style: {},
+    });
+  });
 });
