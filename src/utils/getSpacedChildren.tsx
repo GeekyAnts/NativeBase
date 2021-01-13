848145a361d79348ed566942d1e14cc2e6115c6b
Rohit Singh <rohits@geekyants.com>
2021-01-13 17:18:40 +0530

    fix: stack mt issue and custom props not getting spaced

--- src/utils/getSpacedChildren.tsx
+++ src/utils/getSpacedChildren.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { Box } from 'native-base';
 
-type SpaceType = 'gutter' | '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+type SpaceType =
+  | 'gutter'
+  | '2xs'
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | '2xl'
+  | number;
 
 export default (
   children: JSX.Element[] | JSX.Element,
-  space: number | undefined | SpaceType,
+  space: undefined | SpaceType,
   axis: 'X' | 'Y',
   reverse: string,
   divider: JSX.Element | undefined
-) => {
+): any => {
   let childrenArray = React.Children.toArray(children);
   const orientation = axis === 'X' ? 'vertical' : 'horizontal';
   if (divider) {
@@ -26,6 +36,28 @@ export default (
   /*
   | Separate the trailing (not first) children from the children array
   */
+
+  /*
+  | Wrapping Children with a Box so that any custom component can also work properly placed inside a stack
+  */
+
+  childrenArray = childrenArray.map((child: any, index: number) => {
+    /*
+    | wrapperProps takes child's mt , marginTop , ml or marginLeft and add that to wrapper.
+    */
+    let wrapperProps: object = {};
+    if (child.props.ml !== undefined || child.props.marginLeft !== undefined) {
+      wrapperProps = { ...{ ml: child.props.ml || child.props.marginLeft } };
+    }
+    if (child.props.mt !== undefined || child.props.marginTop !== undefined) {
+      wrapperProps = { ...{ mt: child.props.mt || child.props.marginTop } };
+    }
+
+    return React.cloneElement(<Box>{child}</Box>, {
+      key: `stack-wrapper-${index}`,
+      ...wrapperProps,
+    });
+  });
   const trailingChildren =
     reverse === 'reverse' ? childrenArray.slice(0, -1) : childrenArray.slice(1);
   /*
@@ -85,8 +117,8 @@ export default (
           child,
           {
             key: `reverse-spaced-child-${index}`,
-            ...child.props,
             ...marginProp,
+            ...child.props,
           },
           child.props.children
         );
@@ -101,7 +133,7 @@ export default (
       (child: any, index: number) => {
         return React.cloneElement(
           child,
-          { key: `spaced-child-${index}`, ...child.props, ...marginProp },
+          { key: `spaced-child-${index}`, ...marginProp, ...child.props },
           child.props.children
         );
       }
