83911b985903e32ac03396fd941a1a21125f3698
Ankur Kedia <theankurkedia@gmail.com>
2021-01-21 15:38:44 +0530

    Fixes for HStack

--- src/components/primitives/Stack/index.tsx
+++ src/components/primitives/Stack/index.tsx
@@ -14,30 +14,28 @@ export type IStackProps = IViewProps & {
   reversed?: boolean;
 };
 
-const Stack = React.memo(
-  (props: IStackProps & { direction?: 'column' | 'row' }) => {
-    const {
-      space,
-      children,
-      divider,
-      reversed,
-      direction,
-      ...remainingProps
-    } = props;
+const Stack = (props: IStackProps & { direction?: 'column' | 'row' }) => {
+  const {
+    space,
+    children,
+    divider,
+    reversed,
+    direction,
+    ...remainingProps
+  } = props;
 
-    return (
-      <StyledStack flexDirection={direction} {...remainingProps}>
-        {getSpacedChildren(
-          children,
-          space,
-          direction === 'row' ? 'X' : 'Y',
-          reversed ? 'reverse' : 'normal',
-          divider
-        )}
-      </StyledStack>
-    );
-  }
-);
+  return (
+    <StyledStack flexDirection={direction} {...remainingProps}>
+      {getSpacedChildren(
+        children,
+        space,
+        direction === 'row' ? 'X' : 'Y',
+        reversed ? 'reverse' : 'normal',
+        divider
+      )}
+    </StyledStack>
+  );
+};
 
 export const VStack = Stack;
 
