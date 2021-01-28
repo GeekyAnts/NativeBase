e9822eb578266239c23559f5c301ea2dfc6bba8e
Ankur Kedia <theankurkedia@gmail.com>
2021-01-28 17:52:12 +0530

    Added babel preset react

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:metro-react-native-babel-preset', '@babel/preset-react'],
   plugins:
     process.env.NODE_ENV === 'production' ? ['transform-remove-console'] : [],
 };
