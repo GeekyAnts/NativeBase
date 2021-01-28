5185aafd0c4210c4f29904214d38a94ba316ab18
Meenu Makkar <meenumakkar@Meenus-MacBook-Pro.local>
2021-01-28 17:52:15 +0530

    feat: modal default redesign

--- example/storybook/stories/components/composites/Modal/Basic.tsx
+++ example/storybook/stories/components/composites/Modal/Basic.tsx
@@ -16,7 +16,6 @@ export default function () {
   return (
     <>
       <Modal
-        isCentered
         isOpen={modalVisible}
         onClose={setModalVisible}
         overlayVisible={true}
@@ -30,19 +29,25 @@ export default function () {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quasi
             cupiditate expedita, ipsa corporis officia totam similique delectus!
             Debitis esse, ea blanditiis iste enim iure at odit fugiat autem.
-            Accusamus?
+            Accusamus? Lorem Ipsum is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum has been the industry’s standard
+            dummy text ever since the 1500s, when an unknown printer took a
+            galley of type and scrambled it to make a type specimen book. It has
+            survived not only five centuries, but also the leap into electronic
+            typesetting, remaining essentially unchanged. It was popularised in
             <Input mt={4} placeholder="Lorem ipsum dolor sit" />
           </ModalBody>
           <ModalFooter>
             <Button colorScheme="blue" mr={1}>
-              Save
+              Done
             </Button>
             <Button
+              colorScheme="secondary"
               onPress={() => {
                 setModalVisible(!modalVisible);
               }}
             >
-              Close
+              Cancel
             </Button>
           </ModalFooter>
         </ModalContent>
