677ca2d17b5552cc63edd7be84b402ead36c8b69
Rohit Singh <rohit99.ind@gmail.com>
2021-02-11 19:10:25 +0530

    fix: add forwardref in fab

--- src/components/composites/Fab/Fab.tsx
+++ src/components/composites/Fab/Fab.tsx
@@ -4,7 +4,7 @@ import Button from '../../primitives/Button';
 import { HStack } from '../../primitives/Stack';
 import type { IFabProps } from './types';
 
-const Fab = (props: IFabProps) => {
+const Fab = (props: IFabProps, ref: any) => {
   const { label, icon, placement, ...newProps } = omitUndefined(props);
 
   const placementProps: any = {
@@ -22,6 +22,7 @@ const Fab = (props: IFabProps) => {
       p={2}
       {...placementProps[placement]}
       {...newProps}
+      ref={ref}
     >
       {icon && label ? (
         <HStack space={1} justifyContent="center" alignItems="center">
@@ -35,4 +36,4 @@ const Fab = (props: IFabProps) => {
   );
 };
 
-export default React.memo(Fab);
+export default React.memo(React.forwardRef<any, IFabProps>(Fab));
