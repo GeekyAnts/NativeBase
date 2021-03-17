6ad875286b8b5cb9604d48fd9740d2a653c9a04f
Ankur Kedia <theankurkedia@gmail.com>
2021-03-17 15:23:27 +0530

    Fixes for box composition example

--- example/storybook/stories/components/primitives/Box/composition-shoes-card.tsx
+++ example/storybook/stories/components/primitives/Box/composition-shoes-card.tsx
@@ -1,60 +1,39 @@
 import React from 'react';
-import {
-  Box,
-  Stack,
-  Image,
-  Heading,
-  Icon,
-  Text,
-  Flex,
-  IconButton,
-} from 'native-base';
-export const CompositionShoesCard = () => {
+import { Box, Text, Image, Heading, Stack } from 'native-base';
+export default () => {
   return (
-    <Flex
-      w={64}
-      rounded={12}
+    <Box
+      flexDirection={{ base: 'column', md: 'row' }}
       shadow={4}
-      borderColor="blueGray.400"
+      rounded="xl"
       overflow="hidden"
     >
-      <Box w="100%" h={40} overflow="hidden">
+      <Box width={{ md: 24 }} height={{ base: 32, md: '100%' }}>
         <Image
-          alt="shoes"
           source={{
             uri:
               'https://static.nike.com/a/images/f_auto/dpr_2.0/w_1328,c_limit/b56d1e9b-3861-4c89-995d-b8fb6240a762/nike-just-do-it.jpg',
           }}
+          alt="Shoes"
         />
       </Box>
-      <Stack h={32} p={5} space={2} justifyContent="space-between">
-        <Text fontSize="lg" color="blueGray.400">
-          Sneakers
+      <Stack p={3} space={2} minW={32}>
+        <Text fontSize="xs" color="red.400" fontWeight="semibold">
+          Just In
         </Text>
         <Stack space={1}>
-          <Heading size="lg">Jordan MA2</Heading>
-          <Text fontSize="xl" fontWeight="medium" color="blueGray.600">
-            $ 150
+          <Heading size="sm">Jordan MA2</Heading>
+          <Text fontWeight="medium" color="blueGray.600">
+            Older Kids' Shoe
+          </Text>
+          <Text fontWeight="medium" color="blueGray.600">
+            2 colors
           </Text>
         </Stack>
+        <Text fontSize="md" fontWeight="semibold" color="blueGray.600">
+          $ 150
+        </Text>
       </Stack>
-      <IconButton
-        position="absolute"
-        mt={32}
-        right={4}
-        w={16}
-        h={16}
-        rounded="full"
-        bg="orange.500"
-        icon={<Icon size={6} color="white" name="heart-o" type="FontAwesome" />}
-      />
-      <Icon
-        position="absolute"
-        mt={4}
-        right={4}
-        name="more-horiz"
-        color="white"
-      />
-    </Flex>
+    </Box>
   );
 };
--- example/storybook/stories/components/primitives/Box/composition.tsx
+++ example/storybook/stories/components/primitives/Box/composition.tsx
@@ -1,74 +1,82 @@
 import React from 'react';
-import { Box, Heading, Icon, AspectRatio, Image, Text } from 'native-base';
+import {
+  Box,
+  Heading,
+  Icon,
+  AspectRatio,
+  Image,
+  Text,
+  Center,
+  HStack,
+  Stack,
+} from 'native-base';
 export const Composition = () => {
   return (
-    <Box width={72} bg="gray.50" p={2}>
-      <Box bg="white" shadow={1}>
-        <Box>
-          <AspectRatio ratio={16 / 9}>
-            <Image
-              source={{
-                uri:
-                  'https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg',
-              }}
-              alt="image"
-            />
-          </AspectRatio>
-          <Box
-            bg="red.500"
-            _text={{ color: 'white', fontWeight: '700', fontSize: 'xs' }}
-            position="absolute"
-            bottom={0}
-            px={2}
-            py={1}
-          >
-            PHOTOS
-          </Box>
-          <Box
-            p={2}
-            rounded="full"
-            justifyContent="center"
-            alignItems="center"
-            bg="red.500"
-            boxSize={10}
-            position="absolute"
-            right={0}
-            m={2}
-            _text={{
-              color: 'white',
-              textAlign: 'center',
-              fontWeight: '700',
-              fontSize: '8',
+    <Box width={72} bg="gray.50" shadow={4}>
+      <Box>
+        <AspectRatio ratio={16 / 9}>
+          <Image
+            source={{
+              uri:
+                'https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg',
             }}
-          >
-            27 MAR
-          </Box>
-        </Box>
-        <Box p={4}>
+            alt="image"
+          />
+        </AspectRatio>
+        <Center
+          bg="red.500"
+          _text={{ color: 'white', fontWeight: '700', fontSize: 'xs' }}
+          position="absolute"
+          bottom={0}
+          px={2}
+          py={1}
+        >
+          PHOTOS
+        </Center>
+        <Center
+          p={1}
+          rounded="full"
+          bg="red.500"
+          boxSize={10}
+          position="absolute"
+          right={0}
+          m={2}
+          _text={{
+            color: 'white',
+            textAlign: 'center',
+            fontWeight: '700',
+            fontSize: 'xs',
+          }}
+        >
+          27 MAR
+        </Center>
+      </Box>
+      <Stack p={4} space={3}>
+        <Stack space={2}>
           <Heading size="md">Life in the Garden City</Heading>
-          <Heading size="sm" mt={2} color="red.500" fontWeight="500">
+          <Heading size="sm" color="red.500" fontWeight="500">
             The Silicon Valley of India.
           </Heading>
-          <Text mt={4} lineHeight={6} fontWeight={400}>
-            Bengaluru (also called Bangalore) is the center of India's high-tech
-            industry. The city is also known for its parks and nightlife.
-          </Text>
-          <Box flexDirection="row" mt={4}>
-            <Box flexDirection="row" alignItems="center">
-              <Icon name="access-time" color="gray.500" />
-              <Text ml={1} color="gray.500" fontWeight="500">
-                6 mins ago
-              </Text>
-            </Box>
-            <Box ml={4} flexDirection="row" alignItems="center">
-              <Icon name="ios-chatbubbles" type="Ionicons" color="gray.500" />
-              <Text ml={1} color="gray.500" fontWeight="500">
-                39 comments
-              </Text>
-            </Box>
-          </Box>
-        </Box>
-      </Box>
+        </Stack>
+        <Text lineHeight={6} fontWeight={400}>
+          Bengaluru (also called Bangalore) is the center of India's high-tech
+          industry. The city is also known for its parks and nightlife.
+        </Text>
+        <HStack alignItems="center" space={4} justifyContent="space-between">
+          <HStack alignItems="center">
+            <Icon name="access-time" color="gray.500" />
+            <Text ml={1} color="gray.500" fontWeight="500">
+              6 mins ago
+            </Text>
+          </HStack>
+          <HStack alignItems="center">
+            <Icon name="ios-chatbubbles" type="Ionicons" color="gray.500" />
+            <Text ml={1} color="gray.500" fontWeight="500">
+              39 comments
+            </Text>
+          </HStack>
+        </HStack>
+      </Stack>
     </Box>
   );
 };
