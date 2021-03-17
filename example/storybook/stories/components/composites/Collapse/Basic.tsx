f0f754f62d8c0f9930094c8f02308d7cc841ad27
Rohit Singh <rohit99.ind@gmail.com>
2021-03-17 12:03:35 +0530

    fix: collapse examples and collapse web duration bug

--- example/storybook/stories/components/composites/Collapse/Basic.tsx
+++ example/storybook/stories/components/composites/Collapse/Basic.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import { Collapse, Stack, Heading, Button, Box } from 'native-base';
+import { Collapse, Stack, Heading, Button, Text } from 'native-base';
 export default function () {
   const [show, setShow] = React.useState(false);
   const handleToggle = () => setShow(!show);
   return (
-    <Stack space={4}>
+    <Stack space={4} mx={8}>
       <Heading>Default Collapse</Heading>
       <Collapse isOpen={show}>
         Candy canes macaroon croissant carrot cake soufflé toffee topping
         pastry. Brownie lollipop brownie sesame snaps. Pastry gingerbread
         biscuit sweet I love macaroon bear claw fruitcake oat cake. Jelly sweet
         roll I love. Pastry pastry powder pie lemon drops chocolate topping
-        dessert. Gummi bears ice cream I love cake chocolate bar jujubes powder.
-        Sugar plum pie jujubes jelly cotton candy dragée chocolate bar liquorice
-        sugar plum. I love caramels bonbon macaroon chocolate biscuit donut
-        tart.
-        <Box p={8} bg="black" />
+        dessert.
       </Collapse>
-      <Button size="sm" onPress={handleToggle}>
-        Show {show ? 'Less' : 'More'}
+      <Button size="sm" colorScheme="emerald" onPress={handleToggle}>
+        <Text color="white">Show {show ? 'Less' : 'More'}</Text>
       </Button>
     </Stack>
   );
