985ffa9273d72e0810a0cc238ac45b0cce26bc08
MD-REHMAN <Rehman.2468@gmail.com>
2021-03-23 19:40:33 +0530

    fix: vertical divider width fix

--- src/theme/components/divider.ts
+++ src/theme/components/divider.ts
@@ -1,8 +1,10 @@
 import { mode } from '../tools';
 
 function baseStyle(props: Record<string, any>) {
+  const { orientation } = props;
   return {
-    borderTopWidth: 1,
+    borderTopWidth: orientation === 'vertical' ? 0 : 1,
+    borderLeftWidth: orientation === 'vertical' ? 1 : 0,
     bg: mode('muted.600', 'muted.200')(props),
   };
 }
