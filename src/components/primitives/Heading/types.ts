f9cac13902ee43f4981028deab1fd3d0a23844f7
MD-REHMAN <Rehman.2468@gmail.com>
2021-03-23 18:35:39 +0530

    fix: updated header and text component

--- src/components/primitives/Heading/types.ts
+++ src/components/primitives/Heading/types.ts
@@ -1,40 +1,6 @@
-import type { TextStyle } from 'react-native';
-import type {
-  BorderProps,
-  ColorProps,
-  FlexboxProps,
-  LayoutProps,
-  PositionProps,
-  SpaceProps,
-  TypographyProps,
-} from 'styled-system';
-import type {
-  customBorderProps,
-  customBackgroundProps,
-  customOutlineProps,
-  customLayoutProps,
-  customExtraProps,
-  customShadowProps,
-  customTypographyProps,
-  customPositionProps,
-} from '../../../utils/customProps';
+import type { ITextProps } from '../Text';
 
-export type IHeadingProps = ColorProps &
-  SpaceProps &
-  LayoutProps &
-  FlexboxProps &
-  TypographyProps &
-  PositionProps &
-  customPositionProps &
-  customBorderProps &
-  customExtraProps &
-  customOutlineProps &
-  customShadowProps &
-  customLayoutProps &
-  customBackgroundProps &
-  customTypographyProps &
-  BorderProps & {
-    style?: TextStyle;
-    children?: string | JSX.Element[] | JSX.Element;
-    isTruncated?: any;
-  };
+type sizes = ['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl'];
+export type IHeadingProps = ITextProps & {
+  size?: sizes | number | string;
+};
