ba772aafe58dd66f66aa08fad965d073d438d366
MD-REHMAN <Rehman.2468@gmail.com>
2021-03-23 20:09:18 +0530

    fix: heading type fix

--- src/components/primitives/Heading/types.ts
+++ src/components/primitives/Heading/types.ts
@@ -1,6 +1,5 @@
 import type { ITextProps } from '../Text';
 
-type sizes = ['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl'];
 export type IHeadingProps = ITextProps & {
-  size?: sizes | number | string;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | number | string;
 };
