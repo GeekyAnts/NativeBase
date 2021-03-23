14d1adbe3116b0c2e29293d0bdbf5d09b51cffd1
Rohit Singh <rohit99.ind@gmail.com>
2021-03-23 16:52:46 +0530

    fix: alert usage example

--- example/storybook/stories/components/composites/Alert/usage.tsx
+++ example/storybook/stories/components/composites/Alert/usage.tsx
@@ -3,7 +3,7 @@ import { Alert, Box } from 'native-base';
 export default function () {
   return (
     <Box mx={3}>
-      <Alert status="warning" colorScheme="danger">
+      <Alert>
         <Alert.Icon />
         <Alert.Title>Alert Title</Alert.Title>
         <Alert.Description>description goes here</Alert.Description>
