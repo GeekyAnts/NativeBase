ba1cc9cfed008606401ff19f1579bc0a54edbfde
Rohit Singh <rohit99.ind@gmail.com>
2021-03-24 12:04:50 +0530

    fix: progress example update

--- example/storybook/stories/components/composites/Progress/ColorScheme.tsx
+++ example/storybook/stories/components/composites/Progress/ColorScheme.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Box, Progress, VStack } from 'native-base';
+import { Box, Progress, VStack, Heading } from 'native-base';
 
 export default function () {
   return (
     <Box w="90%">
       <VStack space="md">
+        <Heading textAlign="center" mb={8}>
+          Progress ColorScheme
+        </Heading>
         <Progress colorScheme="primary" value={35} />
         <Progress colorScheme="secondary" value={45} />
         <Progress colorScheme="emerald" value={55} />
