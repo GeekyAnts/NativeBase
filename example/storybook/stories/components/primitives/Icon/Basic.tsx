8a4748e929f9775d987e73bd79bf08c7100f6885
Aditya Jamuar <adityaj@geekyants.com>
2021-03-25 12:55:54 +0530

    icon example made to default

--- example/storybook/stories/components/primitives/Icon/Basic.tsx
+++ example/storybook/stories/components/primitives/Icon/Basic.tsx
@@ -4,11 +4,7 @@ import { Icon } from 'native-base';
 export default function () {
   return (
     <>
-      {['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl'].map(
-        (size) => (
-          <Icon type="Ionicons" name="md-checkmark-circle" size={size} />
-        )
-      )}
+      <Icon type="Ionicons" name="md-checkmark-circle" />
     </>
   );
 }
