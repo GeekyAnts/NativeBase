8493a0b8fc48f5bf4cba3f740e7f514e0eff893b
Nishan <nishanbende@gmail.com>
2021-03-25 12:49:16 +0530

    chore: cleanup

--- example/storybook/stories/components/composites/Menu/Basic.tsx
+++ example/storybook/stories/components/composites/Menu/Basic.tsx
@@ -2,29 +2,17 @@ import React from 'react';
 import { Menu, Divider, IconButton, Icon } from 'native-base';
 
 export default function () {
-  const [loading, setLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 5000);
-  }, []);
   return (
     <Menu
       trigger={(triggerProps: any) => {
         return <IconButton icon={<Icon name="menu" />} {...triggerProps} />;
       }}
     >
-      {loading ? (
-        <Menu.Item isDisabled>Loading</Menu.Item>
-      ) : (
-        <>
-          <Menu.Item>Menu item 2</Menu.Item>
-          <Menu.Item isDisabled>Menu item 3</Menu.Item>
-          <Divider />
-          <Menu.Item>Menu item 4</Menu.Item>
-        </>
-      )}
+      <Menu.Item isDisabled>Menu item 1</Menu.Item>
+      <Menu.Item>Menu item 2</Menu.Item>
+      <Menu.Item isDisabled>Menu item 3</Menu.Item>
+      <Divider />
+      <Menu.Item>Menu item 4</Menu.Item>
     </Menu>
   );
 }
