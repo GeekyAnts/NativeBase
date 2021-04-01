ce3d3a4c92fd3c720d19ef3c995f583f47aee0d4
cyrus25 <cyruspassi@gmail.com>
2021-04-01 17:50:04 +0530

    added fontWeight 700 to card header and footer for both android and ios

--- src/theme/components/CardItem.js
+++ src/theme/components/CardItem.js
@@ -10,19 +10,19 @@ export default (variables /* : * */ = variable) => {
   const transparentBtnCommon = {
     'NativeBase.Text': {
       fontSize: variables.DefaultFontSize - 3,
-      color: variables.sTabBarActiveTextColor
+      color: variables.sTabBarActiveTextColor,
     },
     'NativeBase.Icon': {
       fontSize: variables.iconFontSize - 10,
       color: variables.sTabBarActiveTextColor,
-      marginHorizontal: null
+      marginHorizontal: null,
     },
     'NativeBase.IconNB': {
       fontSize: variables.iconFontSize - 10,
-      color: variables.sTabBarActiveTextColor
+      color: variables.sTabBarActiveTextColor,
     },
     paddingVertical: null,
-    paddingHorizontal: null
+    paddingHorizontal: null,
   };
 
   const cardItemTheme = {
@@ -32,166 +32,166 @@ export default (variables /* : * */ = variable) => {
           '.note': {
             color: variables.listNoteColor,
             fontWeight: '400',
-            marginRight: 20
-          }
+            marginRight: 20,
+          },
         },
         flex: 1,
         marginLeft: 10,
-        alignItems: null
+        alignItems: null,
       },
       'NativeBase.Icon': {
-        fontSize: variables.iconFontSize
+        fontSize: variables.iconFontSize,
       },
       'NativeBase.IconNB': {
-        fontSize: variables.iconFontSize
+        fontSize: variables.iconFontSize,
       },
       'NativeBase.Text': {
         marginLeft: 10,
-        alignSelf: 'center'
+        alignSelf: 'center',
       },
       'NativeBase.Button': {
         '.transparent': {
           ...transparentBtnCommon,
-          paddingRight: variables.cardItemPadding + 5
-        }
+          paddingRight: variables.cardItemPadding + 5,
+        },
       },
       flex: 1,
       flexDirection: 'row',
-      alignItems: 'center'
+      alignItems: 'center',
     },
     '.content': {
       'NativeBase.Text': {
         color: platform === PLATFORM.IOS ? '#555' : '#222',
-        fontSize: variables.DefaultFontSize - 2
-      }
+        fontSize: variables.DefaultFontSize - 2,
+      },
     },
     '.cardBody': {
       padding: -5,
       'NativeBase.Text': {
-        marginTop: 5
-      }
+        marginTop: 5,
+      },
     },
     'NativeBase.Body': {
       'NativeBase.Text': {
         '.note': {
           color: variables.listNoteColor,
           fontWeight: '200',
-          marginRight: 20
-        }
+          marginRight: 20,
+        },
       },
       'NativeBase.Button': {
         '.transparent': {
           ...transparentBtnCommon,
           paddingRight: variables.cardItemPadding + 5,
-          alignSelf: 'stretch'
-        }
+          alignSelf: 'stretch',
+        },
       },
       flex: 1,
       alignSelf: 'stretch',
-      alignItems: 'flex-start'
+      alignItems: 'flex-start',
     },
     'NativeBase.Right': {
       'NativeBase.Badge': {
-        alignSelf: null
+        alignSelf: null,
       },
       'NativeBase.Button': {
         '.transparent': {
-          ...transparentBtnCommon
+          ...transparentBtnCommon,
         },
-        alignSelf: null
+        alignSelf: null,
       },
       'NativeBase.Icon': {
         alignSelf: null,
         fontSize: variables.iconFontSize - 8,
-        color: variables.cardBorderColor
+        color: variables.cardBorderColor,
       },
       'NativeBase.IconNB': {
         alignSelf: null,
         fontSize: variables.iconFontSize - 8,
-        color: variables.cardBorderColor
+        color: variables.cardBorderColor,
       },
       'NativeBase.Text': {
         fontSize: variables.DefaultFontSize - 1,
-        alignSelf: null
+        alignSelf: null,
       },
       'NativeBase.Thumbnail': {
-        alignSelf: null
+        alignSelf: null,
       },
       'NativeBase.Image': {
-        alignSelf: null
+        alignSelf: null,
       },
       'NativeBase.Radio': {
-        alignSelf: null
+        alignSelf: null,
       },
       'NativeBase.Checkbox': {
-        alignSelf: null
+        alignSelf: null,
       },
       'NativeBase.Switch': {
-        alignSelf: null
+        alignSelf: null,
       },
-      flex: 0.8
+      flex: 0.8,
     },
     '.header': {
       'NativeBase.Text': {
         fontSize: 16,
-        fontWeight: platform === PLATFORM.IOS ? '600' : '500'
+        fontWeight: platform === PLATFORM.IOS ? '700' : '700',
       },
       '.bordered': {
         'NativeBase.Text': {
           color: variables.brandPrimary,
-          fontWeight: platform === PLATFORM.IOS ? '600' : '500'
+          fontWeight: platform === PLATFORM.IOS ? '700' : '700',
         },
-        borderBottomWidth: variables.borderWidth
+        borderBottomWidth: variables.borderWidth,
       },
       borderBottomWidth: null,
-      paddingVertical: variables.cardItemPadding + 5
+      paddingVertical: variables.cardItemPadding + 5,
     },
     '.footer': {
       'NativeBase.Text': {
         fontSize: 16,
-        fontWeight: platform === PLATFORM.IOS ? '600' : '500'
+        fontWeight: platform === PLATFORM.IOS ? '700' : '700',
       },
       '.bordered': {
         'NativeBase.Text': {
           color: variables.brandPrimary,
-          fontWeight: platform === PLATFORM.IOS ? '600' : '500'
+          fontWeight: platform === PLATFORM.IOS ? '700' : '700',
         },
-        borderTopWidth: variables.borderWidth
+        borderTopWidth: variables.borderWidth,
       },
-      borderBottomWidth: null
+      borderBottomWidth: null,
     },
     'NativeBase.Text': {
       '.note': {
         color: variables.listNoteColor,
-        fontWeight: '200'
-      }
+        fontWeight: '200',
+      },
     },
     'NativeBase.Icon': {
       width: variables.iconFontSize + 5,
-      fontSize: variables.iconFontSize - 2
+      fontSize: variables.iconFontSize - 2,
     },
     'NativeBase.IconNB': {
       width: variables.iconFontSize + 5,
-      fontSize: variables.iconFontSize - 2
+      fontSize: variables.iconFontSize - 2,
     },
     '.bordered': {
       borderBottomWidth: StyleSheet.hairlineWidth,
-      borderColor: variables.cardBorderColor
+      borderColor: variables.cardBorderColor,
     },
     '.first': {
       borderTopLeftRadius: variables.cardBorderRadius,
-      borderTopRightRadius: variables.cardBorderRadius
+      borderTopRightRadius: variables.cardBorderRadius,
     },
     '.last': {
       borderBottomLeftRadius: variables.cardBorderRadius,
-      borderBottomRightRadius: variables.cardBorderRadius
+      borderBottomRightRadius: variables.cardBorderRadius,
     },
     flexDirection: 'row',
     alignItems: 'center',
     borderRadius: variables.cardBorderRadius,
     padding: variables.cardItemPadding + 5,
     paddingVertical: variables.cardItemPadding,
-    backgroundColor: variables.cardDefaultBg
+    backgroundColor: variables.cardDefaultBg,
   };
 
   return cardItemTheme;
