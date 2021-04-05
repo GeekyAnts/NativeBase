78af6f7cbb35231f18ab354a49628691b5f1897f
Nishan Bende <nishanbende@Nishans-MacBook-Pro.local>
2021-04-05 16:37:59 +0530

    fix: v3 stack wrap

--- src/utils/getSpacedChildren.tsx
+++ src/utils/getSpacedChildren.tsx
@@ -20,9 +20,13 @@ export default (
   divider: JSX.Element | undefined
 ): any => {
   let childrenArray = React.Children.toArray(children);
+  childrenArray =
+    reverse === 'reverse' ? [...childrenArray].reverse() : childrenArray;
+
   const orientation = axis === 'X' ? 'vertical' : 'horizontal';
+
   if (divider) {
-    divider = React.cloneElement(divider, {
+    divider = React.cloneElement(<Box>{divider}</Box>, {
       ...divider.props,
       orientation,
     });
@@ -33,36 +37,7 @@ export default (
     );
     childrenArray = childrenArray.slice(1, -1);
   }
-  /*
-  | Separate the trailing (not first) children from the children array
-  */
 
-  /*
-  | Wrapping Children with a Box so that any custom component can also work properly placed inside a stack
-  */
-
-  childrenArray = childrenArray.map((child: any, index: number) => {
-    /*
-    | wrapperProps takes child's mt , marginTop , ml or marginLeft and add that to wrapper.
-    */
-    let wrapperProps: object = {};
-    if (child.props.ml !== undefined || child.props.marginLeft !== undefined) {
-      wrapperProps = { ...{ ml: child.props.ml || child.props.marginLeft } };
-    }
-    if (child.props.mt !== undefined || child.props.marginTop !== undefined) {
-      wrapperProps = { ...{ mt: child.props.mt || child.props.marginTop } };
-    }
-
-    return React.cloneElement(<Box>{child}</Box>, {
-      key: `stack-wrapper-${index}`,
-      ...wrapperProps,
-    });
-  });
-  const trailingChildren =
-    reverse === 'reverse' ? childrenArray.slice(0, -1) : childrenArray.slice(1);
-  /*
-  | Set margin prop based on axis
-  */
   let spaceValue;
   if (typeof space === 'string') {
     switch (space) {
@@ -100,44 +75,15 @@ export default (
   }
 
   const marginProp: object = {
-    ...(axis === 'X' ? { ml: spaceValue } : { mt: spaceValue }),
+    ...(axis === 'X' ? { width: spaceValue } : { height: spaceValue }),
   };
 
-  /*
-  | Add the margiin to the children
-  */
-  /*
-  | New children array with applied margin to trailing children
-  */
-  if (reverse === 'reverse') {
-    const trailingChildrenWithSpacingReverse = trailingChildren
-      .reverse()
-      .map((child: any, index: number) => {
-        return React.cloneElement(
-          child,
-          {
-            key: `reverse-spaced-child-${index}`,
-            ...marginProp,
-            ...child.props,
-          },
-          child.props.children
-        );
-      });
-
-    return [
-      childrenArray[childrenArray.length - 1],
-      trailingChildrenWithSpacingReverse,
-    ];
-  } else {
-    const trailingChildrenWithSpacing = trailingChildren.map(
-      (child: any, index: number) => {
-        return React.cloneElement(
-          child,
-          { key: `spaced-child-${index}`, ...marginProp, ...child.props },
-          child.props.children
-        );
-      }
+  return childrenArray.map((child: any, index: number) => {
+    return (
+      <React.Fragment key={`spaced-child-${index}`}>
+        {child}
+        {index < childrenArray.length - 1 && <Box {...marginProp} />}
+      </React.Fragment>
     );
-    return [childrenArray[0], trailingChildrenWithSpacing];
-  }
+  });
 };
