4ca52150b4d73e0b48322526831bf2d914629a7a
Nishan Bende <nishanbende@Nishans-MacBook-Pro.local>
2021-04-06 16:28:22 +0530

    fix: load font only once in example app

--- example/App.tsx
+++ example/App.tsx
@@ -1,1 +1,17 @@
-export { default } from './storybook';
+import React from 'react';
+import StoryBookUI from './storybook';
+import { useFonts } from 'expo-font';
+
+export default () => {
+  const [loaded] = useFonts({
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    'Roboto-Italic': require('./assets/fonts/Roboto-Italic.ttf'),
+    'Roboto-Light': require('./assets/fonts/Roboto-Light.ttf'),
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+  });
+
+  if (!loaded) return null;
+
+  return <StoryBookUI />;
+};
