55801cb1aed82959b66d7a10f0e5169320203903
Aditya Jamuar <adityaj@geekyants.com>
2021-04-17 16:27:28 +0530

    fix: changes in few components

--- example/storybook/stories/components/primitives/Image/Basic.tsx
+++ example/storybook/stories/components/primitives/Image/Basic.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { Box, Image } from 'native-base';
 export default function () {
   return (
-    <Box size={200}>
-      <Image
-        source={{
-          uri: 'https://wallpaperaccess.com/full/317501.jpg',
-        }}
-        alt="Alternate Text"
-      />
-    </Box>
+    <Image
+      source={{
+        uri: 'https://wallpaperaccess.com/full/317501.jpg',
+      }}
+      alt="Alternate Text"
+    />
   );
 }
