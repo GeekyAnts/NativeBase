80b43674f5fbc8ff4f35c06e70ad6e9ba51c56f2
Nishan Bende <nishanbende@gmail.com>
2021-05-03 11:21:55 +0530

    chore: merge conflicts resolved

--- example/storybook/stories/components/primitives/Button/basic.tsx
+++ example/storybook/stories/components/primitives/Button/basic.tsx
@@ -2,9 +2,5 @@ import React from 'react';
 import { Button } from 'native-base';
 
 export default function () {
-  return (
-    <Button size="xs" onPress={() => console.log('hello world')}>
-      PRIMARY
-    </Button>
-  );
+  return <Button onPress={() => console.log('hello world')}>PRIMARY</Button>;
 }
