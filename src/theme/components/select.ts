6bdae88869e8a50b146dba2accab37a045a4ef9a
Nishan Bende <nishanbende@gmail.com>
2021-05-03 18:13:24 +0530

    chore: remove rn picker

--- src/theme/components/select.ts
+++ src/theme/components/select.ts
@@ -1,14 +1,5 @@
-import { mode } from '../tools/colors';
-// custom
-const customSelectBaseStyle = (props: Record<string, any>) => {
-  return {
-    type: 'custom',
-    _isDisabled: {
-      opacity: 0.4,
-    },
-    _isInvalid: {
-      borderColor: mode('danger.600', 'danger.200')(props),
-    },
+export const Select = {
+  baseStyle: {
     customDropdownIconProps: {
       type: 'MaterialCommunityIcons',
       name: 'chevron-down',
@@ -19,43 +10,9 @@ const customSelectBaseStyle = (props: Record<string, any>) => {
       maxHeight: '50%',
       pb: 6,
     },
-  };
-};
-export const CustomSelect = {
-  baseStyle: customSelectBaseStyle,
-};
-
-// native
-const nativeSelectBaseStyle = (props: Record<string, any>) => {
-  return {
-    type: 'native',
-    androidIconColor: mode('darkText', 'lightText')(props),
-    color: mode('darkText', 'lightText')(props),
-    _android: {
-      p: 3,
-    },
-    _ios: {},
-    _web: {
-      bg: 'transparent',
-      borderRadius: 'md',
-      borderColor: 'muted.500',
-      p: 3,
-    },
-    _hover: {
-      borderColor: 'primary.500',
-    },
-  };
-};
-
-export const NativeSelect = {
-  baseStyle: nativeSelectBaseStyle,
-};
-
-// Select
-export const Select = {
+  },
   defaultProps: {
     variant: 'outline',
-    type: 'custom',
   },
 };
 
