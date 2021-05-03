f1742e38b8d5e967673b183c80328247df32d901
MD-REHMAN <Rehman.2468@gmail.com>
2021-05-03 12:10:57 +0530

    fix: removed support of name and types form Icon component

--- src/components/primitives/Icon/Icon.tsx
+++ src/components/primitives/Icon/Icon.tsx
@@ -1,85 +1,49 @@
 import React from 'react';
-import {
-  border,
-  color,
-  flexbox,
-  layout,
-  space,
-  typography,
-  position,
-} from 'styled-system';
-import {
-  customBorder,
-  customBackground,
-  customOutline,
-  customLayout,
-  customExtra,
-  customShadow,
-  customTypography,
-  customPosition,
-} from '../../../utils/customProps';
-import styled from 'styled-components/native';
 import { usePropsResolution } from '../../../hooks/useThemeProps';
-import {
-  AntDesign,
-  Entypo,
-  EvilIcons,
-  Feather,
-  FontAwesome,
-  FontAwesome5,
-  Fontisto,
-  Foundation,
-  Ionicons,
-  MaterialCommunityIcons,
-  MaterialIcons,
-  Octicons,
-  SimpleLineIcons,
-  Zocial,
-} from '@expo/vector-icons';
-import type { IIconProps, IconType } from './types';
+import type { IIconProps } from './types';
 import SVGIcon from './SVGIcon';
 import Text from '../Text';
 
-const componentMap = {
-  AntDesign,
-  Entypo,
-  EvilIcons,
-  Feather,
-  FontAwesome,
-  FontAwesome5,
-  Fontisto,
-  Foundation,
-  Ionicons,
-  MaterialCommunityIcons,
-  MaterialIcons,
-  Octicons,
-  SimpleLineIcons,
-  Zocial,
-};
+// const componentMap = {
+//   AntDesign,
+//   Entypo,
+//   EvilIcons,
+//   Feather,
+//   FontAwesome,
+//   FontAwesome5,
+//   Fontisto,
+//   Foundation,
+//   Ionicons,
+//   MaterialCommunityIcons,
+//   MaterialIcons,
+//   Octicons,
+//   SimpleLineIcons,
+//   Zocial,
+// };
 
-let memoizedStyledIcons: any = {};
-function getStyledIconComponent(type: IconType) {
-  if (!memoizedStyledIcons[type]) {
-    memoizedStyledIcons[type] = styled(componentMap[type])<IIconProps>(
-      color,
-      space,
-      layout,
-      flexbox,
-      border,
-      typography,
-      position,
-      customPosition,
-      customBorder,
-      customBackground,
-      customOutline,
-      customShadow,
-      customExtra,
-      customLayout,
-      customTypography
-    );
-  }
-  return memoizedStyledIcons[type];
-}
+// let memoizedStyledIcons: any = {};
+// function getStyledIconComponent(type: IconType) {
+//   if (!memoizedStyledIcons[type]) {
+//     memoizedStyledIcons[type] = styled(componentMap[type])<IIconProps>(
+//       color,
+//       space,
+//       layout,
+//       flexbox,
+//       border,
+//       typography,
+//       position,
+//       customPosition,
+//       customBorder,
+//       customBackground,
+//       customOutline,
+//       customShadow,
+//       customExtra,
+//       customLayout,
+//       customTypography
+//     );
+//   }
+//   return memoizedStyledIcons[type];
+// }
 
 const Icon = ({ as, ...props }: IIconProps, ref?: any) => {
   const { size, ...resolvedProps } = usePropsResolution('Icon', props);
@@ -87,11 +51,11 @@ const Icon = ({ as, ...props }: IIconProps, ref?: any) => {
     return <SVGIcon {...resolvedProps} size={size} ref={ref} />;
   }
 
-  console.log(
-    '%c MYLOG: Icon-resolvedProps',
-    'background: #374151; color: #FBBF24; font-weight: 700; padding: 2px 8px;',
-    resolvedProps
-  );
+  // console.log(
+  //   '%c MYLOG: Icon-resolvedProps',
+  //   'background: #374151; color: #FBBF24; font-weight: 700; padding: 2px 8px;',
+  //   resolvedProps
+  // );
 
   return (
     <Text fontSize={size} {...resolvedProps}>
