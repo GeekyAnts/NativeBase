e8c8dc248b02a8052c0542f41f35ca82e26e6b8d
Nishan Bende <nishanbende@gmail.com>
2021-05-03 14:51:10 +0530

    added analyser

---
+++ benchmarking/web/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {NativeBaseProvider, Box} from 'native-base';
+
+function App() {
+  return (
+    <NativeBaseProvider>
+      <Box>hii</Box>
+    </NativeBaseProvider>
+  );
+}
+
+export default App;
