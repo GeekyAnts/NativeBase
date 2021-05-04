e7512a452ddfec89789493f6fe86c49b2f5282cf
shrutiag14 <48885655+shrutiag14@users.noreply.github.com>
2021-05-04 12:49:20 +0530

    Update DatePicker.js

--- src/basic/DatePicker.js
+++ src/basic/DatePicker.js
@@ -1,63 +1,100 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { TouchableOpacity } from 'react-native';
+import IconNB from 'react-native-vector-icons/Ionicons';
+import { connectStyle } from 'native-base-shoutem-theme';
 
-import DateTimePicker from '@react-native-community/datetimepicker';
-
+import mapPropsToStyleNames from '../utils/mapPropsToStyleNames';
 import variable from '../theme/variables/platform';
+import { PLATFORM } from '../theme/variables/commonColor';
+import computeProps from '../utils/computeProps';
 
-export class DatePicker extends React.Component {
-  static defaultProps = {
-    disabled: false
+class CheckBox extends Component {
+  static contextTypes = {
+    theme: PropTypes.object
   };
-  constructor(props) {
-    super(props);
-    this.state = {
-      defaultDate: props.defaultDate ? props.defaultDate : new Date(),
-      chosenDate:
-        !props.placeHolderText && props.defaultDate
-          ? props.defaultDate
-          : undefined
+
+  getInitialStyle(variables) {
+    const { color, checked, checkboxType, borderColor } = this.props;
+    return {
+      checkStyle: {
+        borderRadius: this.getBorderRadius(checkboxType, variables),
+        borderColor: borderColor || color || variables.checkboxBgColor,
+        backgroundColor:
+          checked === true
+            ? color || variables.checkboxBgColor
+            : variables.checkboxDefaultColor
+      }
     };
   }
 
-  setDate(date) {
-    this.setState({ chosenDate: new Date(date) });
-    if (this.props.onDateChange) {
-      this.props.onDateChange(date);
+  // eslint-disable-next-line class-methods-use-this
+  getBorderRadius(checkboxType, variables) {
+    if (checkboxType === 'rounded') {
+      return 13;
     }
+    if (checkboxType === 'square') {
+      return 0;
+    }
+    return variables.CheckboxRadius;
   }
 
-  formatChosenDate(date) {
-    if (this.props.formatChosenDate) {
-      return this.props.formatChosenDate(date);
-    }
-    return [date.getDate(), date.getMonth() + 1, date.getFullYear()].join('/');
-  }
+  prepareRootProps(variables) {
+    const defaultProps = {
+      style: this.getInitialStyle(variables).checkStyle
+    };
 
+    return computeProps(this.props, defaultProps);
+  }
   render() {
-    const {
-      locale,
-      maximumDate,
-      minimumDate,
-      timeZoneOffsetInMinutes
-    } = this.props;
-
+    const { checked, tickColor } = this.props;
     const variables = this.context.theme
       ? this.context.theme['@@shoutem.theme/themeStyle'].variables
       : variable;
-
+    const platformStyle = variables.platformStyle;
+    const platform = variables.platform;
     return (
-      <DateTimePicker
-        date={
-          this.state.chosenDate ? this.state.chosenDate : this.state.defaultDate
-        }
-        onDateChange={date => this.setDate(date)}
-        minimumDate={minimumDate}
-        maximumDate={maximumDate}
-        mode="date"
-        locale={locale}
-        timeZoneOffsetInMinutes={timeZoneOffsetInMinutes}
-        {...this.props}
-      />
+      <TouchableOpacity
+        ref={c => (this._root = c)}
+        {...this.prepareRootProps(variables)}
+      >
+        <IconNB
+          style={{
+            color:
+              checked === true
+                ? tickColor || variables.checkboxTickColor
+                : variables.checkboxDefaultColor,
+            fontSize: variables.CheckboxFontSize,
+            lineHeight: variables.CheckboxIconSize,
+            marginTop: variables.CheckboxIconMarginTop,
+            textShadowRadius: variables.checkboxTextShadowRadius
+          }}
+          name={
+            platform === PLATFORM.IOS && platformStyle !== PLATFORM.MATERIAL
+              ? 'ios-checkmark'
+              : 'md-checkmark'
+          }
+        />
+      </TouchableOpacity>
     );
   }
 }
+
+CheckBox.propTypes = {
+  ...TouchableOpacity.propTypes,
+  style: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.number,
+    PropTypes.array
+  ]),
+  checked: PropTypes.bool,
+  onPress: PropTypes.func
+};
+
+const StyledCheckBox = connectStyle(
+  'NativeBase.CheckBox',
+  {},
+  mapPropsToStyleNames
+)(CheckBox);
+
+export { StyledCheckBox as CheckBox };
