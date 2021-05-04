e3233814d0dd23d9131c50c4248f6b68dd07ce79
MD-REHMAN <Rehman.2468@gmail.com>
2021-05-04 18:42:33 +0530

    fix: removed expo/vector-icons

--- src/components/primitives/Icon/Icon.tsx
+++ src/components/primitives/Icon/Icon.tsx
@@ -1,76 +1,23 @@
 import React from 'react';
-import { usePropsResolution } from '../../../hooks/useThemeProps';
+import { useToken, usePropsResolution } from '../../../hooks';
 import type { IIconProps } from './types';
 import SVGIcon from './SVGIcon';
-import Text from '../Text';
-
-// const componentMap = {
-//   AntDesign,
-//   Entypo,
-//   EvilIcons,
-//   Feather,
-//   FontAwesome,
-//   FontAwesome5,
-//   Fontisto,
-//   Foundation,
-//   Ionicons,
-//   MaterialCommunityIcons,
-//   MaterialIcons,
-//   Octicons,
-//   SimpleLineIcons,
-//   Zocial,
-// };
-
-// let memoizedStyledIcons: any = {};
-// function getStyledIconComponent(type: IconType) {
-//   if (!memoizedStyledIcons[type]) {
-//     memoizedStyledIcons[type] = styled(componentMap[type])<IIconProps>(
-//       color,
-//       space,
-//       layout,
-//       flexbox,
-//       border,
-//       typography,
-//       position,
-//       customPosition,
-//       customBorder,
-//       customBackground,
-//       customOutline,
-//       customShadow,
-//       customExtra,
-//       customLayout,
-//       customTypography
-//     );
-//   }
-//   return memoizedStyledIcons[type];
-// }
 
 const Icon = ({ as, ...props }: IIconProps, ref?: any) => {
-  const { size, ...resolvedProps } = usePropsResolution('Icon', props);
+  const { size, color, ...resolvedProps } = usePropsResolution('Icon', props);
+  const hexColor = useToken('colors', color || '');
+  const themeSize = useToken('space', size || '');
   if (!as) {
-    return <SVGIcon {...resolvedProps} size={size} ref={ref} />;
+    return <SVGIcon {...props} ref={ref} />;
   }
 
-  // console.log(
-  //   '%c MYLOG: Icon-resolvedProps',
-  //   'background: #374151; color: #FBBF24; font-weight: 700; padding: 2px 8px;',
-  //   resolvedProps
-  // );
-
-  return (
-    <Text fontSize={size} {...resolvedProps}>
-      {as}
-    </Text>
-  );
-  // const Component = getStyledIconComponent(type ?? 'MaterialIcons');
-  // return (
-  //   <Component
-  //     name={name}
-  //     {...resolvedProps}
-  //     ref={ref}
-  //     style={[style, { fontSize: parseInt(size, 10) }]}
-  //   />
-  // );
+  const asWithProps = React.cloneElement(as, {
+    ...as.props,
+    ...resolvedProps,
+    color: hexColor,
+    size: themeSize,
+  });
+  return asWithProps;
 };
 
 export default React.memo(React.forwardRef(Icon));
--- src/components/primitives/Icon/types.ts
+++ src/components/primitives/Icon/types.ts
@@ -22,50 +22,6 @@ import type {
   customPositionProps,
 } from '../../../utils/customProps';
 
-import type { default as AntDesignIconNames } from '@expo/vector-icons/build/AntDesign';
-import type { default as EntypoIconNames } from '@expo/vector-icons/build/Entypo';
-import type { default as EvilIconsIconNames } from '@expo/vector-icons/build/EvilIcons';
-import type { default as FeatherIconNames } from '@expo/vector-icons/build/Feather';
-import type { default as FontAwesomeIconNames } from '@expo/vector-icons/build/FontAwesome';
-import type { default as FontistoIconNames } from '@expo/vector-icons/build/Fontisto';
-import type { default as FoundationIconNames } from '@expo/vector-icons/build/Foundation';
-import type { default as IoniconsIconNames } from '@expo/vector-icons/build/Ionicons';
-import type { default as MaterialCommunityIconsIconNames } from '@expo/vector-icons/build/MaterialCommunityIcons';
-import type { default as MaterialIconsIconNames } from '@expo/vector-icons/build/MaterialIcons';
-import type { default as OcticonsIconNames } from '@expo/vector-icons/build/Octicons';
-import type { default as SimpleLineIconsIconNames } from '@expo/vector-icons/build/SimpleLineIcons';
-import type { default as ZocialIconNames } from '@expo/vector-icons/build/Zocial';
-
-export type IconNameType =
-  | keyof typeof AntDesignIconNames.glyphMap
-  | keyof typeof EntypoIconNames.glyphMap
-  | keyof typeof EvilIconsIconNames.glyphMap
-  | keyof typeof FeatherIconNames.glyphMap
-  | keyof typeof FontAwesomeIconNames.glyphMap
-  | keyof typeof FontistoIconNames.glyphMap
-  | keyof typeof FoundationIconNames.glyphMap
-  | keyof typeof IoniconsIconNames.glyphMap
-  | keyof typeof MaterialCommunityIconsIconNames.glyphMap
-  | keyof typeof MaterialIconsIconNames.glyphMap
-  | keyof typeof OcticonsIconNames.glyphMap
-  | keyof typeof SimpleLineIconsIconNames.glyphMap
-  | keyof typeof ZocialIconNames.glyphMap;
-export type IconType =
-  | 'AntDesign'
-  | 'Entypo'
-  | 'EvilIcons'
-  | 'Feather'
-  | 'FontAwesome'
-  | 'FontAwesome5'
-  | 'Fontisto'
-  | 'Foundation'
-  | 'Ionicons'
-  | 'MaterialCommunityIcons'
-  | 'MaterialIcons'
-  | 'Octicons'
-  | 'SimpleLineIcons'
-  | 'Zocial';
-
 export type IIconProps = IPlatformProps &
   ColorProps &
   SpaceProps &
