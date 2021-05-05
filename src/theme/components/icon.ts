09cdcb0c5e7c1c5659622def5666d26871bbe699
Nishan Bende <nishanbende@gmail.com>
2021-05-05 20:06:59 +0530

    fix: icon size revert

--- src/theme/components/icon.ts
+++ src/theme/components/icon.ts
@@ -6,19 +6,19 @@ const baseStyle = (props: Record<string, any>) => {
   };
 };
 
-const iconSizes = {
-  'xxs': '2',
-  'xs': '4',
-  'sm': '6',
-  'md': '8',
-  'lg': '10',
-  'xl': '12',
-  '2xl': '16',
-  '3xl': '20',
-  '4xl': '24',
-  '5xl': '32',
-  '6xl': '64',
+const sizes = {
+  'xxs': 2,
+  'xs': 4,
+  'sm': 6,
+  'md': 8,
+  'lg': 10,
+  'xl': 12,
+  '2xl': 16,
+  '3xl': 20,
+  '4xl': 24,
+  '5xl': 32,
+  '6xl': 64,
 };
 
 const defaultProps = { size: 'md', color: 'primary' };
-export default { baseStyle, iconSizes, defaultProps };
+export default { baseStyle, sizes, defaultProps };
