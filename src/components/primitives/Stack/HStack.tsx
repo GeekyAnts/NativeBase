8bce43e0aea530f58ea27aa5f34c14496c60e520
Nishan Bende <nishanbende@gmail.com>
2021-05-10 17:31:57 +0530

    fix: stack type and doc fixes

--- src/components/primitives/Stack/HStack.tsx
+++ src/components/primitives/Stack/HStack.tsx
@@ -1,8 +1,15 @@
 import React, { memo, forwardRef } from 'react';
 import StackMain, { IStackProps } from './Stack';
 import { usePropsResolution } from '../../../hooks/useThemeProps';
+export interface IHStackProps extends IStackProps {
+  /**
+   * The direction of the Stack Items.
+   * @default row
+   */
+  direction?: 'column' | 'row';
+}
 
-const HStack = (props: IStackProps, ref?: any) => {
+const HStack = (props: IHStackProps, ref?: any) => {
   const newProps = usePropsResolution('HStack', props);
   return <StackMain ref={ref} direction="row" {...newProps} />;
 };
--- src/components/primitives/Stack/VStack.tsx
+++ src/components/primitives/Stack/VStack.tsx
@@ -1,3 +1,17 @@
-import StackMain from './Stack';
+import React, { memo, forwardRef } from 'react';
+import StackMain, { IStackProps } from './Stack';
+import { usePropsResolution } from '../../../hooks/useThemeProps';
+export interface IVStackProps extends IStackProps {
+  /**
+   * The direction of the Stack Items.
+   * @default column
+   */
+  direction?: 'column' | 'row';
+}
 
-export default StackMain;
+const HStack = (props: IVStackProps, ref?: any) => {
+  const newProps = usePropsResolution('VStack', props);
+  return <StackMain ref={ref} {...newProps} />;
+};
+
+export default memo(forwardRef(HStack));
