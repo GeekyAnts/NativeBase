fac4c5b75d55929713342baff378c3ad78f9121a
Nishan Bende <nishanbende@gmail.com>
2021-05-11 13:53:24 +0530

    fix: toast add custom component example

--- example/storybook/stories/components/composites/Toast/index.tsx
+++ example/storybook/stories/components/composites/Toast/index.tsx
@@ -5,7 +5,8 @@ import Wrapper from '../../Wrapper';
 import { Example as ToastPositions } from './ToastPositions';
 import { Example as ToastVariants } from './ToastVariants';
 import { Example as Basic } from './Basic';
-import { Example as CustomID } from './CustomId';
+import { Example as HideToast } from './HideToast';
+import { Example as CustomComponent } from './CustomComponent';
 import { Example as PreventDuplicate } from './PreventDuplicate';
 
 storiesOf('Toast', module)
@@ -14,5 +15,6 @@ storiesOf('Toast', module)
   .add('Toast Variants', () => <ToastVariants />)
   .add('Basic', () => <Basic />)
   .add('Toast Positions', () => <ToastPositions />)
-  .add('Custom Id', () => <CustomID />)
+  .add('Custom Component', () => <CustomComponent />)
+  .add('Custom Id', () => <HideToast />)
   .add('Prevent Duplicate', () => <PreventDuplicate />);
