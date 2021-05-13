40a4328abf8932e241612bfe4d5869f4478b22ad
Nishan Bende <nishanbende@gmail.com>
2021-05-13 18:13:34 +0530

    chore: image docs

--- src/components/primitives/Image/types.ts
+++ src/components/primitives/Image/types.ts
@@ -36,8 +36,20 @@ export interface IImageProps
     ShadowProps,
     LayoutProps,
     PositionProps {
+  /**
+   * The alt text that describes the image. This will be added as accessibilityLabel in android/iOS and alt on web
+   */
   alt: string;
+  /**
+   * In event there was an error loading the src, specify a fallback source.
+   */
   fallbackSource?: ImageSourcePropType;
+  /**
+   * Opt out of the fallbackSource logic and show alternative text
+   */
   ignoreFallback?: boolean;
+  /**
+   * Text styling for alt.
+   */
   _alt?: ITextProps;
 }
