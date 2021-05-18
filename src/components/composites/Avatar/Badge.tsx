85701501c35ffd38aa16f58ac4d141ab97114823
Nishan Bende <nishanbende@gmail.com>
2021-05-18 14:39:17 +0530

    chore: avatar docs

--- src/components/composites/Avatar/Badge.tsx
+++ src/components/composites/Avatar/Badge.tsx
@@ -3,20 +3,22 @@ import Box from '../../primitives/Box';
 import { usePropsResolution } from '../../../hooks/useThemeProps';
 import type { IAvatarBadgeProps } from './types';
 
-const AvatarBadgeImpl = (props: IAvatarBadgeProps, ref?: any) => {
-  const { boxSize, ...newProps } = usePropsResolution('AvatarBadge', props);
-  return (
-    <Box
-      position="absolute"
-      right={0}
-      bottom={0}
-      {...newProps}
-      width={boxSize || 3}
-      height={boxSize || 3}
-      ref={ref}
-    />
-  );
-};
-const AvatarBadge = memo(forwardRef(AvatarBadgeImpl));
+const AvatarBadge = memo(
+  forwardRef((props: IAvatarBadgeProps, ref?: any) => {
+    const { boxSize, ...newProps } = usePropsResolution('AvatarBadge', props);
+    return (
+      <Box
+        position="absolute"
+        right={0}
+        bottom={0}
+        {...newProps}
+        width={boxSize || 3}
+        height={boxSize || 3}
+        ref={ref}
+      />
+    );
+  })
+);
+
 AvatarBadge.displayName = 'AvatarBadge';
-export default AvatarBadge;
+export default AvatarBadge as (props: IAvatarBadgeProps, ref?: any) => any;
