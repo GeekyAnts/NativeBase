f9f3dbd568e7ebf5789918a3b72af1fb60f21931
Varun <44650484+varunhhhrahul@users.noreply.github.com>
2021-05-19 16:47:13 +0530

    Changed body to functional component

--- src/basic/Body.js
+++ src/basic/Body.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { View, ViewPropTypes } from 'react-native';
 import { connectStyle } from 'native-base-shoutem-theme';
 
 import mapPropsToStyleNames from '../utils/mapPropsToStyleNames';
 
-class Body extends Component {
-  render() {
-    return <View ref={c => (this._root = c)} {...this.props} />;
-  }
+const Body = (props) => {
+  const root = useRef(null);
+  
+  return <View ref={root} {...props} />;
+  
 }
 
 Body.propTypes = {
