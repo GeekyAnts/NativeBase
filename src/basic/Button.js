1a608de1c45fb9756e2e5f7e86dad1891c9e1e7b
Varun Ramnani <varunhhhrahul@gmail.com>
2021-05-19 16:13:06 +0530

    changed button to fcuntional component

--- src/basic/Button.js
+++ src/basic/Button.js
@@ -1,5 +1,5 @@
 /* eslint-disable new-cap */
-import React from 'react';
+import React,{useRef,useState,useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {
   TouchableOpacity,
@@ -16,140 +16,135 @@ import mapPropsToStyleNames from '../utils/mapPropsToStyleNames';
 
 import { Text } from './Text';
 
-
-class Button extends React.PureComponent {
-  static contextTypes = {
-    theme: PropTypes.object
+const Button = props => {
+ const root = useRef(null);
+ const getInitialStyle = () => {
+  return {
+    borderedBtn: {
+      borderWidth: props.bordered
+        ? variable.buttonDefaultBorderWidth
+        : undefined,
+      borderRadius:
+        props.rounded && props.bordered
+          ? variable.borderRadiusLarge
+          : variable.buttonDefaultBorderRadius,
+    },
   };
+ };
 
-  setRoot(c){
-    this._root = c;
-  }
+ const prepareRootProps = () => {
+  const { style, ...others } = props;
 
-  getInitialStyle() {
-    return {
-      borderedBtn: {
-        borderWidth: this.props.bordered
-          ? variable.buttonDefaultBorderWidth
-          : undefined,
-        borderRadius:
-          this.props.rounded && this.props.bordered
-            ? variable.borderRadiusLarge
-            : variable.buttonDefaultBorderRadius
+  return {
+    style: StyleSheet.flatten(
+      StyleSheet.compose(getInitialStyle().borderedBtn, style)
+    ),
+    ...others,
+  };
+ };
+  
+ const variables = 
+//  this.context.theme
+//   ? this.context.theme['@@shoutem.theme/themeStyle'].variables
+//   : 
+  variable;
+
+ const children =
+  Platform.OS === PLATFORM.IOS || !variables.buttonUppercaseAndroidText
+    ? props.children
+    : React.Children.map(props.children, (child) =>
+        child && child.type === Text
+          ? React.cloneElement(child, {
+              uppercase:
+                props.buttonUppercaseAndroidText === false
+                  ? false
+                  : variables.buttonUppercaseAndroidText,
+              ...child.props,
+            })
+          : child
+      );
+
+  
+  const rootProps = prepareRootProps();
+
+if (
+  Platform.OS === PLATFORM.IOS ||
+  Platform.OS === PLATFORM.WEB ||
+  variables.androidRipple === false ||
+  Platform.Version < 21
+) {
+  return (
+    <TouchableOpacity
+      {...rootProps}
+      ref={root}
+      activeOpacity={
+        props.activeOpacity > 0
+          ? props.activeOpacity
+          : variable.buttonDefaultActiveOpacity
       }
-    };
-  }
-
-  prepareRootProps() {
-
-    const {style, ...others} = this.props;
-
-    return {
-      style: StyleSheet.flatten(StyleSheet.compose(this.getInitialStyle().borderedBtn, style)),
-      ...others
-    }
-
-  }
-
-  render() {
-    const variables = this.context.theme
-      ? this.context.theme['@@shoutem.theme/themeStyle'].variables
-      : variable;
-
-    const children =
-      Platform.OS === PLATFORM.IOS || !variables.buttonUppercaseAndroidText
-        ? this.props.children
-        : React.Children.map(this.props.children, child =>
-            child && child.type === Text
-              ? React.cloneElement(child, {
-                uppercase: this.props.buttonUppercaseAndroidText === false
-                ? false : variables.buttonUppercaseAndroidText,
-                ...child.props
-              })
-              : child
-          );
-
-    const rootProps = this.prepareRootProps();
-
-    if (
-      Platform.OS === PLATFORM.IOS ||
-      Platform.OS === PLATFORM.WEB ||
-      variables.androidRipple === false ||
-      Platform.Version < 21
-    ) {
-      return (
-        <TouchableOpacity
-          {...rootProps}
-          ref={this.setRoot}
-          activeOpacity={
-            this.props.activeOpacity > 0
-              ? this.props.activeOpacity
-              : variable.buttonDefaultActiveOpacity
-          }
-        >
-          {children}
-        </TouchableOpacity>
-      );
-    }
-    if (this.props.rounded) {
-      const buttonStyle = { ...rootProps.style };
-      const buttonFlex =
-        this.props.full || this.props.block
-          ? variable.buttonDefaultFlex
-          : buttonStyle.flex;
-      return (
-        <View
-          style={[
-            { maxHeight: buttonStyle.height },
-            buttonStyle,
-            { paddingTop: undefined, paddingBottom: undefined }
-          ]}
-        >
-          <TouchableNativeFeedback
-            ref={this.setRoot}
-            background={TouchableNativeFeedback.Ripple(
-              this.props.androidRippleColor || variables.androidRippleColor,
-              true
-            )}
-            {...rootProps}
-          >
-            <View
-              style={[
-                // eslint-disable-next-line no-use-before-define
-                styles.childContainer,
-                {
-                  paddingTop: buttonStyle.paddingTop,
-                  paddingBottom: buttonStyle.paddingBottom,
-                  height: buttonStyle.height,
-                  flexGrow: buttonFlex
-                }
-              ]}
-            >
-              {children}
-            </View>
-          </TouchableNativeFeedback>
-        </View>
-      );
-    }
-    return (
+    >
+      {children}
+    </TouchableOpacity>
+  );
+}
+if (props.rounded) {
+  const buttonStyle = { ...rootProps.style };
+  const buttonFlex =
+    props.full || props.block
+      ? variable.buttonDefaultFlex
+      : buttonStyle.flex;
+  return (
+    <View
+      style={[
+        { maxHeight: buttonStyle.height },
+        buttonStyle,
+        { paddingTop: undefined, paddingBottom: undefined },
+      ]}
+    >
       <TouchableNativeFeedback
-        ref={this.setRoot}
-        onPress={this.props.onPress}
-        background={
-          this.props.transparent
-            ? TouchableNativeFeedback.Ripple('transparent')
-            : TouchableNativeFeedback.Ripple(
-                variables.androidRippleColor,
-                false
-              )
-        }
+        ref={root}
+        background={TouchableNativeFeedback.Ripple(
+          props.androidRippleColor || variables.androidRippleColor,
+          true
+        )}
         {...rootProps}
       >
-        <View {...rootProps}>{children}</View>
+        <View
+          style={[
+            // eslint-disable-next-line no-use-before-define
+            styles.childContainer,
+            {
+              paddingTop: buttonStyle.paddingTop,
+              paddingBottom: buttonStyle.paddingBottom,
+              height: buttonStyle.height,
+              flexGrow: buttonFlex,
+            },
+          ]}
+        >
+          {children}
+        </View>
       </TouchableNativeFeedback>
-    );
-  }
+    </View>
+  );
 }
+return (
+  <TouchableNativeFeedback
+    ref={root}
+    onPress={props.onPress}
+    background={
+      props.transparent
+        ? TouchableNativeFeedback.Ripple('transparent')
+        : TouchableNativeFeedback.Ripple(variables.androidRippleColor, false)
+    }
+    {...rootProps}
+  >
+    <View {...rootProps}>{children}</View>
+  </TouchableNativeFeedback>
+);
+
+}
+
+
 
 Button.propTypes = {
   ...TouchableOpacity.propTypes,
