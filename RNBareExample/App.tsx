f9b2b337ef3afd4c3a08b6a1cd0227221afa5d0f
Nishan Bende <nishanbende@gmail.com>
2021-05-27 19:44:40 +0530

    feat: add fonts in bare example

--- RNBareExample/App.tsx
+++ RNBareExample/App.tsx
@@ -1,19 +1,53 @@
 import React from 'react';
-import {useColorScheme} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-import {Box, NativeBaseProvider} from 'native-base';
+import {View} from 'react-native';
+import {Box, NativeBaseProvider, extendTheme} from 'native-base';
+
+const theme = extendTheme({
+  fontConfig: {
+    Roboto: {
+      100: {
+        normal: 'Roboto-Light',
+      },
+      200: {
+        normal: 'Roboto-Light',
+      },
+      300: {
+        normal: 'Roboto-Light',
+      },
+      400: {
+        normal: 'Roboto-Regular',
+        italic: 'Roboto-Italic',
+      },
+      500: {
+        normal: 'Roboto-Medium',
+      },
+      600: {
+        normal: 'Roboto-Bold',
+      },
+    },
+    Merriweather: {
+      400: {
+        normal: 'Merriweather-Regular',
+      },
+    },
+  },
+
+  // Make sure values below matches any of the keys in `fontConfig`
+  fonts: {
+    heading: 'Merriweather',
+    body: 'Roboto',
+    mono: 'Roboto',
+  },
+});
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <NativeBaseProvider>
-      <Box>hello world</Box>
-    </NativeBaseProvider>
+    <View style={{flex: 1, backgroundColor: 'white'}}>
+      <NativeBaseProvider theme={theme}>
+        <Box _text={{fontWeight: 100}}>hello world</Box>
+        <Box _text={{fontFamily: 'heading'}}>hello world</Box>
+      </NativeBaseProvider>
+    </View>
   );
 };
 
