552f6c52a59d28938de1e27839105ce2af3cd83d
Amar Somani <amars@geekyants.com>
2021-06-16 12:22:57 +0530

    fix: storybook example - stack switch tabs

--- example/storybook/stories/components/composites/Tabs/Usage.tsx
+++ example/storybook/stories/components/composites/Tabs/Usage.tsx
@@ -7,12 +7,10 @@ export const Example = () => {
       <Tabs.Bar>
         <Tabs.Tab>TAB ONE</Tabs.Tab>
         <Tabs.Tab>TAB TWO</Tabs.Tab>
-        <Tabs.Tab>TAB THREE</Tabs.Tab>
       </Tabs.Bar>
       <Tabs.Views>
         <Tabs.View>One</Tabs.View>
         <Tabs.View>Two</Tabs.View>
-        <Tabs.View>Three</Tabs.View>
       </Tabs.Views>
     </Tabs>
   );
