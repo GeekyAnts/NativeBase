797dae60dc2bce49ba3880bd1454e33482dc3c54
Nishan Bende <nishanbende@gmail.com>
2021-06-17 18:28:34 +0530

    fix: clipboard fix on native

--- src/hooks/useClipboard.ts
+++ src/hooks/useClipboard.ts
@@ -1,23 +1,12 @@
 import React from 'react';
-import { NativeModules } from 'react-native';
+import { Clipboard } from 'react-native';
 
 export function useClipboard() {
   const [hasCopied, setHasCopied] = React.useState(false);
   const [value, setValue] = React.useState<string>('');
   const onCopy = async (copiedValue: string) => {
-    if (typeof window !== 'undefined') {
-      const textField = document.createElement('textarea');
-      textField.innerText = copiedValue;
-      document.body.appendChild(textField);
-      if (window.navigator.platform === 'iPhone') {
-        textField.setSelectionRange(0, 99999);
-      } else {
-        textField.select();
-      }
-      document.execCommand('copy');
-      textField.remove();
-    } else if (NativeModules && NativeModules.Clipboard) {
-      await NativeModules.Clipboard.setString(copiedValue);
+    if (Clipboard) {
+      await Clipboard.setString(copiedValue);
     }
     setValue(copiedValue);
     setHasCopied(true);
