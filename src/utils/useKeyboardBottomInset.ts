7617ad0bec19c60a039d54ac6bc55f6a1acaa54f
Nishan Bende <nishanbende@gmail.com>
2021-06-18 18:18:38 +0530

    fix: focus in textinput due to keyboard avoid view

---
+++ src/utils/useKeyboardBottomInset.ts
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Keyboard, Platform, KeyboardEvent } from 'react-native';
+
+export const useKeyboardBottomInset = () => {
+  const [bottom, setBottom] = React.useState(0);
+  const subscriptions = React.useRef<any[]>([]);
+
+  React.useEffect(() => {
+    function onKeyboardChange(e: KeyboardEvent) {
+      if (
+        e.startCoordinates &&
+        e.endCoordinates.screenY < e.startCoordinates.screenY
+      )
+        setBottom(e.endCoordinates.height / 2);
+      else setBottom(0);
+    }
+
+    if (Platform.OS === 'ios') {
+      subscriptions.current = [
+        Keyboard.addListener('keyboardWillChangeFrame', onKeyboardChange),
+      ];
+    } else {
+      subscriptions.current = [
+        Keyboard.addListener('keyboardDidHide', onKeyboardChange),
+        Keyboard.addListener('keyboardDidShow', onKeyboardChange),
+      ];
+    }
+    return () => {
+      subscriptions.current.forEach((subscription) => {
+        subscription.remove();
+      });
+    };
+  }, [setBottom, subscriptions]);
+
+  return bottom;
+};
