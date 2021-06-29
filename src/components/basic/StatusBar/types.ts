fc98ba76e00b518b573be50b8a4e8b8dc8847f40
MD-REHMAN <rehman.2468@gmail.com>
2021-06-29 14:00:14 +0530

    fix: removed unrequired from statusbar

--- src/components/basic/StatusBar/types.ts
+++ src/components/basic/StatusBar/types.ts
@@ -1,32 +1,3 @@
 import type { StatusBarProps } from 'react-native';
-import type {
-  BorderProps,
-  ColorProps,
-  FlexboxProps,
-  LayoutProps,
-  PositionProps,
-  SpaceProps,
-  ExtraProps,
-  OutlineProps,
-  ShadowProps,
-  BackgroundProps,
-  SafeAreaProps,
-  TransformProps,
-  PlatformProps,
-} from '../../types';
 
-export interface IStatusBarProps
-  extends StatusBarProps,
-    Omit<ColorProps, 'backgroundColor'>,
-    SpaceProps,
-    LayoutProps,
-    FlexboxProps,
-    PositionProps,
-    BorderProps,
-    ExtraProps,
-    OutlineProps,
-    ShadowProps,
-    Omit<BackgroundProps, 'backgroundColor'>,
-    SafeAreaProps,
-    TransformProps,
-    PlatformProps {}
+export interface IStatusBarProps extends StatusBarProps {}
