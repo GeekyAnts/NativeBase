619eb823b2285adb05b15d2828f99f6f0ff448a3
Vidhi Kataria <vidhikataria@Vidhis-MacBook-Pro.local>
2021-07-06 12:32:02 +0530

    feat: api and example added

---
+++ src/theme/components/card.ts
@@ -0,0 +1,12 @@
+const baseStyle = {
+  shadow: 4,
+  borderRadius: 'md',
+  padding: 4,
+  overflow: 'hidden',
+  _dark: { bg: 'gray.700' },
+};
+const defaultProps = {};
+export default {
+  baseStyle,
+  defaultProps,
+};
