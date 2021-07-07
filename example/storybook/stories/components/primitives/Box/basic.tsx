064408b2eaebfee8721ba36075ca1b724eda34f0
Nishan Bende <nishanbende@gmail.com>
2021-07-07 12:15:07 +0530

    fix: add more comments in platform specific space units

--- example/storybook/stories/components/primitives/Box/basic.tsx
+++ example/storybook/stories/components/primitives/Box/basic.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import { Box } from 'native-base';
 export const Example = () => {
   return (
-    <Box
-      bg="primary.400"
-      p={4}
-      _text={{ fontSize: 'md', fontWeight: 'bold', color: 'white' }}
-    >
-      This is a Box
-    </Box>
+    <>
+      <Box
+        bg="primary.400"
+        _text={{
+          fontSize: 'md',
+          fontWeight: 'bold',
+          color: 'white',
+          letterSpacing: '5xl',
+        }}
+      >
+        This is a Box
+      </Box>
+      <Box
+        bg="primary.400"
+        _text={{ fontSize: 'md', fontWeight: 'bold', color: 'white' }}
+      >
+        This is a Box
+      </Box>
+    </>
   );
 };
