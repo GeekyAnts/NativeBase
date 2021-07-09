6f31d88a581742ab36cf622876f9b18f667c9d53
amars29 <amars@geekyants.com>
2021-07-09 16:49:41 +0530

    fix: storybook example - stack, textarea, pressable

--- example/storybook/stories/components/primitives/ZStack/example.tsx
+++ example/storybook/stories/components/primitives/ZStack/example.tsx
@@ -3,10 +3,26 @@ import { ZStack, Box } from 'native-base';
 
 export const Example = () => {
   return (
-    <ZStack mt={-20} ml={-20}>
-      <Box bg="primary.400" size={20} rounded="lg" shadow={3} />
-      <Box bg="secondary.400" mt={5} ml={5} size={20} rounded="lg" shadow={5} />
-      <Box bg="emerald.400" mt={10} ml={10} size={20} rounded="lg" shadow={7} />
-    </ZStack>
+    <Box h="40" mx="auto">
+      <ZStack mt={3} ml={-20}>
+        <Box bg="primary.400" size={20} rounded="lg" shadow={3} />
+        <Box
+          bg="secondary.400"
+          mt={5}
+          ml={5}
+          size={20}
+          rounded="lg"
+          shadow={5}
+        />
+        <Box
+          bg="emerald.400"
+          mt={10}
+          ml={10}
+          size={20}
+          rounded="lg"
+          shadow={7}
+        />
+      </ZStack>
+    </Box>
   );
 };
