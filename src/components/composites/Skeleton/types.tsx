f6cc3a55ab38913f1195033c66ade61af6d1eb80
Vidhi Kataria <vidhikataria@Vidhis-MacBook-Pro.local>
2021-07-13 12:36:26 +0530

    fix: types format fix

--- src/components/composites/Skeleton/types.tsx
+++ src/components/composites/Skeleton/types.tsx
@@ -1,7 +1,6 @@
 import type { IBoxProps } from '../../primitives';
-import type { ITextProps } from '../../primitives';
 
-export type ISkeletonProps = IBoxProps & {
+export interface ISkeletonProps extends IBoxProps {
   /**
    * The fadeIn duration in seconds
    */
@@ -26,43 +25,42 @@ export type ISkeletonProps = IBoxProps & {
    * Sizes for Skeleton
    */
   size?: string | number;
-};
+}
 
-export type ISkeletonTextProps = ITextProps &
-  IBoxProps & {
-    /**
-     * The fadeIn duration in seconds
-     */
-    fadeDuration?: number;
-    /**
-     * If true, it'll render its children
-     */
-    isLoaded?: boolean;
-    /**
-     * The animation speed in seconds
-     */
-    speed?: number;
-    /**
-     * The color at the animation start
-     */
-    startColor?: string;
-    /**
-     * The color at the animation end
-     */
-    endColor?: string;
-    /**
-     * No of Lines in text
-     */
-    noOfLines?: number;
-    /**
-     * Space between two lines
-     */
-    space?: number | string;
-    /**
-     * VStack props
-     */
-    _stack?: any;
-  };
+export interface ISkeletonTextProps extends IBoxProps {
+  /**
+   * The fadeIn duration in seconds
+   */
+  fadeDuration?: number;
+  /**
+   * If true, it'll render its children
+   */
+  isLoaded?: boolean;
+  /**
+   * The animation speed in seconds
+   */
+  speed?: number;
+  /**
+   * The color at the animation start
+   */
+  startColor?: string;
+  /**
+   * The color at the animation end
+   */
+  endColor?: string;
+  /**
+   * No of Lines in text
+   */
+  noOfLines?: number;
+  /**
+   * Space between two lines
+   */
+  space?: number | string;
+  /**
+   * VStack props
+   */
+  _stack?: any;
+}
 
 export type ISkeletonComponentType = ((
   props: ISkeletonProps & { ref?: any }
