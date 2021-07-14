386cd4e66234f9acde565b653e32e9a81a72ff53
Nishan Bende <nishanbende@gmail.com>
2021-07-14 12:00:30 +0530

    feat: add render in portal prop to fab

--- src/components/composites/Fab/Fab.tsx
+++ src/components/composites/Fab/Fab.tsx
@@ -4,21 +4,28 @@ import type { IFabProps } from './types';
 import { usePropsResolution } from '../../../hooks/useThemeProps';
 import { OverlayContainer } from '@react-native-aria/overlays';
 
-const Fab = ({ label, icon, ...props }: IFabProps, ref: any) => {
+const Fab = (
+  { label, icon, renderInPortal = true, ...props }: IFabProps,
+  ref: any
+) => {
   const themeProps = usePropsResolution('FAB', props);
   const { placement, placementProps, ...newProps } = themeProps;
 
-  return (
-    <OverlayContainer>
-      <Button
-        {...placementProps[placement]}
-        ref={ref}
-        startIcon={icon}
-        {...newProps}
-      >
-        {label}
-      </Button>
-    </OverlayContainer>
+  const fabComponent = (
+    <Button
+      {...placementProps[placement]}
+      ref={ref}
+      startIcon={icon}
+      {...newProps}
+    >
+      {label}
+    </Button>
+  );
+
+  return renderInPortal ? (
+    <OverlayContainer>{fabComponent}</OverlayContainer>
+  ) : (
+    fabComponent
   );
 };
 
