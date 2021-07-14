9a07f7806d1f6f75061193e41786ff197a85df41
Nishan Bende <nishanbende@gmail.com>
2021-07-14 14:25:25 +0530

    fix: change container's theming

--- src/theme/components/container.ts
+++ src/theme/components/container.ts
@@ -1,7 +1,41 @@
 const baseStyle = {
-  maxWidth: '80%',
+  mx: 'auto',
+  maxWidth: {
+    base: '100%',
+    sm: '93%',
+    md: '93%',
+    lg: '95%',
+  },
+};
+
+const sizes = {
+  sm: {
+    base: '100%',
+    sm: '93%',
+    md: '93%',
+    lg: '95%',
+  },
+  md: {
+    base: '100%',
+    sm: '100%',
+    md: '93%',
+    lg: '95%',
+  },
+  lg: {
+    base: '100%',
+    sm: '100%',
+    md: '100%',
+    lg: '95%',
+  },
+  xl: {
+    base: '100%',
+    sm: '100%',
+    md: '100%',
+    lg: '100%',
+  },
 };
 
 export default {
   baseStyle,
+  sizes,
 };
