10e2fd5937a34cb1ac9f30d3b339627f901134c5
MD-REHMAN <rehman.2468@gmail.com>
2021-07-15 17:46:27 +0530

    fix: simplified API and support for internal prop

--- example/storybook/stories/components/composites/IconButton/Basic.tsx
+++ example/storybook/stories/components/composites/IconButton/Basic.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { IconButton, Icon } from 'native-base';
+import { IconButton, Icon, VStack } from 'native-base';
 import { AntDesign } from '@expo/vector-icons';
 
 export const Example = () => {
   return (
-    <IconButton
-      variant="solid"
-      icon={<Icon size="md" as={<AntDesign name="search1" />} color="white" />}
-    />
+    <VStack space={4}>
+      <IconButton
+        colorScheme="red"
+        variant="solid"
+        icon={<Icon as={AntDesign} name="search1" />}
+        _icon={{
+          color: 'amber.100',
+        }}
+      />
+      <IconButton
+        colorScheme="emerald"
+        variant="outline"
+        _icon={{
+          as: AntDesign,
+          name: 'home',
+        }}
+      />
+    </VStack>
   );
 };
