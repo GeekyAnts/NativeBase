06f79e58e589e46ed8b482fb6b9b408b71f88495
shrutiag14 <48885655+shrutiag14@users.noreply.github.com>
2021-07-26 08:43:43 +0530

    Update Body.js

--- src/basic/Body.js
+++ src/basic/Body.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import React, { Component, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { View, ViewPropTypes } from 'react-native';
 import { connectStyle } from 'native-base-shoutem-theme';
@@ -9,6 +10,11 @@ class Body extends Component {
   render() {
     return <View ref={c => (this._root = c)} {...this.props} />;
   }
+const Body = (props) => {
+  const root = useRef(null);
+
+  return <View ref={root} {...props} />;
+
 }
 
 Body.propTypes = {
@@ -19,9 +25,7 @@ Body.propTypes = {
     PropTypes.array
   ])
 };
-
 const StyledBody = connectStyle('NativeBase.Body', {}, mapPropsToStyleNames)(
   Body
 );
-
 export { StyledBody as Body };
