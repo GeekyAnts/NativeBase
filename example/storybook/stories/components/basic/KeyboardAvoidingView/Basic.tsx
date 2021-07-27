9da8c9f347d3427344bdaa613d1ad9e464c75b96
amars29 <amars@geekyants.com>
2021-07-27 19:12:30 +0530

    fix: keyboardavoidingview in ios

--- example/storybook/stories/components/basic/KeyboardAvoidingView/Basic.tsx
+++ example/storybook/stories/components/basic/KeyboardAvoidingView/Basic.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import { Input, KeyboardAvoidingView, Text, Button, VStack } from 'native-base';
+import {
+  Input,
+  KeyboardAvoidingView,
+  Text,
+  Button,
+  VStack,
+  useBreakpointValue,
+} from 'native-base';
 import { Platform } from 'react-native';
 
 export const Example = () => {
+  const isLargeScreen = useBreakpointValue({
+    base: false,
+    sm: false,
+    md: false,
+    lg: true,
+  });
   return (
-    <KeyboardAvoidingView h={{ base: '600px', lg: 'auto' }}>
-      {Platform.OS === 'web' ? (
+    <KeyboardAvoidingView
+      h={{ base: '600px', lg: 'auto' }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={192}
+    >
+      {isLargeScreen ? (
         <Text>Please see the example in your mobile to observe the effect</Text>
       ) : (
         <VStack p={6} flex={1} justifyContent="space-around">
