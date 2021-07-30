6ea1638d28cbb2fa2cd4d712dfaec55a1cd80824
meenu <makkarmini007@gmail.com>
2021-07-30 17:48:44 +0530

    fix: added interaction styling props

--- src/components/primitives/Checkbox/Checkbox.tsx
+++ src/components/primitives/Checkbox/Checkbox.tsx
@@ -10,6 +10,7 @@ import { useToggleState } from '@react-stately/toggle';
 import { CheckboxGroupContext } from './CheckboxGroup';
 import { useCheckbox, useCheckboxGroupItem } from '@react-native-aria/checkbox';
 import { CheckIcon } from '../Icon/Icons';
+import { extractInObject, stylingProps } from '../../../theme/tools/utils';
 
 const Checkbox = (
   { icon, children, wrapperRef, ...props }: ICheckboxProps,
@@ -19,15 +20,18 @@ const Checkbox = (
 
   const checkboxGroupContext = React.useContext(CheckboxGroupContext);
   const {
-    _interactionBox: { _pressed: _iterationBoxPressed, ..._interactionBox },
-    _checkbox: {
-      _checked: _checboxChecked,
-      _disabled: _checkboxDisabled,
-      _invalid: _checkboxInvalid,
-      ..._checkbox
-    },
+    _interactionBox,
+    _disabled,
+    _invalid,
+    _pressed,
+    _checked,
+    _unchecked,
+    _indeterminate,
     _icon,
+    _readOnly,
     isInvalid,
+    isReadOnly,
+    isIndeterminate,
     ...themedProps
   } = usePropsResolution('Checkbox', {
     ...checkboxGroupContext,
@@ -42,6 +46,13 @@ const Checkbox = (
   });
   const groupState = useContext(CheckboxGroupContext);
 
+  const [layoutProps, nonLayoutProps] = extractInObject(themedProps, [
+    ...stylingProps.margin,
+    ...stylingProps.layout,
+    ...stylingProps.flexbox,
+    ...stylingProps.position,
+  ]);
+
   // Swap hooks depending on whether this checkbox is inside a CheckboxGroup.
   // This is a bit unorthodox. Typically, hooks cannot be called in a conditional,
   // but since the checkbox won't move in and out of a group, it should be safe.
@@ -49,47 +60,102 @@ const Checkbox = (
     ? // eslint-disable-next-line react-hooks/rules-of-hooks
       useCheckboxGroupItem(
         {
-          ...themedProps,
+          ...nonLayoutProps,
           value: themedProps.value,
         },
         groupState.state,
         ref
       )
     : // eslint-disable-next-line react-hooks/rules-of-hooks
-      useCheckbox(themedProps, state, ref);
+      useCheckbox(nonLayoutProps, state, ref);
 
-  const { checked, disabled } = inputProps;
+  const [, pressableProps] = extractInObject(inputProps, [
+    ...stylingProps.border,
+    ...stylingProps.background,
+    ...stylingProps.flexbox,
+    ...stylingProps.layout,
+    ...stylingProps.padding,
+    ...stylingProps.margin,
+  ]);
 
-  const sizedIcon = icon
-    ? () =>
-        React.cloneElement(
-          icon,
-          {
-            ..._icon,
-          },
-          icon.props.children
-        )
-    : null;
+  const sizedIcon = (icon: JSX.Element, _icon: any) =>
+    React.cloneElement(
+      icon,
+      {
+        ..._icon,
+      },
+      icon.props.children
+    );
+
+  const { checked: isChecked, disabled: isDisabled } = inputProps;
+
+  function iconResolver(isPressed?: any) {
+    if (sizedIcon) {
+      if (isDisabled && _disabled?.icon) {
+        return sizedIcon(_disabled?.icon, {
+          ..._icon,
+          ..._disabled?._icon,
+        });
+      } else if (isReadOnly && _readOnly?.icon) {
+        return sizedIcon(_readOnly?.icon, {
+          ..._icon,
+          ..._readOnly?._icon,
+        });
+      } else if (isInvalid && _invalid?.icon) {
+        return sizedIcon(_invalid?.icon, {
+          ..._icon,
+          ..._invalid?._icon,
+        });
+      } else if (isPressed && _pressed?.icon) {
+        return sizedIcon(_pressed?.icon, {
+          ..._icon,
+          ..._pressed?._icon,
+        });
+      } else if (!isChecked && _unchecked?.icon) {
+        return sizedIcon(_unchecked?.icon, { ..._icon, ..._unchecked?._icon });
+      } else if (isChecked) {
+        if (_checked?.icon) {
+          return sizedIcon(_checked?.icon, { ..._icon, ..._checked?._icon });
+        } else if (icon) {
+          return sizedIcon(icon, { ..._icon });
+        } else {
+          return <CheckIcon name="check" {..._icon} opacity={1} />;
+        }
+      } else if (isIndeterminate && _indeterminate?.icon) {
+        return sizedIcon(_indeterminate?.icon, {
+          ..._icon,
+          ..._indeterminate?._icon,
+        });
+      }
+    }
+    return <CheckIcon name="check" {..._icon} opacity={isChecked ? 1 : 0} />;
+  }
   return (
     <Pressable
-      {...(inputProps as IPressableProps)}
+      {...(pressableProps as IPressableProps)}
       ref={mergeRefs([ref, wrapperRef])}
       accessibilityRole="checkbox"
     >
       {({ isPressed }: any) => {
         return (
           <Center
+            {...layoutProps}
             flexDirection="row"
             justifyContent="center "
             alignItems="center"
             borderRadius="full"
-            {...themedProps}
           >
             <Center>
               {/* Interaction Wrapper */}
               <Box
                 {..._interactionBox}
-                {...(isPressed && _iterationBoxPressed)}
+                {...(!isChecked && _unchecked?._interactionBox)}
+                {...(isChecked && _checked?._interactionBox)}
+                {...(isIndeterminate && _indeterminate?._interactionBox)}
+                {...(isPressed && _pressed._interactionBox)}
+                {...(isInvalid && _invalid?._interactionBox)}
+                {...(isReadOnly && _readOnly?._interactionBox)}
+                {...(isDisabled && _disabled?._interactionBox)}
                 p={5}
                 w="100%"
                 height="100%"
@@ -97,16 +163,16 @@ const Checkbox = (
               />
               {/* Checkbox */}
               <Center
-                {..._checkbox}
-                {...(checked && _checboxChecked)}
-                {...(disabled && _checkboxDisabled)}
-                {...(isInvalid && _checkboxInvalid)}
+                {...nonLayoutProps}
+                {...(!isChecked && _unchecked)}
+                {...(isChecked && _checked)}
+                {...(isPressed && _pressed)}
+                {...(isIndeterminate && _indeterminate)}
+                {...(isInvalid && _invalid)}
+                {...(isReadOnly && _readOnly)}
+                {...(isDisabled && _disabled)}
               >
-                {icon && sizedIcon && checked ? (
-                  sizedIcon()
-                ) : (
-                  <CheckIcon {..._icon} opacity={checked ? 1 : 0} />
-                )}
+                {iconResolver(isPressed)}
               </Center>
             </Center>
             {/* Label */}
