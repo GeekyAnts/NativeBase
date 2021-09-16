8da6daf6a8ccc71d495f588632414427761c5471
Vidhi Kataria <vidhikataria@Vidhis-MacBook-Pro.local>
2021-09-16 13:45:30 +0530

    fix: badge-design

--- src/theme/components/badge.ts
+++ src/theme/components/badge.ts
@@ -1,25 +1,29 @@
 import { mode, getColorScheme } from '../tools';
 
 const baseStyle = {
-  px: 1,
-  _text: { fontSize: 'xs', fontWeight: 'bold' },
+  px: 2,
+  py: '2px',
+  alignItems: 'center',
+  _text: { fontSize: 'xs', fontWeight: 'medium' },
 };
 
 function variantSolid(props: Record<string, any>) {
   let colorScheme = getColorScheme(props);
   return {
-    bg: mode(`${colorScheme}.500`, `${colorScheme}.200`)(props),
+    bg: mode(`${colorScheme}.600`, `${colorScheme}.200`)(props),
     borderWidth: 1,
     borderColor: 'transparent',
+    borderRadius: 2,
   };
 }
 
 function variantSubtle(props: Record<string, any>) {
   let colorScheme = getColorScheme(props);
   return {
-    bg: mode(`${colorScheme}.300`, `${colorScheme}.600`)(props),
-    _text: { color: mode(`${colorScheme}.900`, `${colorScheme}.100`)(props) },
+    bg: mode(`${colorScheme}.200`, `${colorScheme}.600`)(props),
+    _text: { color: mode(`${colorScheme}.600`, `${colorScheme}.100`)(props) },
     borderWidth: 1,
+    borderRadius: 2,
     borderColor: 'transparent',
   };
 }
@@ -29,6 +33,7 @@ function variantOutline(props: Record<string, any>) {
   return {
     borderColor: mode(`${colorScheme}.500`, `${colorScheme}.400`)(props),
     _text: { color: mode(`${colorScheme}.500`, `${colorScheme}.400`)(props) },
+    borderRadius: 2,
     borderWidth: 1,
   };
 }
@@ -41,7 +46,7 @@ const variants = {
 
 const defaultProps = {
   variant: 'subtle',
-  colorScheme: 'muted',
+  colorScheme: 'coolGray',
 };
 
 export default {
