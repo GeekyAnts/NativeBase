12213b91db1dc10bcc8cb18cbc79e5e61f63123a
Vidhi Kataria <vidhikataria@Vidhis-MacBook-Pro.local>
2021-09-16 18:13:59 +0530

    fix: stack design

--- example/storybook/stories/components/primitives/Stack/basic.tsx
+++ example/storybook/stories/components/primitives/Stack/basic.tsx
@@ -12,126 +12,162 @@ import {
 export function Example() {
   return (
     <ScrollView>
-      <VStack space="6">
-        <Heading>row</Heading>
-        <Stack direction="row" space={3}>
+      <VStack space="2.5" mt="4">
+        <Heading size="md">row</Heading>
+        <Stack direction="row" mb={'2.5'} mt={'1.5'} space={'3'}>
           <Center
             size="16"
-            bg="primary.400"
-            rounded="md"
-            _text={{ color: 'white' }}
-            shadow={3}
+            bg="primary.500"
+            rounded="sm"
+            _text={{
+              color: 'warmGray.50',
+              fontWeight: 'medium',
+            }}
+            shadow={'3'}
           >
             Box 1
           </Center>
           <Center
-            bg="secondary.400"
+            bg="secondary.500"
             size="16"
-            rounded="md"
-            _text={{ color: 'white' }}
-            shadow={3}
+            rounded="sm"
+            _text={{
+              color: 'warmGray.50',
+              fontWeight: 'medium',
+            }}
+            shadow={'3'}
           >
             Box 2
           </Center>
           <Center
             size="16"
-            bg="emerald.400"
-            rounded="md"
-            _text={{ color: 'white' }}
-            shadow={3}
+            bg="emerald.500"
+            rounded="sm"
+            _text={{
+              color: 'warmGray.50',
+              fontWeight: 'medium',
+            }}
+            shadow={'3'}
           >
             Box 3
           </Center>
         </Stack>
         <Divider />
-        <Heading>column</Heading>
-        <Stack direction="column" space={3}>
+        <Heading size="md">column</Heading>
+        <Stack mb={'2.5'} mt={1.5} direction="column" space={'3'}>
           <Center
             size="16"
-            bg="primary.400"
-            rounded="md"
-            _text={{ color: 'white' }}
-            shadow={3}
+            bg="primary.500"
+            rounded="sm"
+            _text={{
+              color: 'warmGray.50',
+              fontWeight: 'medium',
+            }}
+            shadow={'3'}
           >
             Box 1
           </Center>
           <Center
-            bg="secondary.400"
+            bg="secondary.500"
             size="16"
-            rounded="md"
-            _text={{ color: 'white' }}
-            shadow={3}
+            rounded="sm"
+            _text={{
+              color: 'warmGray.50',
+              fontWeight: 'medium',
+            }}
+            shadow={'3'}
           >
             Box 2
           </Center>
           <Center
             size="16"
-            bg="emerald.400"
-            rounded="md"
-            _text={{ color: 'white' }}
-            shadow={3}
+            bg="emerald.500"
+            rounded="sm"
+            _text={{
+              color: 'warmGray.50',
+              fontWeight: 'medium',
+            }}
+            shadow={'3'}
           >
             Box 3
           </Center>
         </Stack>
         <Divider />
-        <Heading>row-reverse</Heading>
-        <Stack direction="row" reversed space={3}>
+        <Heading size="md">row-reverse</Heading>
+        <Stack mb={'2.5'} mt={1.5} direction="row" reversed space={'3'}>
           <Center
             size="16"
-            bg="primary.400"
-            rounded="md"
-            _text={{ color: 'white' }}
-            shadow={3}
+            bg="primary.500"
+            rounded="sm"
+            _text={{
+              color: 'warmGray.50',
+              fontWeight: 'medium',
+            }}
+            shadow={'3'}
           >
             Box 1
           </Center>
           <Center
-            bg="secondary.400"
+            bg="secondary.500"
             size="16"
-            rounded="md"
-            _text={{ color: 'white' }}
-            shadow={3}
+            rounded="sm"
+            _text={{
+              color: 'warmGray.50',
+              fontWeight: 'medium',
+            }}
+            shadow={'3'}
           >
             Box 2
           </Center>
           <Center
             size="16"
-            bg="emerald.400"
-            rounded="md"
-            _text={{ color: 'white' }}
-            shadow={3}
+            bg="emerald.500"
+            rounded="sm"
+            _text={{
+              color: 'warmGray.50',
+              fontWeight: 'medium',
+            }}
+            shadow={'3'}
           >
             Box 3
           </Center>
         </Stack>
         <Divider />
-        <Heading>column-reverse</Heading>
-        <Stack direction="column-reverse" space={3}>
+        <Heading size="md">column-reverse</Heading>
+        <Stack mb={'2.5'} mt={1.5} direction="column-reverse" space={'3'}>
           <Center
             size="16"
-            bg="primary.400"
-            rounded="md"
-            _text={{ color: 'white' }}
-            shadow={3}
+            bg="primary.500"
+            rounded="sm"
+            _text={{
+              color: 'warmGray.50',
+              fontWeight: 'medium',
+            }}
+            shadow={'3'}
           >
             Box 1
           </Center>
           <Center
-            bg="secondary.400"
+            bg="secondary.500"
             size="16"
-            rounded="md"
-            _text={{ color: 'white' }}
-            shadow={3}
+            rounded="sm"
+            _text={{
+              color: 'warmGray.50',
+              fontWeight: 'medium',
+            }}
+            shadow={'3'}
           >
             Box 2
           </Center>
           <Center
             size="16"
-            bg="emerald.400"
-            rounded="md"
-            _text={{ color: 'white' }}
-            shadow={3}
+            bg="emerald.500"
+            rounded="sm"
+            _text={{
+              color: 'warmGray.50',
+              fontWeight: 'medium',
+            }}
+            shadow={'3'}
           >
             Box 3
           </Center>
--- example/storybook/stories/components/primitives/VStack/basic.tsx
+++ example/storybook/stories/components/primitives/VStack/basic.tsx
@@ -4,10 +4,12 @@ import { VStack, Center, Heading } from 'native-base';
 export function Example() {
   return (
     <VStack space={4} alignItems="center">
-      <Heading>VStack</Heading>
-      <Center w="64" h="20" bg="primary.400" rounded="md" shadow={3}></Center>
-      <Center w="64" h="20" bg="secondary.400" rounded="md" shadow={3}></Center>
-      <Center w="64" h="20" bg="emerald.400" rounded="md" shadow={3}></Center>
+      <Heading textAlign="center" mb="10">
+        VStack
+      </Heading>
+      <Center w="64" h="20" bg="primary.500" rounded="md" shadow={3} />
+      <Center w="64" h="20" bg="secondary.500" rounded="md" shadow={3} />
+      <Center w="64" h="20" bg="emerald.500" rounded="md" shadow={3} />
     </VStack>
   );
 }
