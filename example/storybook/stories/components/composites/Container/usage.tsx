32aa57477f506f4677b62ad1b84e595ed66eff99
Vidhi Kataria <vidhikataria@Vidhis-MacBook-Pro.local>
2021-09-16 16:56:31 +0530

    fix: design dark mode

--- example/storybook/stories/components/composites/Container/usage.tsx
+++ example/storybook/stories/components/composites/Container/usage.tsx
@@ -5,9 +5,9 @@ export function Example() {
     <Container>
       <Heading>
         A component library for the
-        <Heading color="emerald.400"> React Ecosystem</Heading>
+        <Heading color="emerald.500"> React Ecosystem</Heading>
       </Heading>
-      <Text pt="3" fontWeight="md">
+      <Text pt={3} fontWeight={'medium'}>
         NativeBase is a simple, modular and accessible component library that
         gives you building blocks to build you React applications.
       </Text>
