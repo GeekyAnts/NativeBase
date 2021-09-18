48db44c7fd74dbd9a7e60626837032ef6a2a82f1
Rohit Singh <rohit99.ind@gmail.com>
2021-09-18 10:30:44 +0530

    feat: new transition examples

--- example/storybook/stories/components/composites/Transitions/SlideFade.tsx
+++ example/storybook/stories/components/composites/Transitions/SlideFade.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
-import { PresenceTransition, Button, Box } from 'native-base';
+import { PresenceTransition, Button, Center } from 'native-base';
 export const Example = () => {
   const [isOpen, setIsOpen] = React.useState(false);
 
   return (
     <>
       <Button onPress={() => setIsOpen(!isOpen)}>
-        Click Me to {isOpen ? 'hide' : 'show'}
+        {isOpen ? 'Hide' : 'Show'}
       </Button>
       <PresenceTransition
         visible={isOpen}
         initial={{ translateX: -20, opacity: 0 }}
         animate={{ translateX: 0, opacity: 1 }}
       >
-        <Box
-          p="40px"
+        <Center
+          w="200"
+          h="100"
           _text={{ color: 'white' }}
           mt="4"
           bg="teal.500"
           rounded="md"
         >
-          PresenceTransition
-        </Box>
+          SlideFade
+        </Center>
       </PresenceTransition>
     </>
   );
