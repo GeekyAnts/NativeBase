f77f9886fee992fd8b293574bded03bea53bdeb7
meenu <makkarmini007@gmail.com>
2021-09-20 18:58:44 +0530

    fix: keyboardavoidingview example updated

--- example/storybook/stories/components/basic/KeyboardAvoidingView/Basic.tsx
+++ example/storybook/stories/components/basic/KeyboardAvoidingView/Basic.tsx
@@ -5,6 +5,7 @@ import {
   Text,
   Button,
   VStack,
+  Heading,
   useBreakpointValue,
 } from 'native-base';
 import { Platform } from 'react-native';
@@ -21,22 +22,22 @@ export const Example = () => {
       h={{ base: '600px', lg: 'auto' }}
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
     >
-      {isLargeScreen ? (
-        <Text>Please see the example in your mobile to observe the effect</Text>
-      ) : (
-        <VStack p={6} flex={1} justifyContent="space-around">
-          <Text fontSize={36} mb={12}>
-            Header
+      <VStack p="6" flex="1" justifyContent="flex-end">
+        <Heading fontSize={36} mb="3">
+          Forgot Password
+        </Heading>
+        <Text>
+          Not to worry! Enter email address associated with your account and
+          we’ll send a link to reset your password.
+        </Text>
+        <Input placeholder="Email Address" mt="10" mb="4" />
+        <Button mb="4">Proceed</Button>
+        {isLargeScreen ? (
+          <Text>
+            Please see the example in your mobile to observe the effect.
           </Text>
-          <Input
-            placeholder="Username"
-            borderBottomWidth="1px"
-            mb={9}
-            mt="auto"
-          />
-          <Button variant="solid">Submit</Button>
-        </VStack>
-      )}
+        ) : null}
+      </VStack>
     </KeyboardAvoidingView>
   );
 };
