2eb30a60ac2d23de3ff1278e6a47fa62ba230cf5
MD-REHMAN <rehman.2468@gmail.com>
2021-09-21 15:58:52 +0530

    fix: review changes

--- src/theme/components/avatar-group.ts
+++ src/theme/components/avatar-group.ts
@@ -3,8 +3,8 @@ import { mode } from '../tools';
 function baseStyle(props: Record<string, any>) {
   return {
     borderWidth: 2,
-    borderColor: mode('light.50', 'gray.800')(props),
-    bg: mode('gray.600', 'light.100')(props),
+    borderColor: mode('gray.50', 'gray.800')(props),
+    bg: mode('gray.600', 'gray.100')(props),
     space: -4,
   };
 }
