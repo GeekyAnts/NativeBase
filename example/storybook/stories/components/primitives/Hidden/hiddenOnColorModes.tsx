140f2476a2b4d451647d6f0e1a64fa1659b92e47
Vidhi Kataria <vidhikataria@Vidhis-MacBook-Pro.local>
2021-09-23 17:24:11 +0530

    fix: example

--- example/storybook/stories/components/primitives/Hidden/hiddenOnColorModes.tsx
+++ example/storybook/stories/components/primitives/Hidden/hiddenOnColorModes.tsx
@@ -5,16 +5,20 @@ export function Example() {
   return (
     <Stack>
       <Box bg="orange.300" p={2}>
-        <Text>This text will be visible on every colormode.</Text>
+        <Text _dark={{ color: 'coolGray.800' }}>
+          This text will be visible on every colormode.
+        </Text>
       </Box>
       <Hidden colorMode="light">
-        <Box bg="red.400" p={2}>
-          <Text>This text will be hidden on light mode</Text>
+        <Box bg="red.400" p={2} mt={3}>
+          <Text _dark={{ color: 'coolGray.800' }}>{'  '}</Text>
         </Box>
       </Hidden>
       <Hidden colorMode="dark">
-        <Box bg="red.400" p={2}>
-          <Text>This text will be hidden on dark mode</Text>
+        <Box bg="red.400" p={2} mt={3}>
+          <Text _dark={{ color: 'coolGray.800' }}>
+            This text will be hidden on dark mode
+          </Text>
         </Box>
       </Hidden>
     </Stack>
