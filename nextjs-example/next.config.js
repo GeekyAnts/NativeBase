d9d24c3f864739779b418fcf161d5ca4f8f7d967
Nishan Bende <nishanbende@gmail.com>
2021-10-20 16:22:48 +0530

    feat: next example init

---
+++ nextjs-example/next.config.js
@@ -0,0 +1,8 @@
+// @generated: @expo/next-adapter@3.1.3
+// Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#withexpo
+
+const { withExpo } = require('@expo/next-adapter');
+
+module.exports = withExpo({
+  projectRoot: __dirname,
+});
