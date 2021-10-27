c4f4a5aa5e56df99f42b2a788d8c967704ae5f79
Rohit Singh <rohit99.ind@gmail.com>
2021-10-27 13:35:13 +0530

    feat: refactored aspectratio component

--- example/storybook/stories/components/composites/AspectRatio/EmbedImage.tsx
+++ example/storybook/stories/components/composites/AspectRatio/EmbedImage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AspectRatio, Image } from 'native-base';
-export const Example = () => {
+const Example = () => {
   return (
     <AspectRatio ratio={9 / 10} height={400}>
       <Image
@@ -9,8 +9,9 @@ export const Example = () => {
           uri:
             'https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
         }}
-        alt="nativebase tiny logo"
+        alt="Picture of a Flower"
       />
     </AspectRatio>
   );
 };
+export default Example;
