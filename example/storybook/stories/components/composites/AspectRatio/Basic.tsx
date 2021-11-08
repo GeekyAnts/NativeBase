2af1eda43e0ac52088754b7b208d7fd4c583c7ca
Rohit Singh <rohit99.ind@gmail.com>
2021-11-08 13:33:17 +0530

    fix: aspectratio example breaking docs build automation

--- example/storybook/stories/components/composites/AspectRatio/Basic.tsx
+++ example/storybook/stories/components/composites/AspectRatio/Basic.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { AspectRatio, Box } from 'native-base';
 
-const Example = () => {
+export const Example = () => {
   return (
     <AspectRatio height={200} ratio={{ base: 4 / 3, md: 16 / 9 }}>
       <Box bg="red.400" />
     </AspectRatio>
   );
 };
-
-export default Example;
