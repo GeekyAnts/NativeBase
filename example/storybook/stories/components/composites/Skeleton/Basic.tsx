240bec180fb0890499bfe8f3c4cfe119d3136b77
MD-REHMAN <rehman.2468@gmail.com>
2021-11-15 20:23:39 +0530

    fix: updated skeleton api

--- example/storybook/stories/components/composites/Skeleton/Basic.tsx
+++ example/storybook/stories/components/composites/Skeleton/Basic.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Skeleton, Stack } from 'native-base';
+import { Skeleton, VStack } from 'native-base';
 export const Example = () => {
   return (
-    <Stack w="50%" space={8}>
-      <Skeleton h={20}></Skeleton>
-      <Skeleton.Circle size={20} />
-      <Skeleton.Text noOfLines={5} lineHeight={3} h={90}></Skeleton.Text>
-    </Stack>
+    <VStack
+      w="400"
+      borderWidth="1"
+      space={8}
+      overflow="hidden"
+      rounded="md"
+      borderColor="coolGray.200"
+    >
+      <Skeleton h="40" />
+      <Skeleton.Text px="4" />
+      <Skeleton m="4" rounded="md" startColor="primary.100" />
+    </VStack>
   );
 };
--- example/storybook/stories/components/composites/Skeleton/Composition.tsx
+++ example/storybook/stories/components/composites/Skeleton/Composition.tsx
@@ -1,60 +1,33 @@
-import React, { useState } from 'react';
-import {
-  Box,
-  Skeleton,
-  VStack,
-  Text,
-  Avatar,
-  HStack,
-  Image,
-} from 'native-base';
-
-function CompositionComponent() {
-  const [loaded, setLoaded] = useState(false);
-  setTimeout(() => {
-    setLoaded(true);
-  }, 5000);
-  return (
-    <VStack>
-      <HStack alignItems="center">
-        <Skeleton.Circle size={12} isLoaded={loaded}>
-          <Avatar
-            source={{
-              uri: 'https://nativebase.github.io/img/native-base-icon.png',
-            }}
-          />
-        </Skeleton.Circle>
-
-        <Skeleton.Text
-          isLoaded={loaded}
-          w="50%"
-          ml={2}
-          _stack={{ justifyContent: 'center' }}
-        >
-          <Text fontWeight="bold" fontSize="lg" my="auto" ml={2}>
-            Nativebase
-          </Text>
-        </Skeleton.Text>
-      </HStack>
-      <Box size={200} mt={1}>
-        <Skeleton height="100%" isLoaded={loaded}>
-          <Image
-            source={{
-              uri: 'https://www.w3schools.com/css/img_lights.jpg',
-            }}
-            alt="NativeBase logo"
-            size={200}
-          />
-        </Skeleton>
-      </Box>
-    </VStack>
-  );
-}
-
+import React from 'react';
+import { Skeleton, VStack, HStack } from 'native-base';
 export const Example = () => {
   return (
-    <VStack space={4}>
-      <CompositionComponent />
+    <VStack
+      w="400"
+      borderWidth="1"
+      space={6}
+      rounded="md"
+      alignItems="center"
+      borderColor="coolGray.200"
+    >
+      <Skeleton h="40" />
+      <Skeleton
+        borderWidth={1}
+        borderColor="coolGray.200"
+        endColor="warmGray.50"
+        size="20"
+        rounded="full"
+        mt="-70"
+      />
+      <HStack space="2">
+        <Skeleton size="5" rounded="full" />
+        <Skeleton size="5" rounded="full" />
+        <Skeleton size="5" rounded="full" />
+        <Skeleton size="5" rounded="full" />
+        <Skeleton size="5" rounded="full" />
+      </HStack>
+      <Skeleton.Text lines={3} alignItems="center" px="12" />
+      <Skeleton mb="3" w="40" rounded="20" />
     </VStack>
   );
 };
