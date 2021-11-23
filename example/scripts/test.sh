6535960405ef552eb156561af160b297db91531a
Aditya Jamuar <adityaj@geekyants.com>
2021-11-23 18:01:28 +0530

    feat: all tests working

--- example/scripts/test.sh
+++ example/scripts/test.sh
@@ -16,10 +16,12 @@ if [ $# -eq 0 ]
   then
     jest --config jest-ios.config.js --silent
     jest --config jest-android.config.js --silent
+    jest --silent
   else
     echo "Tests will be updated!"
     jest --config jest-ios.config.js -u --silent
     jest --config jest-android.config.js -u --silent
+    jest -u --silent
 fi
 
 yarn install  --check-files
