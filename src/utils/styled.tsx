9537a0c6fee588f94c5b5caa69104d47edcd4d7f
MD-REHMAN <rehman.2468@gmail.com>
2021-12-20 16:24:09 +0530

    fix: input background not applying

--- src/utils/styled.tsx
+++ src/utils/styled.tsx
@@ -6,7 +6,20 @@ export const makeStyledComponent = (Comp: any) => {
     const [style, restProps] = useStyledSystemPropsResolver(props);
     if (process.env.NODE_ENV === 'development' && debug) {
       /* eslint-disable-next-line */
-      console.log('style:: => ', style, ' restProps:: => ', restProps);
+      console.log(
+        `%cstyleSystem`,
+        'background: #4b5563; color: #d97706; font-weight: 700; padding: 2px 8px;'
+      );
+      /* eslint-disable-next-line */
+      console.log('%c props: ', 'color: #4ade80; font-weight: 700;', props);
+      /* eslint-disable-next-line */
+      console.log('%c style: ', 'color: #22d3ee; font-weight: 700;', style);
+      /* eslint-disable-next-line */
+      console.log(
+        '%c restProps: ',
+        'color: #22d3ee; font-weight: 700;',
+        restProps
+      );
     }
     return (
       <Comp {...restProps} style={style} ref={ref}>
