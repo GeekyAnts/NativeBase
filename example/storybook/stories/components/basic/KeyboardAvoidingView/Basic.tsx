655869bedb15513979ccfc85b5136d0f21957d2a
meenu <makkarmini007@gmail.com>
2022-01-12 19:47:16 +0530

    feat: docs example fixes

--- example/storybook/stories/components/basic/KeyboardAvoidingView/Basic.tsx
+++ example/storybook/stories/components/basic/KeyboardAvoidingView/Basic.tsx
@@ -6,38 +6,30 @@ import {
   Button,
   VStack,
   Heading,
-  useBreakpointValue,
+  Center,
 } from 'native-base';
 import { Platform } from 'react-native';
 
 export const Example = () => {
-  const isLargeScreen = useBreakpointValue({
-    base: false,
-    sm: false,
-    md: false,
-    lg: true,
-  });
   return (
     <KeyboardAvoidingView
-      h={{ base: '400px', lg: 'auto' }}
+      h={{
+        base: '400px',
+        lg: 'auto',
+      }}
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
     >
-      <VStack p="6" flex="1" justifyContent="flex-end">
-        <Heading fontSize={36} mb="3">
-          Forgot Password
-        </Heading>
-        <Text>
-          Not to worry! Enter email address associated with your account and
-          we’ll send a link to reset your password.
-        </Text>
-        <Input placeholder="Email Address" mt="10" mb="4" />
-        <Button mb="4">Proceed</Button>
-        {isLargeScreen ? (
-          <Text>
-            Please see the example in your mobile to observe the effect.
+      <Center>
+        <VStack flex="1" justifyContent="flex-end" w="100%" maxW="300">
+          <Heading mb="3">Forgot Password</Heading>
+          <Text color="muted.400">
+            Not to worry! Enter email address associated with your account and
+            we’ll send a link to reset your password.
           </Text>
-        ) : null}
-      </VStack>
+          <Input placeholder="Email Address" mt="10" mb="4" />
+          <Button mb="4">Proceed</Button>
+        </VStack>
+      </Center>
     </KeyboardAvoidingView>
   );
 };
--- example/storybook/stories/examples/Signin.tsx
+++ example/storybook/stories/examples/Signin.tsx
@@ -9,68 +9,71 @@ import {
   Link,
   Button,
   HStack,
+  Center,
 } from 'native-base';
 
 export const Example = () => {
   return (
-    <Box safeArea p="2" py="8" w="90%" maxW="290">
-      <Heading
-        size="lg"
-        fontWeight="600"
-        color="coolGray.800"
-        _dark={{ color: 'warmGray.50' }}
-      >
-        Welcome
-      </Heading>
-      <Heading
-        mt="1"
-        _dark={{ color: 'warmGray.200' }}
-        color="coolGray.600"
-        fontWeight="medium"
-        size="xs"
-      >
-        Sign in to continue!
-      </Heading>
+    <Center>
+      <Box safeArea p="2" py="8" w="90%" maxW="290">
+        <Heading
+          size="lg"
+          fontWeight="600"
+          color="coolGray.800"
+          _dark={{ color: 'warmGray.50' }}
+        >
+          Welcome
+        </Heading>
+        <Heading
+          mt="1"
+          _dark={{ color: 'warmGray.200' }}
+          color="coolGray.600"
+          fontWeight="medium"
+          size="xs"
+        >
+          Sign in to continue!
+        </Heading>
 
-      <VStack space={3} mt="5">
-        <FormControl>
-          <FormControl.Label>Email ID</FormControl.Label>
-          <Input />
-        </FormControl>
-        <FormControl>
-          <FormControl.Label>Password</FormControl.Label>
-          <Input type="password" />
-          <Link
-            _text={{ fontSize: 'xs', fontWeight: '500', color: 'indigo.500' }}
-            alignSelf="flex-end"
-            mt="1"
-          >
-            Forget Password?
-          </Link>
-        </FormControl>
-        <Button mt="2" colorScheme="indigo">
-          Sign in
-        </Button>
-        <HStack mt="6" justifyContent="center">
-          <Text
-            fontSize="sm"
-            color="coolGray.600"
-            _dark={{ color: 'warmGray.200' }}
-          >
-            I'm a new user.{' '}
-          </Text>
-          <Link
-            _text={{
-              color: 'indigo.500',
-              fontWeight: 'medium',
-              fontSize: 'sm',
-            }}
-            href="#"
-          >
-            Sign Up
-          </Link>
-        </HStack>
-      </VStack>
-    </Box>
+        <VStack space={3} mt="5">
+          <FormControl>
+            <FormControl.Label>Email ID</FormControl.Label>
+            <Input />
+          </FormControl>
+          <FormControl>
+            <FormControl.Label>Password</FormControl.Label>
+            <Input type="password" />
+            <Link
+              _text={{ fontSize: 'xs', fontWeight: '500', color: 'indigo.500' }}
+              alignSelf="flex-end"
+              mt="1"
+            >
+              Forget Password?
+            </Link>
+          </FormControl>
+          <Button mt="2" colorScheme="indigo">
+            Sign in
+          </Button>
+          <HStack mt="6" justifyContent="center">
+            <Text
+              fontSize="sm"
+              color="coolGray.600"
+              _dark={{ color: 'warmGray.200' }}
+            >
+              I'm a new user.{' '}
+            </Text>
+            <Link
+              _text={{
+                color: 'indigo.500',
+                fontWeight: 'medium',
+                fontSize: 'sm',
+              }}
+              href="#"
+            >
+              Sign Up
+            </Link>
+          </HStack>
+        </VStack>
+      </Box>
+    </Center>
   );
 };
