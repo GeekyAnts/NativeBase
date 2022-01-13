b8264b333f469f5762c2996265984f98e208c595
meenu <makkarmini007@gmail.com>
2022-01-13 18:49:40 +0530

    fix: example fixes

--- example/storybook/stories/hooks/useMediaQuery/orientation.tsx
+++ example/storybook/stories/hooks/useMediaQuery/orientation.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import {
   Text,
   useMediaQuery,
-  Box,
   Heading,
-  Image,
-  AspectRatio,
+  VStack,
+  Skeleton,
   Center,
   Stack,
   HStack,
@@ -17,71 +16,27 @@ export const Example = () => {
     { orientation: 'portrait' },
   ]);
   return (
-    <Box>
+    <Center>
       {isPortrait ? (
-        <Box
-          rounded="lg"
-          overflow="hidden"
-          shadow={1}
+        <HStack
+          w="90%"
+          maxW="400"
           borderWidth="1"
-          _light={{ backgroundColor: 'gray.50', borderColor: 'coolGray.200' }}
-          _dark={{ borderColor: 'coolGray.600', backgroundColor: 'gray.700' }}
+          space={8}
+          rounded="md"
+          borderColor="coolGray.400"
+          p="4"
         >
-          <Box>
-            <AspectRatio ratio={16 / 9}>
-              <Image
-                source={{
-                  uri:
-                    'https://www.holidify.com/images/cmsuploads/compressed/Bangalore_citycover_20190613234056.jpg',
-                }}
-                alt="image"
-              />
-            </AspectRatio>
-            <Center
-              bg="violet.500"
-              _text={{ color: 'white', fontWeight: '700', fontSize: 'xs' }}
-              position="absolute"
-              bottom={0}
-              px="3"
-              py="1.5"
-            >
-              PHOTOS
-            </Center>
-          </Box>
-          <Stack p="4" space={3}>
-            <Stack space={2}>
-              <Heading size="md" ml="-1">
-                The Garden City
-              </Heading>
-              <Text
-                fontSize="xs"
-                _light={{ color: 'violet.500' }}
-                _dark={{ color: 'violet.300' }}
-                fontWeight="500"
-                ml="-0.5"
-                mt="-1"
-              >
-                The Silicon Valley of India.
-              </Text>
-            </Stack>
-            <Text fontWeight="400">
-              Bengaluru (also called Bangalore) is the center of India's
-              high-tech industry. The city is also known for its parks and
-              nightlife.
-            </Text>
-            <HStack
-              alignItems="center"
-              space={4}
-              justifyContent="space-between"
-            >
-              <HStack alignItems="center">
-                <Text color="gray.500" fontWeight="400">
-                  6 mins ago
-                </Text>
-              </HStack>
+          <VStack flex="3" space="4">
+            <Skeleton />
+            <Skeleton.Text />
+            <HStack space="2" alignItems="center">
+              <Skeleton size="5" rounded="full" />
+              <Skeleton h="3" flex="2" rounded="full" />
+              <Skeleton h="3" flex="1" rounded="full" />
             </HStack>
-          </Stack>
-        </Box>
+          </VStack>
+        </HStack>
       ) : (
         <></>
       )}
@@ -135,6 +90,6 @@ export const Example = () => {
       ) : (
         <></>
       )}
-    </Box>
+    </Center>
   );
 };
