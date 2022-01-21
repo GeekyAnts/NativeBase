e5f97ec86478da04cf5336a071b7da61173871e6
Rohit Singh <rohit99.ind@gmail.com>
2022-01-21 10:54:46 +0530

    fix: examples

--- example/storybook/stories/components/primitives/Icon/Sizes.tsx
+++ example/storybook/stories/components/primitives/Icon/Sizes.tsx
@@ -3,21 +3,7 @@ import { Icon, Heading, Center, VStack } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
 
 export const Example = () => {
-  const sizes = [
-    '10',
-    '50px',
-    '2xl',
-    // 'xs',
-    // 'sm',
-    // 'md',
-    // 'lg',
-    // 'xl',
-    // '2xl',
-    // '3xl',
-    // '4xl',
-    // '5xl',
-    // '6xl',
-  ];
+  const sizes = ['10', '50px', '2xl'];
   return (
     <Center>
       <Heading mb="10">Sizes</Heading>
