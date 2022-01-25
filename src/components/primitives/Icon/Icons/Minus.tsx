d17b1cd93eabee13b477abc4c7ec717b7ef4ee3b
Mayank Pagar <mayanks@geekyants.com>
2022-01-25 16:39:26 +0530

    feat: added new icon pack with proper sizing

--- src/components/primitives/Icon/Icons/Minus.tsx
+++ src/components/primitives/Icon/Icons/Minus.tsx
@@ -1,11 +1,6 @@
-import React from 'react';
 import { createIcon } from '../createIcon';
-import { G, Rect } from '../nbSvg';
 
 export const MinusIcon = createIcon({
-  path: (
-    <G fill="currentColor">
-      <Rect height="4" width="20" x="2" y="10" />
-    </G>
-  ),
+  viewBox: '0 0 16 16',
+  d: 'M14 7H2V8.875H14V7Z',
 });
