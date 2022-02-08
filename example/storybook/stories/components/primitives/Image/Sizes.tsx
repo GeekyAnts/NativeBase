bba1731e4767fe57576da6a82c759d9d0f0ab0a7
Viraj-10 <virajjoshi132@gmail.com>
2022-02-08 16:10:48 +0530

    fix: image size example scrollview

--- example/storybook/stories/components/primitives/Image/Sizes.tsx
+++ example/storybook/stories/components/primitives/Image/Sizes.tsx
@@ -3,31 +3,35 @@ import { VStack, Image, ScrollView, Heading, Center } from 'native-base';
 
 export function Example() {
   return (
-    <ScrollView px="20">
-      {/* <Center mt="3" h="80"> */}
+    <>
       <Heading mb="10" textAlign="center" mt="3">
         Image Sizes
       </Heading>
-      <VStack
-        space={2}
-        justifyContent="center"
-        alignItems="center"
-        safeAreaTop
-        my={6}
-      >
-        {['xs', 'sm', 'md', 'lg', 'xl', '2xl'].map((size) => (
-          <Image
-            key={size}
-            size={size}
-            resizeMode="cover"
-            source={{
-              uri: 'https://wallpaperaccess.com/full/317501.jpg',
-            }}
-            alt={'Alternate Text ' + size}
-          />
-        ))}
-      </VStack>
-      {/* </Center> */}
-    </ScrollView>
+      <ScrollView px="20">
+        {/* <Center mt="3" h="80"> */}
+
+        <VStack
+          space={2}
+          justifyContent="center"
+          alignItems="center"
+          safeAreaTop
+          // my={6}
+          mb={6}
+        >
+          {['xs', 'sm', 'md', 'lg', 'xl', '2xl'].map((size) => (
+            <Image
+              key={size}
+              size={size}
+              resizeMode="cover"
+              source={{
+                uri: 'https://wallpaperaccess.com/full/317501.jpg',
+              }}
+              alt={'Alternate Text ' + size}
+            />
+          ))}
+        </VStack>
+        {/* </Center> */}
+      </ScrollView>
+    </>
   );
 }
