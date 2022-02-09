215a0c86786975da3ca72609aa63a20025e6ae04
ankit-tailor <tailorankit3345@gmail.com>
2022-02-09 19:49:49 +0530

    feat: basic example table component

---
+++ example/storybook/stories/components/primitives/Table/Basic.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Table } from 'native-base';
+
+export const Example = () => {
+  return (
+    <>
+      <Table textAlign="left" bg="white">
+        <Table.Header>
+          <Table.HeaderData>Name</Table.HeaderData>
+          <Table.HeaderData>Address</Table.HeaderData>
+          <Table.HeaderData>Age</Table.HeaderData>
+        </Table.Header>
+        <Table.Row>
+          <Table.Data>Ankit</Table.Data>
+          <Table.Data>Gujarat</Table.Data>
+          <Table.Data> 21</Table.Data>
+        </Table.Row>
+        <Table.Row>
+          <Table.Data>Viraj</Table.Data>
+          <Table.Data>Maharashtra</Table.Data>
+          <Table.Data>24</Table.Data>
+        </Table.Row>
+        <Table.Row>
+          <Table.Data>Sanket</Table.Data>
+          <Table.Data>Banglore</Table.Data>
+          <Table.Data>26</Table.Data>
+        </Table.Row>
+        <Table.Row>
+          <Table.Data>Mayank</Table.Data>
+          <Table.Data>Nashik</Table.Data>
+          <Table.Data>21</Table.Data>
+        </Table.Row>
+      </Table>
+    </>
+  );
+};
