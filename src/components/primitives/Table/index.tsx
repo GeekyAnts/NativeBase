ba869b1d39d991c7f3882d04a0ce2f82dc2ea21e
ankit-tailor <tailorankit3345@gmail.com>
2022-02-09 19:51:51 +0530

    feat: table component

---
+++ src/components/primitives/Table/index.tsx
@@ -0,0 +1,13 @@
+import TableMain from './Table';
+import TableHeader from './TableHeader';
+import TableHeaderData from './TableHeaderData';
+import TableData from './TableData';
+import TableRow from './TableRow';
+
+const Table: any = TableMain;
+Table.Header = TableHeader;
+Table.HeaderData = TableHeaderData;
+Table.Row = TableRow;
+Table.Data = TableData;
+
+export default Table;
