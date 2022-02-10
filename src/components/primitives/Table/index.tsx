e38ba8b4832c70cc6f2806e884aad1e360de801a
ankit-tailor <tailorankit3345@gmail.com>
2022-02-10 16:17:45 +0530

    feat: add type for TableComponent

--- src/components/primitives/Table/index.tsx
+++ src/components/primitives/Table/index.tsx
@@ -3,11 +3,14 @@ import TableHeader from './TableHeader';
 import TableHeaderData from './TableHeaderData';
 import TableData from './TableData';
 import TableRow from './TableRow';
+import type { ITableComponentType } from './types';
 
-const Table: any = TableMain;
-Table.Header = TableHeader;
-Table.HeaderData = TableHeaderData;
-Table.Row = TableRow;
-Table.Data = TableData;
+const TableTemp: any = TableMain;
+TableTemp.Header = TableHeader;
+TableTemp.HeaderData = TableHeaderData;
+TableTemp.Row = TableRow;
+TableTemp.Data = TableData;
+
+const Table = TableTemp as ITableComponentType;
 
 export default Table;
