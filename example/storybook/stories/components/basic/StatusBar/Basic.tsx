8c4936097aee8b3135a399798095d6d8451647ad
Viraj-10 <virajjoshi132@gmail.com>
2022-02-27 13:14:45 +0530

    fix: added example for statusbar

--- example/storybook/stories/components/basic/StatusBar/Basic.tsx
+++ example/storybook/stories/components/basic/StatusBar/Basic.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
-import { StatusBar } from 'native-base';
+import React, { useState } from 'react';
+import { StatusBar, Box, Text, Button } from 'native-base';
+
 export const Example = () => {
-  return <StatusBar />;
+  const [hidden, setHidden] = useState(false);
+  const changeStatusBarVisibility = () => setHidden(!hidden);
+  return (
+    <Box flex={1} justifyContent="center" safeArea>
+      <StatusBar
+        animated={true}
+        backgroundColor="#61dafb"
+        barStyle="default"
+        showHideTransition="fade"
+        hidden={hidden}
+      />
+      <Text textAlign="center" mb={3}>
+        StatusBar Visibility:{'\n'}
+        {hidden ? 'Hidden' : 'Visible'}
+      </Text>
+      <Box p={10}>
+        <Button onPress={changeStatusBarVisibility}>Toggle StatusBar</Button>
+      </Box>
+    </Box>
+  );
 };
