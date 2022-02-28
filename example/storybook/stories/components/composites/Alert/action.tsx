dcd50327b9e85a6e5dc910a098f28ce3309dabf8
Viraj-10 <virajjoshi132@gmail.com>
2022-02-28 17:27:41 +0530

    fix: alert action,variant

--- example/storybook/stories/components/composites/Alert/action.tsx
+++ example/storybook/stories/components/composites/Alert/action.tsx
@@ -9,6 +9,7 @@ import {
   CloseIcon,
   Box,
   Text,
+  Center,
 } from 'native-base';
 
 export function Example() {
@@ -16,35 +17,37 @@ export function Example() {
   return (
     <Box w="100%" alignItems="center">
       <Collapse isOpen={show}>
-        <Alert w="90%" maxW="400" status="error">
-          <VStack space={1} flexShrink={1} w="100%">
-            <HStack
-              flexShrink={1}
-              space={2}
-              alignItems="center"
-              justifyContent="space-between"
-            >
-              <HStack flexShrink={1} space={2} alignItems="center">
-                <Alert.Icon />
-                <Text
-                  fontSize="md"
-                  fontWeight="medium"
-                  _dark={{ color: 'coolGray.800' }}
-                >
-                  Please try again later!
-                </Text>
+        <Center>
+          <Alert w="90%" maxW="400" status="error">
+            <VStack space={1} flexShrink={1} w="100%">
+              <HStack
+                flexShrink={1}
+                space={2}
+                alignItems="center"
+                justifyContent="space-between"
+              >
+                <HStack flexShrink={1} space={2} alignItems="center">
+                  <Alert.Icon />
+                  <Text
+                    fontSize="md"
+                    fontWeight="medium"
+                    _dark={{ color: 'coolGray.800' }}
+                  >
+                    Please try again later!
+                  </Text>
+                </HStack>
+                <IconButton
+                  variant="unstyled"
+                  icon={<CloseIcon size="3" color="coolGray.600" />}
+                  onPress={() => setShow(false)}
+                />
               </HStack>
-              <IconButton
-                variant="unstyled"
-                icon={<CloseIcon size="3" color="coolGray.600" />}
-                onPress={() => setShow(false)}
-              />
-            </HStack>
-            <Box pl="6" _dark={{ _text: { color: 'coolGray.600' } }}>
-              Your coupon could not be processed at this time.
-            </Box>
-          </VStack>
-        </Alert>
+              <Box pl="6" _dark={{ _text: { color: 'coolGray.600' } }}>
+                Your coupon could not be processed at this time.
+              </Box>
+            </VStack>
+          </Alert>
+        </Center>
       </Collapse>
       <Button size={'sm'} onPress={() => setShow(true)} mt={8} mx="auto">
         Re-Open
