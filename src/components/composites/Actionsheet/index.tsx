c69a515a204e7faa5a28e9f587b5644c42da536e
ankit-tailor <tailorankit3345@gmail.com>
2022-03-09 12:01:56 +0530

    fix: remove actionsheet header and footer

--- src/components/composites/Actionsheet/index.tsx
+++ src/components/composites/Actionsheet/index.tsx
@@ -1,15 +1,15 @@
 import ActionsheetMain from './Actionsheet';
 import ActionsheetItem from './ActionsheetItem';
-import ActionsheetHeader from './ActionsheetHeader';
-import ActionsheetFooter from './ActionsheetFooter';
+// import ActionsheetHeader from './ActionsheetHeader';
+// import ActionsheetFooter from './ActionsheetFooter';
 import ActionsheetContent from './ActionsheetContent';
 import type { IActionsheetComponentType } from './types';
 
 const ActionsheetTemp: any = ActionsheetMain;
 ActionsheetTemp.Content = ActionsheetContent;
 ActionsheetTemp.Item = ActionsheetItem;
-ActionsheetTemp.Header = ActionsheetHeader;
-ActionsheetTemp.Footer = ActionsheetFooter;
+// ActionsheetTemp.Header = ActionsheetHeader;
+// ActionsheetTemp.Footer = ActionsheetFooter;
 
 // To add typings
 const Actionsheet = ActionsheetTemp as IActionsheetComponentType;
@@ -20,6 +20,4 @@ export type {
   IActionsheetProps,
   IActionsheetContentProps,
   IActionsheetItemProps,
-  IActionsheetHeaderProps,
-  IActionsheetFooterProps,
 } from './types';
