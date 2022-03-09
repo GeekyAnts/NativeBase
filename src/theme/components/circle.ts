c0d1d272132d30037608cc3cde524a94ed3be8dd
ankit-tailor <tailorankit3345@gmail.com>
2022-03-09 16:41:46 +0530

    feat: circle theme

---
+++ src/theme/components/circle.ts
@@ -0,0 +1,12 @@
+const baseStyle = (props: any) => ({
+  rounded: 'full',
+  alignItems: 'center',
+  justifyContent: 'center',
+  size: props?.size,
+  height: props?.height,
+  width: props?.width,
+});
+
+export default {
+  baseStyle,
+};
