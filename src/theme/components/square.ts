a12cf8ff22a3a4290c8e1a8cc69d100fb906a853
ankit-tailor <tailorankit3345@gmail.com>
2022-03-11 15:12:11 +0530

    fix: add sizes

--- src/theme/components/square.ts
+++ src/theme/components/square.ts
@@ -1,13 +1,36 @@
-import type { Dict } from './../tools';
+const sizes = {
+  'xs': {
+    height: 10,
+    width: 10,
+  },
+  'sm': {
+    height: 12,
+    width: 12,
+  },
+  'md': {
+    height: 16,
+    width: 16,
+  },
+  'lg': {
+    height: 24,
+    width: 24,
+  },
+  'xl': {
+    height: 32,
+    width: 32,
+  },
+  '2xl': {
+    height: 40,
+    width: 40,
+  },
+};
 
-const baseStyle = (props: Dict) => ({
+const baseStyle = {
   alignItems: 'center',
   justifyContent: 'center',
-  size: props?.size,
-  height: props?.height,
-  width: props?.width,
-});
+};
 
 export default {
   baseStyle,
+  sizes,
 };
