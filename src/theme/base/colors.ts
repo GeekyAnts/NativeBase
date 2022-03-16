13930d45c8a931c2f779cd0f68a1c840dc5ca73a
Rohit Singh <rohit99.ind@gmail.com>
2022-03-16 14:54:58 +0530

    feat: text color palette

--- src/theme/base/colors.ts
+++ src/theme/base/colors.ts
@@ -310,6 +310,18 @@ const colors = {
     800: '#f4f4f5',
     900: '#fafafa',
   },
+  text: {
+    50: '#fafafa',
+    100: '#f5f5f5',
+    200: '#e5e5e5',
+    300: '#d4d4d4',
+    400: '#a3a3a3',
+    500: '#737373',
+    600: '#525252',
+    700: '#404040',
+    800: '#262626',
+    900: '#171717',
+  },
   // Derived colors
   danger: {} as IColorHues,
   error: {} as IColorHues,
