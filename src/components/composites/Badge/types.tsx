a9ae128f8217af5cb1cf06f10320b2ac45437e11
mayank-96 <pagarmayank07@gmail.com>
2022-03-16 18:47:47 +0530

    fix: badge design changes

--- src/components/composites/Badge/types.tsx
+++ src/components/composites/Badge/types.tsx
@@ -11,4 +11,20 @@ export interface IBadgeProps extends IBoxProps<IBadgeProps> {
    * The color scheme to use for the badge. Must be a key in theme.colors.
    */
   colorScheme?: string;
+  /**
+   * The right icon element to use in the button.
+   */
+  rightIcon?: JSX.Element | Array<JSX.Element>;
+  /**
+   * The left icon element to use in the button.
+   */
+  leftIcon?: JSX.Element | Array<JSX.Element>;
+  /**
+   * The start icon element to use in the button.
+   */
+  startIcon?: JSX.Element | Array<JSX.Element>;
+  /**
+   * The end icon element to use in the button.
+   */
+  endIcon?: JSX.Element | Array<JSX.Element>;
 }
