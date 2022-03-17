a58413544580b0b49a7959e496836c51785b3857
MD-REHMAN <rehman.2468@gmail.com>
2022-03-17 19:03:25 +0530

    fix: example

--- example/storybook/stories/components/primitives/Button/sizes.tsx
+++ example/storybook/stories/components/primitives/Button/sizes.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import { Button, VStack, HStack, AddIcon } from 'native-base';
+import { Button, VStack } from 'native-base';
 
 export const Example = () => {
   return (
     <VStack space={4} alignItems="center">
       {['xs', 'sm', 'md', 'lg'].map((size) => (
-        <HStack space="4">
-          <Button size={size}>BUTTON</Button>
-          <Button size={size} leftIcon={<AddIcon />}>
-            BUTTON
-          </Button>
-          <Button size={size} rightIcon={<AddIcon />}>
-            BUTTON
-          </Button>
-          <Button size={size} isLoading>
-            BUTTON
-          </Button>
-        </HStack>
+        <Button key={size} size={size}>
+          BUTTON
+        </Button>
       ))}
     </VStack>
   );
