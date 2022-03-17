bebdccfddedcfe14cceda6320ff415f039417e65
ankit-tailor <tailorankit3345@gmail.com>
2022-03-17 16:35:14 +0530

    fix: hover issue

--- src/components/composites/Popover/PopoverCloseButton.tsx
+++ src/components/composites/Popover/PopoverCloseButton.tsx
@@ -4,15 +4,22 @@ import { default as IconButton, IIconButtonProps } from '../IconButton';
 import { CloseIcon } from '../../primitives/Icon/Icons';
 import { PopoverContext } from './PopoverContext';
 import { useHasResponsiveProps } from '../../../hooks/useHasResponsiveProps';
+import { useHover } from '@react-native-aria/interactions';
+import { mergeRefs } from '../../../utils';
 
 const PopoverCloseButton = (
   props: Omit<IIconButtonProps, 'icon'>,
   ref: any
 ) => {
   const { onClose }: any = React.useContext(PopoverContext);
+  const _ref = React.useRef(null);
+
+  const { isHovered } = useHover({}, _ref);
+
   const { _icon, ...resolvedPorps } = usePropsResolution(
     'PopoverCloseButton',
-    props
+    props,
+    { isHovered }
   );
   //TODO: refactor for responsive prop
   if (useHasResponsiveProps(props)) {
@@ -20,7 +27,7 @@ const PopoverCloseButton = (
   }
   return (
     <IconButton
-      ref={ref}
+      ref={mergeRefs([ref, _ref])}
       {...resolvedPorps}
       icon={<CloseIcon {..._icon} />}
       onPress={onClose}
