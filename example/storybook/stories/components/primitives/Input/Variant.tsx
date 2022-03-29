9f470b5599164f791a2a63f9a4e99974f525758f
amars29 <amars@geekyants>
2022-03-29 12:48:31 +0530

    fix: variants example in Input

--- example/storybook/stories/components/primitives/Input/Variant.tsx
+++ example/storybook/stories/components/primitives/Input/Variant.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { Input, Stack, Center } from 'native-base';
 export const Example = () => {
   return (
-    <Center>
-      <Stack space={4} w="75%" maxW="300px">
-        <Input variant="outline" placeholder="Outline" />
-        <Input variant="filled" placeholder="Filled" />
-        <Input variant="underlined" placeholder="Underlined" />
-        <Input variant="unstyled" placeholder="Unstyled" />
-        <Input variant="rounded" placeholder="Round" />
-      </Stack>
-    </Center>
+    <Stack space={4} w="75%">
+      <Input variant="outline" placeholder="Outline" />
+      <Input variant="filled" placeholder="Filled" />
+      <Input variant="underlined" placeholder="Underlined" />
+      <Input variant="unstyled" placeholder="Unstyled" />
+      <Input variant="rounded" placeholder="Round" />
+    </Stack>
   );
 };
