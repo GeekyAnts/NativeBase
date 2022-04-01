e7aede65e589bf2f05969732e353f8396bb69114
amars29 <amars@geekyants>
2022-04-01 16:49:56 +0530

    fix: outline color of pressable

--- src/theme/components/pressable.ts
+++ src/theme/components/pressable.ts
@@ -1,4 +1,24 @@
+import { mode } from '../tools';
+
+const baseStyle = (props: any) => {
+  const { primary } = props.theme.colors;
+  const focusRing = mode(
+    {
+      boxShadow: `${primary[400]} 0px 0px 0px 2px`,
+    },
+    {
+      boxShadow: `${primary[500]} 0px 0px 0px 2px`,
+    }
+  )(props);
+
+  return {
+    _focusVisible: {
+      _web: { style: { ...focusRing, outlineWidth: 0 } },
+    },
+  };
+};
+
 export default {
-  baseStyle: {},
+  baseStyle,
   defaultProps: {},
 };
