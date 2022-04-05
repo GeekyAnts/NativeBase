43d161d8f41a1dc869328bae50d540917aa48a9f
Viraj-10 <virajjoshi132@gmail.com>
2022-04-05 15:44:16 +0530

    fix: button size typing

--- src/components/primitives/Button/types.ts
+++ src/components/primitives/Button/types.ts
@@ -63,7 +63,7 @@ export interface InterfaceButtonProps
   /**
    * The size of the button.
    */
-  size?: ThemeComponentSizeType<'Button'>;
+  size?: ThemeComponentSizeType<'Button'> | number;
   // size?: SizeType;
   /**
    * The start icon element to use in the button.
