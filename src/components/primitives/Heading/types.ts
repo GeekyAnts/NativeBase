f92f65ca208aebc219fac6d2aed82eafd7c6859b
Viraj-10 <virajjoshi132@gmail.com>
2022-04-05 16:17:43 +0530

    fix: heading size typing

--- src/components/primitives/Heading/types.ts
+++ src/components/primitives/Heading/types.ts
@@ -1,6 +1,4 @@
 import type { ITextProps } from '../Text';
-import type { ResponsiveValue } from '../../../components/types';
-import type { ISizes } from '../../../theme/base/sizes';
 
 import type { CustomProps, ThemeComponentSizeType } from '../../types/utils';
 
@@ -9,9 +7,7 @@ export interface IterfaceHeadingProps extends ITextProps {
    * The size of the heading.
    * @default xl
    */
-  size?:
-    | ResponsiveValue<ISizes | (string & {}) | number>
-    | ThemeComponentSizeType<'Heading'>;
+  size?: ThemeComponentSizeType<'Heading'>;
 }
 
 export type IHeadingProps = IterfaceHeadingProps & CustomProps<'Heading'>;
