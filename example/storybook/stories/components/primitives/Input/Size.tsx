6e883c4ac5c42b18559b3987c0433e07ee8f44b5
ankit-tailor <tailorankit3345@gmail.com>
2022-04-07 16:57:11 +0530

    fix: input width

--- example/storybook/stories/components/primitives/Input/Size.tsx
+++ example/storybook/stories/components/primitives/Input/Size.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import { Input, Stack, Center } from 'native-base';
+import { Input, Stack } from 'native-base';
 
 export const Example = () => {
   return (
-    <Center>
-      <Stack mt={3} space={4} w="75%" maxW="300px">
-        <Input size="xs" placeholder="xs Input" />
-        <Input size="sm" placeholder="sm Input" />
-        <Input size="md" placeholder="md Input" />
-        <Input size="lg" placeholder="lg Input" />
-        <Input size="xl" placeholder="xl Input" />
-        <Input size="2xl" placeholder="2xl Input" />
-      </Stack>
-    </Center>
+    <Stack space={4} w="75%" maxW="300px">
+      <Input size="xs" placeholder="xs Input" />
+      <Input size="sm" placeholder="sm Input" />
+      <Input size="md" placeholder="md Input" />
+      <Input size="lg" placeholder="lg Input" />
+      <Input size="xl" placeholder="xl Input" />
+      <Input size="2xl" placeholder="2xl Input" />
+    </Stack>
   );
 };
