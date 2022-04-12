2c40d4c7c3aecebbd0f3802914904c3cc913ad92
shivam kawde <shivamkawd@geekyants.com>
2022-04-12 18:25:18 +0530

    added new fab props

--- src/theme/components/fab.ts
+++ src/theme/components/fab.ts
@@ -3,6 +3,11 @@ const placementProps: any = {
   'top-left': { top: 4, left: 4, position: 'absolute' },
   'bottom-right': { bottom: 4, right: 4, position: 'absolute' },
   'bottom-left': { bottom: 4, left: 4, position: 'absolute' },
+  'top-center':{left:'50%',position:'absolute',},
+  'bottom-center':{left:'50%',bottom:0,position:'absolute',},
+  'center':{left:'50%',top:'50%',position:'absolute',},
+  'left-center':{top:'50%',position:'absolute',}, 
+  'right-center':{top:'50%',right:0,position:'absolute',}
 };
 const baseStyle = {
   shadow: 7,
