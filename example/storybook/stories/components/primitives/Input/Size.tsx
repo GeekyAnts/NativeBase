4bf8adbba2cf801dfa2f593edb37cfcef028c80f
mayank-96 <pagarmayank07@gmail.com>
2022-04-13 19:22:43 +0530

    fix: input example center

--- example/storybook/stories/components/primitives/Input/Size.tsx
+++ example/storybook/stories/components/primitives/Input/Size.tsx
@@ -3,7 +3,7 @@ import { Input, Stack } from 'native-base';
 
 export const Example = () => {
   return (
-    <Stack space={4} w="75%" maxW="300px">
+    <Stack space={4} w="75%" maxW="300px" mx="auto">
       <Input size="xs" placeholder="xs Input" />
       <Input size="sm" placeholder="sm Input" />
       <Input size="md" placeholder="md Input" />
