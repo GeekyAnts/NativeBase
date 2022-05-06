4edab3039965792e7f9443bad9fd402fe1a1a45a
mayank-96 <pagarmayank07@gmail.com>
2022-05-06 13:28:59 +0530

    fix: fade animation props

--- src/components/composites/Transitions/Fade.tsx
+++ src/components/composites/Transitions/Fade.tsx
@@ -5,16 +5,36 @@ import { useHasResponsiveProps } from '../../../hooks/useHasResponsiveProps';
 import { usePropsResolution } from '../../../hooks/';
 
 const Fade = ({ children, ...props }: IFadeProps, ref?: any) => {
-  const { in: animationState, ...resolvedProps } = usePropsResolution(
-    'Fade',
-    props
-  );
+  const {
+    in: animationState,
+    entryDuration,
+    exitDuration,
+    delay,
+    ...resolvedProps
+  } = usePropsResolution('Fade', props);
+
+  // Need to have similar API for all animations
+
+  // Temporary fix
+  const animationStyle = {
+    style: {
+      initial: { opacity: 0 },
+      animate: { opacity: 1, transition: { duration: entryDuration, delay } },
+      exit: { opacity: 0, transition: { duration: exitDuration, delay } },
+    },
+  };
+
   //TODO: refactor for responsive prop
   if (useHasResponsiveProps(props)) {
     return null;
   }
   return (
-    <PresenceTransition visible={animationState} {...resolvedProps} ref={ref}>
+    <PresenceTransition
+      visible={animationState}
+      {...animationStyle.style}
+      {...resolvedProps}
+      ref={ref}
+    >
       {children}
     </PresenceTransition>
   );
