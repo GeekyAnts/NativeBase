6fc93d48ba6ae17de0f290e1d119a161bf3fbc66
Viraj-10 <virajjoshi132@gmail.com>
2022-05-31 14:07:02 +0530

    feat: heading performance fix

--- src/components/primitives/Heading/index.tsx
+++ src/components/primitives/Heading/index.tsx
@@ -3,14 +3,33 @@ import Text from '../Text';
 import { usePropsResolution } from '../../../hooks/useThemeProps';
 import type { IHeadingProps } from './types';
 import { useHasResponsiveProps } from '../../../hooks/useHasResponsiveProps';
+import { useColorMode } from '../../../core/color-mode';
+import { getThemeProps } from '../../../core';
 
 const Heading = (props: IHeadingProps, ref: any) => {
-  const resolvedProps = usePropsResolution('Heading', props);
+  const { colorMode } = useColorMode();
+
+  const { style, unResolvedProps } = getThemeProps(
+    'Heading',
+    colorMode,
+    {},
+    props
+  );
+
+  const { size: _size, ...resolvedProps } = usePropsResolution(
+    'Heading',
+    {
+      ...unResolvedProps,
+      ...props,
+    },
+    {}
+  );
   //TODO: refactor for responsive prop
   if (useHasResponsiveProps(props)) {
     return null;
   }
-  return <Text {...resolvedProps} ref={ref} />;
+
+  return <Text INTERNAL_themeStyle={style} {...resolvedProps} ref={ref} />;
 };
 
 export default memo(forwardRef(Heading));
