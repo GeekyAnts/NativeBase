096332a5c014c048595a8cc61fa42d3e0f870f9b
Viraj-10 <virajjoshi132@gmail.com>
2022-05-31 12:30:17 +0530

    feat: center performance fix

--- src/components/composites/Center/Center.tsx
+++ src/components/composites/Center/Center.tsx
@@ -3,14 +3,32 @@ import { usePropsResolution } from '../../../hooks/useThemeProps';
 import Box from '../../primitives/Box';
 import type { ICenterProps } from './types';
 import { useHasResponsiveProps } from '../../../hooks/useHasResponsiveProps';
+import { useColorMode } from '../../../core/color-mode';
+import { getThemeProps } from '../../../core';
 
 const Center = (props: ICenterProps, ref: any) => {
-  const reslovedProps = usePropsResolution('Center', props);
+  const { colorMode } = useColorMode();
+
+  const { style, unResolvedProps } = getThemeProps(
+    'Center',
+    colorMode,
+    {},
+    props
+  );
+
+  const reslovedProps = usePropsResolution(
+    'Center',
+    {
+      ...unResolvedProps,
+      ...props,
+    },
+    {}
+  );
   //TODO: refactor for responsive prop
   if (useHasResponsiveProps(props)) {
     return null;
   }
-  return <Box ref={ref} {...reslovedProps} />;
+  return <Box ref={ref} INTERNAL_themeStyle={style} {...reslovedProps} />;
 };
 
 export default memo(forwardRef(Center));
