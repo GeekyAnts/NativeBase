49acd224fad8e3c1aa3b9983d40f5e1f640d0c33
Viraj-10 <virajjoshi132@gmail.com>
2022-05-31 12:53:31 +0530

    feat: container performance fix

--- src/components/composites/Container/index.tsx
+++ src/components/composites/Container/index.tsx
@@ -3,16 +3,34 @@ import { usePropsResolution } from '../../../hooks';
 import Box from '../../primitives/Box';
 import type { IContainerProps } from './types';
 import { useHasResponsiveProps } from '../../../hooks/useHasResponsiveProps';
+import { useColorMode } from '../../../core/color-mode';
+import { getThemeProps } from '../../../core';
 
 const Container = ({ children, ...props }: IContainerProps, ref?: any) => {
-  const resolvedProps = usePropsResolution('Container', props);
+  const { colorMode } = useColorMode();
+  const { style, unResolvedProps } = getThemeProps(
+    'Container',
+    colorMode,
+    {},
+    props
+  );
+
+  const resolvedProps = usePropsResolution(
+    'Container',
+    {
+      ...unResolvedProps,
+      ...props,
+    },
+    {}
+  );
 
   //TODO: refactor for responsive prop
   if (useHasResponsiveProps(props)) {
     return null;
   }
+
   return (
-    <Box ref={ref} {...resolvedProps}>
+    <Box ref={ref} INTERNAL_themeStyle={style} {...resolvedProps}>
       {children}
     </Box>
   );
