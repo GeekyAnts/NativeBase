7dcb4bcfe8432cda0876a253dfbf7575e9258483
Viraj-10 <virajjoshi132@gmail.com>
2022-06-27 13:47:14 +0530

    fix: revert transfromer of font

--- src/theme/styled-system.ts
+++ src/theme/styled-system.ts
@@ -531,10 +531,10 @@ export const typography = {
   fontFamily: {
     property: 'fontFamily',
     scale: 'fonts',
-    transformer: (val: any, scale: any) => {
-      const value = get(scale, val);
-      return value ? value.toString() : undefined;
-    },
+    // transformer: (val: any, scale: any) => {
+    //   const value = get(scale, val);
+    //   return value ? value.toString() : undefined;
+    // },
   },
   fontSize: {
     property: 'fontSize',
