58099b2d827935623fbb0ab98d9f46a1c3ef3bf0
Suraj Ahmed <suraj@geekyants.com>
2022-07-07 19:14:28 +0530

    fix: button basic example

--- example/storybook/stories/components/primitives/Button/basic.tsx
+++ example/storybook/stories/components/primitives/Button/basic.tsx
@@ -4,27 +4,7 @@ import { Button, Box } from 'native-base';
 export const Example = () => {
   return (
     <Box alignItems="center">
-      <Button
-        size="sm"
-        colorScheme={'green'}
-        onPress={() => console.log('hello world')}
-      >
-        Click Me
-      </Button>
-      {/* <Button
-        size="sm"
-        // colorScheme={'green'}
-        onPress={() => console.log('hello world')}
-      >
-        Click Me
-      </Button> */}
-      {/* <Button
-        size="sm"
-        // colorScheme={'green'}
-        onPress={() => console.log('hello world')}
-      >
-        Click Me
-      </Button> */}
+      <Button onPress={() => console.log('hello world')}>Click Me</Button>
     </Box>
   );
 };
