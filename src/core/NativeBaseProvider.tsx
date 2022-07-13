c56d254d3732b20fb0e506f293090208b50c236e
Rohit Singh <rohit99.ind@gmail.com>
2022-07-13 20:37:04 +0530

    feat: extend theme map update

--- src/core/NativeBaseProvider.tsx
+++ src/core/NativeBaseProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
   SafeAreaProvider,
   Metrics,
@@ -19,7 +19,10 @@ import { useToast } from '../components/composites/Toast';
 import { Platform, useWindowDimensions } from 'react-native';
 import { getClosestBreakpoint } from '../theme/tools/utils';
 import { platformSpecificSpaceUnits } from '../theme/tools/platformSpecificSpaceUnits';
-import { init as initResolvedStyleMap } from '../utils/styled';
+import {
+  init as initResolvedStyleMap,
+  resolvedStyledMap,
+} from '../utils/styled';
 import { uniqueId } from 'lodash';
 
 initResolvedStyleMap();
@@ -48,6 +51,71 @@ export interface NativeBaseProviderProps {
   // Refer https://github.com/th3rdwave/react-native-safe-area-context#testing
 }
 
+const getComponentNamesFromTheme = (theme: ITheme) => {
+  const componentNames = theme?.components
+    ? Object.keys(theme?.components)
+    : [];
+  return componentNames;
+};
+
+const getComponentVariants = (componentTheme: any) => {
+  const variants = componentTheme.variants
+    ? Object.keys(componentTheme.variants)
+    : [];
+  return variants;
+};
+
+const getComponentSizes = (componentTheme: any) => {
+  const sizes = componentTheme.sizes ? Object.keys(componentTheme.sizes) : [];
+  // console.log('component', componentTheme);
+  return sizes;
+};
+
+function findCharacterFrequency(str: string, character: string) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === character) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const isKeyWithSingleOrDoubleValue = (key: string) => {
+  return findCharacterFrequency(key, '.') === 0 ||
+    findCharacterFrequency(key, '.') === 1
+    ? true
+    : false;
+};
+
+const deleteFromResolvedMap = (
+  object: any,
+  providerId: string,
+  value: string,
+  condition: string
+) => {
+  const keys = Object.keys(object[providerId]);
+  keys.forEach((key) => {
+    if (key.startsWith(value) && key.includes(condition)) {
+      delete object[providerId][key];
+    }
+    if (
+      key.startsWith(value) &&
+      !key.includes(condition) &&
+      isKeyWithSingleOrDoubleValue(key)
+    ) {
+      delete object[providerId][key];
+      // console.log(
+      //   'resolvedStyledMap',
+      //   JSON.parse(JSON.stringify(object)),
+      //   providerId,
+      //   key
+      // );
+    }
+  });
+  // return ;
+};
+
 const NativeBaseProvider = (props: NativeBaseProviderProps) => {
   const {
     colorModeManager,
@@ -59,7 +127,31 @@ const NativeBaseProvider = (props: NativeBaseProviderProps) => {
     disableContrastText,
     providerId,
   } = props;
-  const theme = config.theme ?? propsTheme;
+  // @ts-ignore
+  const { customUserTheme, ...theme } = config.theme ?? propsTheme;
+  useEffect(() => {
+    let components = getComponentNamesFromTheme(customUserTheme);
+
+    components.map((component) => {
+      let variants = getComponentVariants(
+        customUserTheme?.components?.[component]
+      );
+      let sizes = getComponentSizes(customUserTheme?.components?.[component]);
+      variants.map((variant) => {
+        deleteFromResolvedMap(
+          resolvedStyledMap,
+          // @ts-ignore
+          providerId,
+          component,
+          variant
+        );
+      });
+      sizes.map((size) => {
+        // @ts-ignore
+        deleteFromResolvedMap(resolvedStyledMap, providerId, component, size);
+      });
+    });
+  }, [customUserTheme, providerId]);
 
   const newTheme = React.useMemo(() => {
     if (config.enableRem) {
