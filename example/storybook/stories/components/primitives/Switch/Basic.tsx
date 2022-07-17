175f3c6eae143f3ba2a15fadebb990aef26cd105
cyrus25 <cyruspassi@gmail.com>
2022-07-17 17:43:09 +0530

    fix: switch onToggle not getting updated value

--- example/storybook/stories/components/primitives/Switch/Basic.tsx
+++ example/storybook/stories/components/primitives/Switch/Basic.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import { Switch, HStack, Text } from 'native-base';
 
 export const Example = () => {
+  const [isEnabled, setIsEnabled] = React.useState(false);
   return (
     <HStack alignItems="center" space={4}>
       <Text>Bluetooth</Text>
-      <Switch size="sm" />
+      <Switch
+        value={isEnabled}
+        onToggle={(value) => {
+          setIsEnabled(value);
+        }}
+        size="sm"
+      />
     </HStack>
   );
 };
