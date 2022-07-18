a5cc7687b6388cef02106026cb51da75feacd7e2
ankit-tailor <tailorankit3345@gmail.com>
2022-07-18 16:44:00 +0530

    fix: popover break on falsy children value

--- src/components/composites/Popover/PopoverContent.tsx
+++ src/components/composites/Popover/PopoverContent.tsx
@@ -52,8 +52,11 @@ export const PopoverContent = React.forwardRef(
 
     let arrowElement = null;
     const restChildren: any = [];
-    React.Children.forEach(props.children, (child) => {
-      if (child.type.displayName === 'PopperArrow') {
+    React.Children.toArray(props.children).forEach((child: any) => {
+      if (
+        typeof child === 'object' &&
+        child.type.displayName === 'PopperArrow'
+      ) {
         arrowElement = React.cloneElement(child, {
           backgroundColor: child.props.color ?? color,
         });
