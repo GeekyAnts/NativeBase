342191368f26c4fe2255035582fdf0a2f0dd3d2c
cyrus25 <cyruspassi@gmail.com>
2022-07-19 12:20:59 +0530

    fix: reverted onToggle example

--- example/storybook/stories/components/primitives/Switch/Basic.tsx
+++ example/storybook/stories/components/primitives/Switch/Basic.tsx
@@ -2,17 +2,10 @@ import React from 'react';
 import { Switch, HStack, Text } from 'native-base';
 
 export const Example = () => {
-  const [isEnabled, setIsEnabled] = React.useState(false);
   return (
     <HStack alignItems="center" space={4}>
       <Text>Bluetooth</Text>
-      <Switch
-        value={isEnabled}
-        onToggle={(value) => {
-          setIsEnabled(value);
-        }}
-        size="sm"
-      />
+      <Switch size="sm" />
     </HStack>
   );
 };
