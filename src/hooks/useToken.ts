7ec0dda86990ef7a11cada0f377d3308351f4362
ankit-tailor <tailorankit3345@gmail.com>
2022-07-29 15:49:49 +0530

    fix: resolve component tokenized value

--- src/hooks/useToken.ts
+++ src/hooks/useToken.ts
@@ -4,7 +4,8 @@ import get from 'lodash.get';
 export function useToken<T extends string | number>(
   property: string,
   token: T | T[],
-  fallback?: T | T[]
+  fallback?: T | T[],
+  config?: { component: string; property: string }
 ) {
   const theme = useTheme();
   if (Array.isArray(token)) {
@@ -16,6 +17,20 @@ export function useToken<T extends string | number>(
       const path = `${property}.${innerToken}`;
       return get(theme, path, fallbackArr[index] ?? innerToken);
     });
+  }
+
+  if (config?.component && config?.property) {
+    const componentThemePath = `components.${config.component}.${config?.property}.${token}`;
+    const tokenizedThemeValue = get(
+      theme,
+      componentThemePath,
+      fallback ?? token
+    );
+
+    const path = `${property}.${tokenizedThemeValue}`;
+    const tokenizedValue = get(theme, path, fallback ?? token);
+
+    return tokenizedValue;
   }
 
   const path = `${property}.${token}`;
