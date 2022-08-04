952964a713a3616747f151c8f7932821c8800d5f
amars29 <amars@geekyants>
2022-08-04 12:11:07 +0530

    feat: converted props to responsive props

--- src/components/composites/SimpleGrid/types.ts
+++ src/components/composites/SimpleGrid/types.ts
@@ -1,9 +1,10 @@
+import type { ResponsiveValue } from '../../../components/types';
 import type { InterfaceBoxProps } from '../../primitives/Box/types';
 
 export type ISimpleGridProps = InterfaceBoxProps<ISimpleGridProps> & {
-  space?: number;
-  columns?: number;
-  minChildWidth?: number;
-  spacingX?: number;
-  spacingY?: number;
+  space?: ResponsiveValue<number>;
+  columns?: ResponsiveValue<number>;
+  minChildWidth?: ResponsiveValue<number>;
+  spacingX?: ResponsiveValue<number>;
+  spacingY?: ResponsiveValue<number>;
 };
