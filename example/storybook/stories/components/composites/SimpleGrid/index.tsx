f206eda5c8e0709ef26da802f5ce1445c2de105b
amars29 <amars@geekyants>
2022-08-05 10:52:02 +0530

    fix: corrected imports in SimpleGrid

--- example/storybook/stories/components/composites/SimpleGrid/index.tsx
+++ example/storybook/stories/components/composites/SimpleGrid/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import { withKnobs } from '@storybook/addon-knobs';
 import Wrapper from './../../Wrapper';
-import NumberOfColumnsSimpleGrid from './NumberOfColumns';
-import MinChildWidthSimpleGrid from './MinWidthColumns';
-import ResponsiveColumns from './ResponsiveColumns';
-import Spacing from './Spacing';
+import { Example as NumberOfColumnsSimpleGrid } from './NumberOfColumns';
+import { Example as MinChildWidthSimpleGrid } from './MinWidthColumns';
+import { Example as ResponsiveColumns } from './ResponsiveColumns';
+import { Example as Spacing } from './Spacing';
 
 storiesOf('SimpleGrid', module)
   .addDecorator(withKnobs)
