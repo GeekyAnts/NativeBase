f7aad04c30f052e64b1db13926790a4106b343d2
Suraj Ahmed <suraj@geekyants.com>
2022-09-07 16:42:34 +0530

    fix: updated hstack theme

--- src/theme/components/hstack.ts
+++ src/theme/components/hstack.ts
@@ -1,15 +1,6 @@
 const baseStyle = {};
 const defaultProps = {};
-const sizes = {
-  'gutter': 0,
-  '2xs': 1,
-  'xs': 2,
-  'sm': 3,
-  'md': 4,
-  'lg': 6,
-  'xl': 7,
-  '2xl': 8,
-};
+const sizes = {};
 export default {
   baseStyle,
   defaultProps,
