c29a966c40fd926eedd59f41b2244178d79659dc
Suraj Ahmed <suraj@geekyants.com>
2022-09-07 15:28:43 +0530

    fix: added type for variant and size in custom props type

--- src/theme/components/hstack.ts
+++ src/theme/components/hstack.ts
@@ -1,6 +1,17 @@
 const baseStyle = {};
 const defaultProps = {};
+const sizes = {
+  'gutter': 0,
+  '2xs': 1,
+  'xs': 2,
+  'sm': 3,
+  'md': 4,
+  'lg': 6,
+  'xl': 7,
+  '2xl': 8,
+};
 export default {
   baseStyle,
   defaultProps,
+  sizes,
 };
