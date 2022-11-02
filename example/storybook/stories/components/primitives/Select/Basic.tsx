09a908efd736be8101622dfbeacbfaee901eada0
ruchik02 <ruchikasharma.cse19@chitkarauniversity.edu.in>
2022-11-02 17:48:19 +0530

    fix: alignment issue in select component in basic example

--- example/storybook/stories/components/primitives/Select/Basic.tsx
+++ example/storybook/stories/components/primitives/Select/Basic.tsx
@@ -6,7 +6,7 @@ export const Example = () => {
 
   return (
     <Center>
-      <Box maxW="300">
+      <Box maxW="300" w="3/4">
         <Select
           selectedValue={service}
           minWidth="200"
