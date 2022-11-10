0c6e843591f49f130e93a92e7d427b0bd8e0525a
ruchik02 <ruchikasharma.cse19@chitkarauniversity.edu.in>
2022-11-10 18:52:48 +0530

    fix: remove space from addon component

--- example/storybook/stories/components/primitives/Input/Addons.tsx
+++ example/storybook/stories/components/primitives/Input/Addons.tsx
@@ -10,7 +10,7 @@ import {
 
 export const Example = () => {
   return (
-    <Stack space={4} w="100%" alignItems="center">
+    <Stack w="100%" alignItems="center">
       <InputGroup w={{ base: '70%', md: '25%' }} justifyContent="center">
         <InputLeftAddon children={'https://'} />
         <Input w={{ base: '70%', md: '100%' }} placeholder="nativebase" />
