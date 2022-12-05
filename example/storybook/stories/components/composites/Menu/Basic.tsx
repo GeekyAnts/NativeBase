9901ca795d1ff03ad15f067722fc203f8b254bf1
Suraj Ahmed <suraj@geekyants.com>
2022-12-05 20:16:04 +0530

    fix: menu example changes

--- example/storybook/stories/components/composites/Menu/Basic.tsx
+++ example/storybook/stories/components/composites/Menu/Basic.tsx
@@ -13,8 +13,6 @@ export function Example() {
             </Pressable>
           );
         }}
-        // isOpen={true}
-        // defaultIsOpen={true}
       >
         <Menu.Item>Arial</Menu.Item>
         <Menu.Item>Nunito Sans</Menu.Item>
